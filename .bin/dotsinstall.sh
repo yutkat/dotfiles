882c90219c1e95d7fcd24c1b2a9c417616c114ad
yutakatay <yutakatay@gmail.com>
2015-12-28 23:32:35 +0900

    Update auto copy

--- .bin/dotsinstall.sh
+++ .bin/dotsinstall.sh
@@ -2,7 +2,9 @@
 set -ue
 
 helpmsg(){
-    echo "Usage: $0 [--help|-h] [--without-tmux-extensions]" 0>&2
+    echo "Usage: $0 [--help|-h] [--without-tmux-extensions|--with-copy-to-home|--force]" 0>&2
+    echo '  --with-copy-to-home: dotfiles copy to $HOME'
+    echo "  --force: force overwrite"
     echo ""
 }
 
@@ -147,11 +149,23 @@ install_fzf(){
     $fzf_dir/install --no-key-bindings --completion  --no-update-rc
 }
 
+copy_to_homedir() {
+    SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    DOTDIR=$(readlink -f ${SCRIPT_DIR}/..)
+    if [[ $HOME != $DOTDIR ]];then
+        echo "cp -r${FORCE_OVERWRITE} ${DOTDIR}/* ${DOTDIR}/.[^.]* $HOME"
+        cp -r${FORCE_OVERWRITE} ${DOTDIR}/* ${DOTDIR}/.[^.]* $HOME
+    fi
+}
+
 
 ############
 ### main ###
 ############
 WITHOUT_TMUX_EXTENSIONS="false"
+UPDATE_MODE="false"
+FORCE_OVERWRITE=""
+COPY_TO_HOME_MODE="false"
 
 while [ $# -gt 0 ];do
     case ${1} in
@@ -165,6 +179,15 @@ while [ $# -gt 0 ];do
         --without-tmux-extensions)
             WITHOUT_TMUX_EXTENSIONS="true"
         ;;
+        --update|-u)
+            UPDATE_MODE="true"
+        ;;
+        --with-copy-to-home|-c)
+            COPY_TO_HOME_MODE="true"
+        ;;
+        --force|-f)
+            FORCE_OVERWRITE="f"
+        ;;
         *)
         ;;
     esac
@@ -173,6 +196,10 @@ done
 
 
 DISTRO=`whichdistro`
+
+if [[ $COPY_TO_HOME_MODE ]];then
+    copy_to_homedir
+fi
 
 checkinstall zsh git vim tmux ctags bc wget xsel
 makedir
