1021136df87819d2734a18f6604cd6c9c4c412e6
Yuta Katayama <yutakatay@gmail.com>
2017-01-11 01:34:05 +0900

    Debug wercker

--- .bin/gnome-terminal-config-restore.sh
+++ .bin/gnome-terminal-config-restore.sh
@@ -4,9 +4,10 @@ set -ue
 
 REPO_ROOT_DIR=$(builtin cd $(dirname "${BASH_SOURCE[0]:-$0}")/.. && pwd)
 echo "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-echo ${BASH_SOURCE[0]}
-echo $0
+echo $(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 echo $REPO_ROOT_DIR
+ls -lthra ..
+ls -lthra $(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 echo "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 # backup
 #dbus-launch dconf dump /org/gnome/terminal/ > gnome-terminal.conf
