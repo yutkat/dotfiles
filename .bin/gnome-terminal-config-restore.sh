b89653bc2f5bc31bea0dbb3ce3b056822ba427ba
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2017-01-11 00:43:52 +0900

    Fix source -zsh. solved bash_source

--- .bin/gnome-terminal-config-restore.sh
+++ .bin/gnome-terminal-config-restore.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-REPO_ROOT_DIR=$(cd $(dirname $0)/.. && pwd)
+set -ue
 
+REPO_ROOT_DIR=$(cd $(dirname "${BASH_SOURCE:-$0}")/.. && pwd)
 # backup
-#dconf dump /org/gnome/terminal/ > gnome-terminal.conf
+#dbus-launch dconf dump /org/gnome/terminal/ > gnome-terminal.conf
 
 # restore
-dconf load /org/gnome/terminal/ < ${REPO_ROOT_DIR}/.i3/app-config//gnome-terminal.conf
+dbus-launch dconf load /org/gnome/terminal/ < ${REPO_ROOT_DIR}/.i3/app-config/gnome-terminal.conf
 
