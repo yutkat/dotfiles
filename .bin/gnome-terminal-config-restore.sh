d4003db6e87f06ee51bb46fe14ab7ecd91ad4774
Yuta Katayama <yutakatay@gmail.com>
2017-01-11 01:48:29 +0900

    Fix wercker build bug

--- .bin/gnome-terminal-config-restore.sh
+++ .bin/gnome-terminal-config-restore.sh
@@ -3,12 +3,6 @@
 set -ue
 
 REPO_ROOT_DIR=$(builtin cd $(dirname "${BASH_SOURCE[0]:-$0}")/.. && pwd)
-echo "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-echo $(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
-echo $REPO_ROOT_DIR
-ls -lthra ..
-ls -lthra $(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
-echo "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 # backup
 #dbus-launch dconf dump /org/gnome/terminal/ > gnome-terminal.conf
 
