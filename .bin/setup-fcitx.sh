85109f8f11a0531e20ee024e6695f10ee9852803
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2017-08-14 11:34:00 +0900

    Add fcitx setup script

---
+++ .bin/setup-fcitx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -f ~/.xprofile ]; then
+  echo "ERROR: ~/.xprofile is exist"
+  exit 1
+fi
+
+{
+echo export GTK_IM_MODULE=fcitx
+echo export QT_IM_MODULE=fcitx
+echo export XMODIFIERS=@im=fcitx
+echo fcitx
+} >> ~/.xprofile
+
