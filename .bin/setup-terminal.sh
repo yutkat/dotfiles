ec0a122349d703948a29f4468f9c6f0d5bd3520b
Yuta Katayama <yutakatay@gmail.com>
2017-09-05 20:39:30 +0900

    Divide files

---
+++ .bin/setup-terminal.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -ue
+
+source $(dirname "${BASH_SOURCE[0]:-$0}")/utilfuncs.sh
+
+setup_gnome_terminal_config() {
+  if type gnome-terminal > /dev/null 2>&1;then
+    if type dbus-launch > /dev/null 2>&1;then
+      if type gsettings > /dev/null 2>&1;then
+        $(dirname "${BASH_SOURCE[0]:-$0}")/gnome-terminal-config-restore.sh
+      fi
+    fi
+  fi
+}
+
+setup_urxvt() {
+  local distro=`whichdistro`
+  if [[ $distro == "debian" ]];then
+    sudo apt-get install -y rxvt-unicode-256color x11-xserver-utils
+  elif [[ $distro == "redhat" ]];then
+    sudo yum install -y rxvt-unicode-256color
+  elif [[ $distro == "arch" ]];then
+    sudo pacman -S --noconfirm rxvt-unicode urxvt-perls
+  fi
+  xrdb -remove && xrdb -merge ~/.Xresources || true
+}
+
+setup_urxvt
