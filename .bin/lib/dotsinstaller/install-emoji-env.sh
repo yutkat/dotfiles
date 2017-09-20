b70c9edd339152b8a4aa4ff57a85c92779dc5f7d
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2017-09-21 07:29:25 +0900

    Add local bin directory

--- .bin/lib/dotsinstaller/install-emoji-env.sh
+++ .bin/lib/dotsinstaller/install-emoji-env.sh
@@ -5,6 +5,6 @@ set -ue
 source $(dirname "${BASH_SOURCE[0]:-$0}")/utilfuncs.sh
 
 
-curl https://raw.githubusercontent.com/mrowa44/emojify/master/emojify -o $HOME/.bin/emojify && chmod +x $HOME/.bin/emojify
+curl https://raw.githubusercontent.com/mrowa44/emojify/master/emojify -o $HOME/.bin.local/emojify && chmod +x $HOME/.bin.local/emojify
 
 checkinstall jq
