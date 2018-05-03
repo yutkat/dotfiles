62f1600e569e40f727817b4b565bdcbb04c39075
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2018-05-03 16:36:36 +0900

    Delete duplicate settings

--- .bin/lib/dotsinstaller/setup-fcitx.sh
+++ .bin/lib/dotsinstaller/setup-fcitx.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [ -f ~/.xprofile ]; then
-  echo "ERROR: ~/.xprofile is exist"
-  exit 1
-fi
-
-{
-echo export GTK_IM_MODULE=fcitx
-echo export QT_IM_MODULE=fcitx
-echo export XMODIFIERS=@im=fcitx
-echo fcitx
-} >> ~/.xprofile
+#if [ -f ~/.xprofile ]; then
+#  echo "ERROR: ~/.xprofile is exist"
+#  exit 1
+#fi
+#
+#{
+#echo export GTK_IM_MODULE=fcitx
+#echo export QT_IM_MODULE=fcitx
+#echo export XMODIFIERS=@im=fcitx
+#echo fcitx
+#} >> ~/.xprofile
 
 
 sed -i -e 's/#InactivateKey=/InactivateKey=ESCAPE CTRL_[/g' ~/.config/fcitx/config
