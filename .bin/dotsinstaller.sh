4ad4630bd206a08a93d3a6bbc0940bb175625c5b
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2018-07-05 10:08:06 +0900

    Add neovim

--- .bin/dotsinstaller.sh
+++ .bin/dotsinstaller.sh
@@ -121,7 +121,7 @@ fi
 
 
 if [[ "$IS_UPDATE" = true ]];then
-  checkinstall zsh git vim tmux ctags bc curl xsel gawk
+  checkinstall zsh git vim neovim tmux ctags bc curl xsel gawk
   if [[ "$NO_GUI" = false ]];then
     source $current_dir/lib/dotsinstaller/install-i3.sh
     source $current_dir/lib/dotsinstaller/setup-terminal.sh
