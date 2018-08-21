f6a20c47fb4224175996df2300b9c83b4721cb61
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2018-08-21 23:21:03 +0900

    Fix installer bug

--- .bin/lib/arch-extra-setup/utils.sh
+++ .bin/lib/arch-extra-setup/utils.sh
@@ -4,7 +4,7 @@ sudo pacman -S --noconfirm ripgrep mlocate urxvt-perls
 if [ ! -d /tmp/yay ]; then
   (cd /tmp && git clone https://aur.archlinux.org/yay.git)
 fi
-(cd /tmp/yay && makepkg -si)
+(cd /tmp/yay && makepkg -si && yay -Syy)
 
 yay -S i3-easyfocus-git clipmenu urxvt-resize-font-git
 
