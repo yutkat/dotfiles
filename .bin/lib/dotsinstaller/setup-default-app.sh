10e2e445a3b84bf397e16a802e53f711483e80cd
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2018-08-26 17:51:10 +0900

    Fix no commnad

--- .bin/lib/dotsinstaller/setup-default-app.sh
+++ .bin/lib/dotsinstaller/setup-default-app.sh
@@ -7,7 +7,7 @@ set_default_app() {
   local dist=$2
   for target in $list;do
     local target_path=$(which $target 2> /dev/null)
-    if [ $? -eq 0 ];then
+    if [[ $? -eq 0 && -n "$target_path" ]];then
       ln -snf $target_path $HOME/.bin.local/$dist
       break
     fi
