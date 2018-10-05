06ce43466f56a4f88efef3fe7dcd8f869baf00e2
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2018-10-05 12:00:00 +0900

    Add to connect display

---
+++ .i3/scripts/detect_displays.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+XRANDR="xrandr"
+CMD="${XRANDR}"
+declare -A VOUTS
+eval VOUTS=$(${XRANDR}|awk 'BEGIN {printf("(")} /^\S.*connected/{printf("[%s]=%s ", $1, $2)} END{printf(")")}')
+declare -A POS
+#XPOS=0
+#YPOS=0
+#POS="${XPOS}x${YPOS}"
+
+POS=([X]=0 [Y]=0)
+
+find_mode() {
+  echo $(${XRANDR} |grep ${1} -A1|awk '{FS="[ x]"} /^\s/{printf("WIDTH=%s\nHEIGHT=%s", $4,$5)}')
+}
+
+
+xrandr_params_for() {
+  if [ "${2}" == 'connected' ]; then
+    # eval $(find_mode ${1})  #sets ${WIDTH} and ${HEIGHT}
+    # MODE="${WIDTH}x${HEIGHT}"
+    # CMD="${CMD} --output ${1} --mode ${MODE} --pos ${POS[X]}x${POS[Y]}"
+    # POS[X]=$((${POS[X]}+${WIDTH}))
+    CMD="${CMD} --output ${1} --auto $3"
+    return 0
+  else
+    CMD="${CMD} --output ${1} --off"
+    return 1
+  fi
+}
+
+# for VOUT in ${!VOUTS[*]}; do
+#   xrandr_params_for ${VOUT} ${VOUTS[${VOUT}]} $OPTION
+# done
+OPTION=""
+for VOUT in ${!VOUTS[*]}; do
+  if xrandr_params_for ${VOUT} ${VOUTS[${VOUT}]} "$OPTION"; then
+    OPTION="--left-of ${VOUT}"
+  fi
+done
+set -x
+${CMD}
+set +x
