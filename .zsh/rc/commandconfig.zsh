2f3136f765d1e422e6575683c3dff0d294ab7b49
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2018-10-12 10:57:50 +0900

    Add pip completion

--- .zsh/rc/commandconfig.zsh
+++ .zsh/rc/commandconfig.zsh
@@ -58,3 +58,21 @@ if existsCommand fzf; then
 fi
 
 
+#==============================================================#
+## terraform completion
+#==============================================================#
+
+if existsCommand pip; then
+  # pip zsh completion start
+  function _pip_completion {
+    local words cword
+    read -Ac words
+    read -cn cword
+    reply=( $( COMP_WORDS="$words[*]" \
+               COMP_CWORD=$(( cword-1 )) \
+               PIP_AUTO_COMPLETE=1 $words[1] ) )
+  }
+  compctl -K _pip_completion pip
+  # pip zsh completion end
+fi
+
