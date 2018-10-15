2f5cfe726bdc590f5c73d2d8e826e05f60d749a7
Yuta Katayama <yutakatay@gmail.com>
2018-10-15 12:41:50 +0900

    Add bluetooth

--- .bin/lib/arch-extra-setup/laptop.sh
+++ .bin/lib/arch-extra-setup/laptop.sh
@@ -3,3 +3,6 @@ sudo pacman -S --noconfirm xf86-input-synaptics tpacpi-bat acpi_call urxvt-perls
 sudo pacman -S --noconfirm tlp acpi
 sudo systemctl enable tlp
 sudo pacman -S --noconfirm networkmanager network-manager-applet
+sudo pacman -S --noconfirm blueberry
+sudo systemctl start bluetooth
+sudo systemctl enable bluetooth
