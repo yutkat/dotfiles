7990638d88e788bec18a65199c483ae34c94ca2a
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2018-10-19 02:10:57 +0900

    Add install option

--- .bin/arch-extra-setup.sh
+++ .bin/arch-extra-setup.sh
@@ -3,6 +3,46 @@
 set -ue
 
 current_dir=$(dirname "${BASH_SOURCE[0]:-$0}")
+source $current_dir/lib/dotsinstaller/utilfuncs.sh
+
+helpmsg() {
+  print_default "Usage: "${BASH_SOURCE[0]:-$0}" [--extra] [--multi-display] [--security] [--all] [--help | -h]" 0>&2
+  print_default '  --all: --extra + --multi-display + --security'
+  print_default ""
+}
+
+EXTRA="false"
+MULTI_DISPLAY="false"
+SECURITY="false"
+
+while [ $# -gt 0 ];do
+  case ${1} in
+    --debug|-d)
+      set -uex
+      ;;
+    --help|-h)
+      helpmsg
+      exit 1
+      ;;
+    --extra)
+      EXTRA="false"
+      ;;
+    --multi-display)
+      MULTI_DISPLAY="false"
+      ;;
+    --security)
+      SECURITY="false"
+      ;;
+    --all)
+      EXTRA="false"
+      MULTI_DISPLAY="false"
+      SECURITY="false"
+      ;;
+    *)
+      ;;
+  esac
+  shift
+done
 
 source $current_dir/lib/arch-extra-setup/utils.sh
 source $current_dir/lib/arch-extra-setup/development.sh
@@ -11,8 +51,15 @@ if [ -f /sys/module/battery/initstate ] || [ -d /proc/acpi/battery/BAT0 ]; then
   source $current_dir/lib/arch-extra-setup/laptop.sh
 fi
 
-if [[ $1 -eq "--extra" ]]; then
+if [[ "$EXTRA" = true ]];then
   source $current_dir/lib/arch-extra-setup/apps.sh
 fi
 
+if [[ "$MULTI_DISPLAY" = true ]];then
+  source $current_dir/lib/arch-extra-setup/multi-display.sh
+fi
+
+if [[ "$SECURITY" = true ]];then
+  source $current_dir/lib/arch-extra-setup/security.sh
+fi
 
