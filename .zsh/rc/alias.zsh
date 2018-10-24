1fd3893baf06962bc8b040e6b30bafb9752f252d
Yuta Katayama <yutakatay@gmail.com>
2018-10-24 18:11:48 +0900

    Add pacman's alias

--- .zsh/rc/alias.zsh
+++ .zsh/rc/alias.zsh
@@ -122,3 +122,32 @@ alias-improve hdu "ncdu --color dark -rr -x --exclude .git --exclude node_module
 alias-improve help tldr
 
 
+#==============================================================#
+##          ArchLinux                                         ##
+#==============================================================#
+
+if [ -f /etc/arch-release ] ;then
+  # install
+  alias pac-update="sudo pacman -Sy"
+  alias pac-upgrade="sudo pacman -Syu"
+  alias pac-upgrade-force="sudo pacman -Syyu"
+  alias pac-install="sudo pacman -S"
+  alias pac-remove="sudo pacman -Rs"
+  # search remote package
+  alias pac-search="sudo pacman -Ss"
+  alias pac-package-info='sudo pacman -Si'
+  # search local package
+  alias pac-installed-list="sudo pacman -Qs"
+  alias pac-installed-package-info="sudo pacman -Qi"
+  alias pac-installed-files="sudo pacman -Ql"
+  # search package from filename
+  alias pac-included-files="sudo pacman -Fl"
+  alias pac-search-by-filename="sudo pkgfile"
+  # log
+  alias pac-log="cat /var/log/sudo pacman.log | \grep 'installed\|removed\|upgraded'"
+  alias pac-aur-packages="sudo pacman -Qm"
+  # etc
+  alias pac-clean="sudo pacman -Sc"
+fi
+
+
