835dfca2d08d258a324a468839206405b643ade7
Yuta Katayama <yutakatay@gmail.com>
2018-11-07 11:27:14 +0900

    Apply fedora

--- .bin/lib/dotsinstaller/install-basic-packages.sh
+++ .bin/lib/dotsinstaller/install-basic-packages.sh
@@ -5,5 +5,11 @@ set -ue
 source $(dirname "${BASH_SOURCE[0]:-$0}")/utilfuncs.sh
 
 checkinstall zsh git vim neovim tmux ctags bc curl xsel gawk python-pip unzip sqlite
-sudo pip install neovim
+
+local distro=`whichdistro`
+if [[ $distro == "redhat" ]];then
+  sudo python3 -m pip install neovim
+else
+  sudo pip install neovim
+fi
 
