c6b190dfb7516401ccfc215bc10b63572ee73324
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2018-11-20 15:56:08 +0900

    Add ntp

--- .bin/lib/arch-extra-setup/equipment.sh
+++ .bin/lib/arch-extra-setup/equipment.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo pacman -S --noconfirm cups cups-pdf bind-tools
+sudo pacman -S --noconfirm cups cups-pdf bind-tools ntp
+sudo systemd enable ntpd.service
+sudo systemd start ntpd.service
 
