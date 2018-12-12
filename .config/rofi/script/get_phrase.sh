4e3e2c900ea7b6ee7c2339efdae33c5fc52ed1c7
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2018-12-12 12:35:55 +0900

    Support xclip

--- .config/rofi/script/get_phrase.sh
+++ .config/rofi/script/get_phrase.sh
@@ -7,6 +7,10 @@ fi
 if [ -z "$@" ]; then
   cat ~/.cache/rofi/phrase.txt
 else
-  echo $@ | tr -d '\n' | xsel -i -b
+  if builtin command -v xsel > /dev/null 2>&1; then
+    echo $@ | tr -d '\n' | xsel -i -b
+  elif builtin command -v xclip > /dev/null 2>&1; then
+    echo $@ | tr -d '\n' | xclip -i -selection clipboard
+  fi
 fi
 
