ac801ba396afb533fc032d6916787d5811cb6e3c
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2019-01-09 15:59:27 +0900

    Add gadd command

--- .zsh/rc/commandconfig.zsh
+++ .zsh/rc/commandconfig.zsh
@@ -85,6 +85,15 @@ if existsCommand fzf; then
   zle     -N   fzf-git-files-widget
   bindkey '^B' fzf-git-files-widget
 
+  function gadd() {
+    local selected
+    selected=$(unbuffer git status -s | fzf -m --ansi --preview="echo {} | awk '{print \$2}' | xargs git diff --color" | awk '{print $2}')
+    if [[ -n "$selected" ]]; then
+      selected=$(tr '\n' ' ' <<< "$selected")
+      git add $selected
+      echo "Completed: git add $selected"
+    fi
+  }
 fi
 
 
