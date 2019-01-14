83a1ee0056f6112aad959f1b368946b368393e2f
Yuta Katayama <yutakatay@gmail.com>
2019-01-14 15:32:28 +0900

    Add fcitx config

--- .bin/lib/dotsinstaller/setup-fcitx.sh
+++ .bin/lib/dotsinstaller/setup-fcitx.sh
@@ -14,10 +14,10 @@ source $(dirname "${BASH_SOURCE[0]:-$0}")/utilfuncs.sh
 #echo fcitx
 #} >> ~/.xprofile
 
-fcitx > /dev/null 2>&1 || true
-if [ -f ~/.config/fcitx/config ]; then
-  sed -i -e 's/#InactivateKey=/InactivateKey=ESCAPE CTRL_[/g' ~/.config/fcitx/config
-else
-  print_error "Please execute fcitx"
-  exit 1
-fi
+# fcitx > /dev/null 2>&1 || true
+# if [ -f ~/.config/fcitx/config ]; then
+#   sed -i -e 's/#InactivateKey=/InactivateKey=ESCAPE CTRL_[/g' ~/.config/fcitx/config
+# else
+#   print_error "Please execute fcitx"
+#   exit 1
+# fi
