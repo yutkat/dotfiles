11b6ed791a1ec824b0f480c72eb7e9dbb741b727
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2019-01-30 09:48:48 +0900

    Add split tab

---
+++ .i3/scripts/open_split_tab.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+layout=$(i3-msg -t get_tree | jq -r 'recurse(.nodes[]) | select(.nodes[].focused == true) | .layout')
+
+case "$layout" in
+  tabbed) : ;;
+  *) i3-msg "split horizontal; layout tabbed" ;;
+esac
+
+
+i3-msg exec $1
+
