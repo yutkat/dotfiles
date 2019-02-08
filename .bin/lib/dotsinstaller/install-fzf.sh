750816f7df67c254e27ef31c250863653c7ac52a
Yuta Katayama <yutakatay@gmail.com>
2019-02-08 11:15:54 +0900

    Delete unused function

--- .bin/lib/dotsinstaller/install-fzf.sh
+++ .bin/lib/dotsinstaller/install-fzf.sh
@@ -4,20 +4,11 @@ set -ue
 
 source $(dirname "${BASH_SOURCE[0]:-$0}")/utilfuncs.sh
 
-function install_fzf_local() {
-  echo "Installing fzf local..."
-  echo ""
-  source $(dirname "${BASH_SOURCE[0]:-$0}")/install-golang.sh
-  builtin cd $fzf_dir && command make install && \
-    echo "export PATH=$PATH:$fzf_dir/bin" > $HOME/.fzf.zsh
-}
-
 function install_fzf() {
   local fzf_dir="$HOME/.fzf"
   git_clone_or_fetch https://github.com/junegunn/fzf.git \
     $fzf_dir
-  $fzf_dir/install --key-bindings --no-completion  --no-update-rc || \
-    install_fzf_local
+  $fzf_dir/install --key-bindings --no-completion  --no-update-rc
 }
 
 install_fzf
