dfd4af72b27a8f82e7d7351df77fdf57f73adfc3
Yuta Katayama <yutakatay@gmail.com>
2019-02-19 15:06:37 +0900

    Add google-chrome

--- .bin/lib/dotsinstaller/setup-default-app.sh
+++ .bin/lib/dotsinstaller/setup-default-app.sh
@@ -6,7 +6,12 @@ function set_default_app() {
   local list=$1
   local dist=$2
   for target in $list;do
-    local target_path=$(which $target 2> /dev/null)
+    local target_path=$(command -v $target 2> /dev/null)
+    if [[ $? -eq 0 && -n "$target_path" ]];then
+      ln -snf $target_path $HOME/.bin.local/$dist
+      break
+    fi
+    local target_path=$(compgen -c | \grep $target | sort | head -n 1 | xargs which 2> /dev/null)
     if [[ $? -eq 0 && -n "$target_path" ]];then
       ln -snf $target_path $HOME/.bin.local/$dist
       break
@@ -14,6 +19,6 @@ function set_default_app() {
   done
 }
 
-set_default_app "chromium firefox" x-www-browser
+set_default_app "google-chrome chromium firefox" x-www-browser
 set_default_app "alacritty urxvt gnome-terminal" x-terminal-emulator
 
