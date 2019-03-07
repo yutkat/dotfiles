7bd301ea2ed68d2b327267e93c0a8df23cab10dd
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2019-03-08 08:20:12 +0900

    Update shellscript style

--- .bin/dotsinstaller.sh
+++ .bin/dotsinstaller.sh
@@ -2,8 +2,8 @@
 
 set -ue
 
-current_dir=$(dirname "${BASH_SOURCE[0]:-$0}")
-source $current_dir/lib/dotsinstaller/utilfuncs.sh
+CURRENT_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
+source $CURRENT_DIR/lib/dotsinstaller/utilfuncs.sh
 
 
 #--------------------------------------------------------------#
@@ -23,85 +23,94 @@ function helpmsg() {
 ##          main                                              ##
 #--------------------------------------------------------------#
 
-IS_INSTALL="false"
-IS_UPDATE="true"
-NO_GUI="false"
-EMOJI="false"
-EXTRA="false"
 
-while [ $# -gt 0 ];do
-  case ${1} in
-    --debug|-d)
-      set -uex
-      ;;
-    --help|-h)
-      helpmsg
-      exit 1
-      ;;
-    install)
-      IS_INSTALL="true"
-      ;;
-    update)
-      IS_UPDATE="true"
-      ;;
-    --no-gui)
-      NO_GUI="true"
-      ;;
-    --emoji)
-      EMOJI="true"
-      ;;
-    --extra)
-      EXTRA="true"
-      ;;
-    --all)
-      EMOJI="true"
-      EXTRA="true"
-      ;;
-    *)
-      ;;
-  esac
-  shift
-done
+function main() {
+  local is_install="false"
+  local is_update="true"
+  local no_gui="false"
+  local emoji="false"
+  local extra="false"
 
-
-if [[ ! -e "$HOME/.bin/"$(basename "${BASH_SOURCE[0]:-$0}") ]]; then
-  IS_INSTALL=true
-fi
-
-
-if [[ "$IS_INSTALL" = true ]];then
-  source $current_dir/lib/dotsinstaller/install-required-packages.sh
-  source $current_dir/lib/dotsinstaller/link-to-homedir.sh
-  source $current_dir/lib/dotsinstaller/gitconfig.sh
-  print_info ""
-  print_info "#####################################################"
-  print_info "$(basename "${BASH_SOURCE[0]:-$0}") install success!!!"
-  print_info "#####################################################"
-  print_info ""
-fi
+  while [ $# -gt 0 ];do
+    case ${1} in
+      --help|-h)
+        helpmsg
+        exit 1
+        ;;
+      install)
+        is_install="true"
+        ;;
+      update)
+        is_update="true"
+        ;;
+      --no-gui)
+        no_gui="true"
+        ;;
+      --emoji)
+        emoji="true"
+        ;;
+      --extra)
+        extra="true"
+        ;;
+      --all)
+        emoji="true"
+        extra="true"
+        ;;
+      --verbose|--debug)
+        set -x; shift
+        ;;
+      *)
+        echo "[ERROR] Invalid arguments '${1}'"
+        usage
+        exit 1
+        ;;
+    esac
+    shift
+  done
 
 
-if [[ "$IS_UPDATE" = true ]];then
-  source $current_dir/lib/dotsinstaller/install-basic-packages.sh
-  source $current_dir/lib/dotsinstaller/install-fzf.sh
-
-  if [[ "$EMOJI" = true ]];then
-    source $current_dir/lib/dotsinstaller/install-emoji-env.sh
+  if [[ ! -e "$HOME/.bin/"$(basename "${BASH_SOURCE[0]:-$0}") ]]; then
+    is_install=true
   fi
 
-  if [[ "$EXTRA" = true ]];then
-    source $current_dir/lib/dotsinstaller/install-extra.sh
+
+  if [[ "$is_install" = true ]];then
+    source $CURRENT_DIR/lib/dotsinstaller/install-required-packages.sh
+    source $CURRENT_DIR/lib/dotsinstaller/link-to-homedir.sh
+    source $CURRENT_DIR/lib/dotsinstaller/gitconfig.sh
+    print_info ""
+    print_info "#####################################################"
+    print_info "$(basename "${BASH_SOURCE[0]:-$0}") install success!!!"
+    print_info "#####################################################"
+    print_info ""
   fi
 
-  if [[ "$NO_GUI" = false ]];then
-    source $current_dir/lib/dotsinstaller/setup-terminal.sh
-    source $current_dir/lib/dotsinstaller/install-i3.sh
-  fi
 
-  print_info ""
-  print_info "#####################################################"
-  print_info "$(basename "${BASH_SOURCE[0]:-$0}") update finish!!!"
-  print_info "#####################################################"
-  print_info ""
-fi
+  if [[ "$is_update" = true ]];then
+    source $CURRENT_DIR/lib/dotsinstaller/install-basic-packages.sh
+    source $CURRENT_DIR/lib/dotsinstaller/install-fzf.sh
+
+    if [[ "$emoji" = true ]];then
+      source $CURRENT_DIR/lib/dotsinstaller/install-emoji-env.sh
+    fi
+
+    if [[ "$extra" = true ]];then
+      source $CURRENT_DIR/lib/dotsinstaller/install-extra.sh
+    fi
+
+    if [[ "$no_gui" = false ]];then
+      source $CURRENT_DIR/lib/dotsinstaller/setup-terminal.sh
+      source $CURRENT_DIR/lib/dotsinstaller/install-i3.sh
+    fi
+
+    print_info ""
+    print_info "#####################################################"
+    print_info "$(basename "${BASH_SOURCE[0]:-$0}") update finish!!!"
+    print_info "#####################################################"
+    print_info ""
+  fi
+}
+
+
+main "$@"
 
