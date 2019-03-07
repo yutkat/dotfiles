a76b8f14a2cbd410a10229d6001fbe913b25179c
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2019-03-08 08:28:29 +0900

    Update shellscript code style

--- .bin/arch-extra-setup.sh
+++ .bin/arch-extra-setup.sh
@@ -2,8 +2,8 @@
 
 set -ue
 
-current_dir=$(dirname "${BASH_SOURCE[0]:-$0}")
-source $current_dir/lib/dotsinstaller/utilfuncs.sh
+CURRENT_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
+source $CURRENT_DIR/lib/dotsinstaller/utilfuncs.sh
 
 function helpmsg() {
   print_default "Usage: "${BASH_SOURCE[0]:-$0}" [--extra] [--multi-display] [--security] [--all] [--help | -h]" 0>&2
@@ -11,56 +11,60 @@ function helpmsg() {
   print_default ""
 }
 
-EXTRA="false"
-MULTI_DISPLAY="false"
-SECURITY="false"
+function main() {
+  local extra="false"
+  local multi_display="false"
+  local security="false"
 
-while [ $# -gt 0 ];do
-  case ${1} in
-    --debug|-d)
-      set -uex
-      ;;
-    --help|-h)
-      helpmsg
-      exit 1
-      ;;
-    --extra)
-      EXTRA="false"
-      ;;
-    --multi-display)
-      MULTI_DISPLAY="false"
-      ;;
-    --security)
-      SECURITY="false"
-      ;;
-    --all)
-      EXTRA="true"
-      MULTI_DISPLAY="true"
-      SECURITY="true"
-      ;;
-    *)
-      ;;
-  esac
-  shift
-done
+  while [ $# -gt 0 ];do
+    case ${1} in
+      --debug|-d)
+        set -uex
+        ;;
+      --help|-h)
+        helpmsg
+        exit 1
+        ;;
+      --extra)
+        extra="false"
+        ;;
+      --multi-display)
+        multi_display="false"
+        ;;
+      --security)
+        security="false"
+        ;;
+      --all)
+        extra="true"
+        multi_display="true"
+        security="true"
+        ;;
+      *)
+        ;;
+    esac
+    shift
+  done
 
-source $current_dir/lib/arch-extra-setup/utils.sh
-source $current_dir/lib/arch-extra-setup/development.sh
+  source $CURRENT_DIR/lib/arch-extra-setup/utils.sh
+  source $CURRENT_DIR/lib/arch-extra-setup/development.sh
 
-if [ -f /sys/module/battery/initstate ] || [ -d /proc/acpi/battery/BAT0 ]; then
-  source $current_dir/lib/arch-extra-setup/laptop.sh
-fi
+  if [ -f /sys/module/battery/initstate ] || [ -d /proc/acpi/battery/BAT0 ]; then
+    source $CURRENT_DIR/lib/arch-extra-setup/laptop.sh
+  fi
 
-if [[ "$EXTRA" = true ]];then
-  source $current_dir/lib/arch-extra-setup/apps.sh
-  source $current_dir/lib/arch-extra-setup/equipment.sh
-fi
+  if [[ "$extra" = true ]];then
+    source $CURRENT_DIR/lib/arch-extra-setup/apps.sh
+    source $CURRENT_DIR/lib/arch-extra-setup/equipment.sh
+  fi
 
-if [[ "$MULTI_DISPLAY" = true ]];then
-  source $current_dir/lib/arch-extra-setup/multi-display.sh
-fi
+  if [[ "$multi_display" = true ]];then
+    source $CURRENT_DIR/lib/arch-extra-setup/multi-display.sh
+  fi
 
-if [[ "$SECURITY" = true ]];then
-  source $current_dir/lib/arch-extra-setup/security.sh
-fi
+  if [[ "$security" = true ]];then
+    source $CURRENT_DIR/lib/arch-extra-setup/security.sh
+  fi
+}
+
+main "$@"
 
