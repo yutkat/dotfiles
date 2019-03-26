7bce365de17e94dd1e7a520be15c9039a7b464ac
Yuta Katayama <yutakatay@gmail.com>
2019-03-26 14:39:05 +0900

    Update systemd config

--- .bin/lib/arch-extra-setup/security.sh
+++ .bin/lib/arch-extra-setup/security.sh
@@ -2,21 +2,7 @@
 
 function setting_lock() {
   local s_file="/etc/systemd/system/i3lock@$(whoami).service"
-  sudo tee $s_file << "EOF" > /dev/null
-[Unit]
-Description=i3lock
-Before=sleep.target
-
-[Service]
-User=%I
-Type=forking
-Environment=DISPLAY=:0
-ExecStart=/usr/bin/i3lock -c 282C34
-
-[Install]
-WantedBy=sleep.target
-EOF
-
+  cat $HOME/.i3/systemd/i3lock@user.service | envsubst | sudo tee $s_file > /dev/null
   sudo systemctl enable $(basename $s_file)
 }
 
