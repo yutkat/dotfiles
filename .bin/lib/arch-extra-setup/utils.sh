84bc45fbcee271cd164949f4743c666d614c7f42
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2019-05-15 15:01:42 +0900

    Update neovim installer

--- .bin/lib/arch-extra-setup/utils.sh
+++ .bin/lib/arch-extra-setup/utils.sh
@@ -9,4 +9,9 @@ if ! builtin command -v yay > /dev/null 2>&1; then
 fi
 
 yay -S --noconfirm --needed i3-easyfocus-git clipmenu urxvt-resize-font-git light-git
+sudo pacman -Rs neovim || true
+if [[ -L /usr/local/bin/nvim ]]; then
+  sudo rm /usr/local/bin/nvim || true
+fi
+yay -S --noconfirm --needed neovim-nightly
 
