e9151046be3d121a3dac6e7be03e9697fa96d0da
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2019-09-06 11:39:36 +0900

    Fix udev config because display is slow when devices reconnected

--- .bin/lib/arch-extra-setup/udev/external-kbd.sh
+++ .bin/lib/arch-extra-setup/udev/external-kbd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ ! -e /usr/local/bin/auto_xmodmap.sh ]; then
-  sudo tee /usr/local/bin/auto_xmodmap.sh << "EOF" > /dev/null
+if [ ! -e /usr/local/bin/set_xmodmap.sh ]; then
+  sudo tee /usr/local/bin/set_xmodmap.sh << "EOF" > /dev/null
 #!/bin/bash
-sleep 2
+sleep 1
 
 DISPLAY=":0"
 HOME=/home/kata
@@ -11,13 +11,22 @@ XAUTHORITY=$HOME/.Xauthority
 export DISPLAY HOME XAUTHORITY
 
 setxkbmap
-nohup xmodmap $HOME/.Xmodmap
+xmodmap $HOME/.Xmodmap
+EOF
+  sudo chmod +x /usr/local/bin/set_xmodmap.sh
+fi
+
+if [ ! -e /usr/local/bin/auto_xmodmap.sh ]; then
+  sudo tee /usr/local/bin/auto_xmodmap.sh << "EOF" > /dev/null
+#!/bin/bash
+
+nohup /usr/local/bin/set_xmodmap.sh >/dev/null 2>&1 &
 EOF
   sudo chmod +x /usr/local/bin/auto_xmodmap.sh
 fi
 
-line='SUBSYSTEM==\"input\", ACTION==\"add\|remove\", RUN+=\"/usr/local/bin/auto_xmodmap.sh\"'
+line='SUBSYSTEM==\"input\", KERNEL=\"input*\", ATTRS{phys}==\"usb*input*\", ACTION==\"add\|remove\", RUN+=\"/usr/local/bin/auto_xmodmap.sh\"'
 sudo bash -c "echo $line > /etc/udev/rules.d/99-keyboard-hotplug.rules"
 
-
+sudo udevadm control --reload-rules;
 
