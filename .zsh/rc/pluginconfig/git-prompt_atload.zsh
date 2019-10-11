17402fcd1733020495b8fa7dcad701b23c4c78ae
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2019-10-11 21:53:36 +0900

    Update zsh prompt

---
+++ .zsh/rc/pluginconfig/git-prompt_atload.zsh
@@ -0,0 +1,19 @@
+
+PROMPT='[%n@%m:%.$(gitprompt)]${WINDOW:+"[$WINDOW]"}$(__show_status)%# '
+
+ZSH_THEME_GIT_PROMPT_PREFIX="("
+ZSH_THEME_GIT_PROMPT_SUFFIX=")"
+ZSH_THEME_GIT_PROMPT_SEPARATOR="|"
+ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg[cyan]%}"
+ZSH_THEME_GIT_PROMPT_STAGED="%{$fg_bold[yellow]%}%{+%G%}"
+ZSH_THEME_GIT_PROMPT_CONFLICTS="%{$fg_bold[magenta]%}%{!%G%}"
+ZSH_THEME_GIT_PROMPT_CHANGED="%{$fg_bold[red]%}%{*%G%}"
+ZSH_THEME_GIT_PROMPT_BEHIND="%{$fg_bold[blue]%}%{<%G%}"
+ZSH_THEME_GIT_PROMPT_AHEAD="%{$fg_bold[blue]%}%{>%G%}"
+ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg_bold[white]%}%{?%G%}"
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}%{/%G%}"
+ZSH_THEME_GIT_PROMPT_DETACHED="%{$fg_bold[cyan]%}:"
+ZSH_THEME_GIT_PROMPT_UNMERGED="%{$fg[red]%}%{X%G%}"
+ZSH_THEME_GIT_PROMPT_UNSTAGED="%{$fg[red]%}%{+%G%}"
+ZSH_THEME_GIT_PROMPT_STASHED="%{$fg[blue]%}%{#%G%}"
+
