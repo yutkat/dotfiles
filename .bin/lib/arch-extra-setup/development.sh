7e43cd47c6cb109d6914ea63e761d47c77ba74d2
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2019-11-11 16:09:27 +0900

    Change ghq install way from go get to yay

--- .bin/lib/arch-extra-setup/development.sh
+++ .bin/lib/arch-extra-setup/development.sh
@@ -2,4 +2,4 @@
 sudo pacman -S --noconfirm --needed rustup go clang gcc gdb make cmake
 sudo pacman -S --noconfirm --needed docker xorg-xhost jq
 
-go get github.com/motemen/ghq
+yay -S --noconfirm --needed ghq-bin
