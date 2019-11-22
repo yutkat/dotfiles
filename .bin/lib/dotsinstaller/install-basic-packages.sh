421ffa9cc75499ed1ed879e0675088e27527b57b
Yuta Katayama <yutakatay@gmail.com>
2019-11-22 18:01:52 +0900

    Add jq because of cica installer

--- .bin/lib/dotsinstaller/install-basic-packages.sh
+++ .bin/lib/dotsinstaller/install-basic-packages.sh
@@ -7,9 +7,9 @@ source $(dirname "${BASH_SOURCE[0]:-$0}")/utilfuncs.sh
 
 distro=`whichdistro`
 if [[ $distro == "redhat" ]];then
-  checkinstall zsh git vim tmux ctags bc curl wget gawk python3-pip unzip sqlite gettext procps
+  checkinstall zsh git vim tmux ctags bc curl wget gawk python3-pip unzip sqlite gettext procps jq
 else
-  checkinstall zsh git vim tmux ctags bc curl wget xsel gawk python-pip unzip sqlite gettext procps
+  checkinstall zsh git vim tmux ctags bc curl wget xsel gawk python-pip unzip sqlite gettext procps jq
 fi
 
 if [[ $distro == "redhat" ]];then
