1c3345501233c6896e15fa8c69ebab6021080124
Yuta Katayama <yutakatay@gmail.com>
2019-11-22 17:07:57 +0900

    Change default font from myrica to cica

--- .bin/lib/dotsinstaller/install-font.sh
+++ .bin/lib/dotsinstaller/install-font.sh
@@ -4,9 +4,16 @@ set -ue
 
 source $(dirname "${BASH_SOURCE[0]:-$0}")/utilfuncs.sh
 
-sudo mkdir -p /usr/share/fonts/myrica
-sudo curl -L https://github.com/tomokuni/Myrica/raw/master/product/MyricaM.zip -o /tmp/MyricaM.zip
-(cd /tmp && sudo unzip -o MyricaM.zip -d /usr/share/fonts/myrica)
+# myrica
+# sudo mkdir -p /usr/share/fonts/myrica
+# sudo curl -L https://github.com/tomokuni/Myrica/raw/master/product/MyricaM.zip -o /tmp/MyricaM.zip
+# (cd /tmp && sudo unzip -o MyricaM.zip -d /usr/share/fonts/myrica)
+
+# cica
+sudo mkdir -p /usr/share/fonts/cica
+CICA_RELEASES_URL="https://api.github.com/repos/miiton/Cica/releases"
+sudo curl -sfL "${CICA_RELEASES_URL}" | jq -r '.[0].assets | .[].browser_download_url' | grep with_emoji.zip | xargs -I{} curl -fL -o /tmp/Cica.zip "{}"
+(cd /tmp && sudo unzip -o Cica.zip -d /usr/share/fonts/cica)
 
 if [ ! -d $HOME/.config/fontconfig ]; then
   mkdir -p $HOME/.config/fontconfig
@@ -17,7 +24,7 @@ if [ ! -d $HOME/.config/fontconfig ]; then
 	<alias>
 		<family>monospace</family>
 		<prefer>
-			<family>MyricaM M</family>
+			<family>Cica</family>
 		</prefer>
 	</alias>
 </fontconfig>
