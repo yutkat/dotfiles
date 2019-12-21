4ba58a1cab5c1f4d9881d6d5e29003392e4e5685
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2019-12-21 20:02:27 +0900

    Add fontconfig

--- .bin/lib/dotsinstaller/install-font.sh
+++ .bin/lib/dotsinstaller/install-font.sh
@@ -15,21 +15,5 @@ CICA_RELEASES_URL="https://api.github.com/repos/miiton/Cica/releases"
 sudo curl -sfL "${CICA_RELEASES_URL}" | jq -r '.[0].assets | .[].browser_download_url' | grep with_emoji.zip | xargs -I{} curl -fL -o /tmp/Cica.zip "{}"
 (cd /tmp && sudo unzip -o Cica.zip -d /usr/share/fonts/cica)
 
-if [ ! -d $HOME/.config/fontconfig ]; then
-  mkdir -p $HOME/.config/fontconfig
-  cat > $HOME/.config/fontconfig/fonts.conf << "EOF"
-<?xml version="1.0"?>
-<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-<fontconfig>
-	<alias>
-		<family>monospace</family>
-		<prefer>
-			<family>Cica</family>
-		</prefer>
-	</alias>
-</fontconfig>
-EOF
-fi
-
 sudo fc-cache -vf
 
