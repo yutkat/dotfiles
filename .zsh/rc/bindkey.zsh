5ef0c1604140929c01e097601ceb7d5d53cca973
Yuta Katayama <yutakatay@users.noreply.github.com>
2019-12-27 19:46:12 +0900

    Change bindkey (#909)

--- .zsh/rc/bindkey.zsh
+++ .zsh/rc/bindkey.zsh
@@ -82,8 +82,8 @@ bindkey -M menuselect '^[[Z' reverse-menu-complete
 ## edit ##
 # copy command
 zle -N pbcopy-buffer
-bindkey '^X^P' pbcopy-buffer
-bindkey '^Xp' pbcopy-buffer
+bindkey '^X^Y' pbcopy-buffer
+bindkey '^Xy' pbcopy-buffer
 bindkey '^[u' undo
 bindkey '^[r' redo
 
