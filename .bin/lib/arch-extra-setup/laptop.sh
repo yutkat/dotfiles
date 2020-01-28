6ecb2c76b9762a85dab0b47638472f645f93e059
Yuta Katayama <yutakatay@gmail.com>
2020-01-28 22:21:56 +0900

    Change key swap tool to udev from xkb

--- .bin/lib/arch-extra-setup/laptop.sh
+++ .bin/lib/arch-extra-setup/laptop.sh
@@ -7,12 +7,3 @@ sudo pacman -S --noconfirm --needed blueberry
 sudo systemctl start bluetooth
 sudo systemctl enable bluetooth
 
-# use xkbcomp
-# function service-keymap() {
-#   local s_file="/etc/systemd/system/loadkeys.service"
-#   cat $HOME/.bin/lib/arch-extra-setup/systemd/loadkeys.service | envsubst | sudo tee $s_file > /dev/null
-#   sudo systemctl enable $(basename $s_file)
-# }
-#
-# service-keymap
-
