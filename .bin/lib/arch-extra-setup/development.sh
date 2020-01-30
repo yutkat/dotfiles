f069fbaea28c3fb2b892f8d618069989d04f9803
Yuta Katayama <yutakatay@gmail.com>
2020-01-30 12:02:30 +0900

    Fix ci

--- .bin/lib/arch-extra-setup/development.sh
+++ .bin/lib/arch-extra-setup/development.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+sudo pacman -R --noconfirm go || true
 sudo pacman -S --noconfirm --needed rustup go-pie clang gcc gdb make cmake
 sudo pacman -S --noconfirm --needed docker xorg-xhost jq
 
