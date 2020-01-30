ccfaf0603bae4d18214f7840bab5a8d432013666
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2020-01-30 11:41:05 +0900

    Update installer

--- .bin/lib/arch-extra-setup/development.sh
+++ .bin/lib/arch-extra-setup/development.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo pacman -S --noconfirm --needed rustup go clang gcc gdb make cmake
+sudo pacman -S --noconfirm --needed rustup go-pie clang gcc gdb make cmake
 sudo pacman -S --noconfirm --needed docker xorg-xhost jq
 
 yay -S --noconfirm --needed ghq-bin
