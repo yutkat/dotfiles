c3dd0ca4dce7156d5b73c8f1742304d5c9b34eda
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2020-01-30 17:05:03 +0900

    Rename zplugin to zinit

--- .zsh/rc/pluginlist.zsh
+++ .zsh/rc/pluginlist.zsh
@@ -1,23 +1,23 @@
 
 #==============================================================#
-## Setup zplugin                                              ##
+## Setup zinit                                              ##
 #==============================================================#
 
 if [ -z "$ZPLG_HOME" ]; then
-    ZPLG_HOME="${ZHOMEDIR:-$HOME}/zplugin"
+    ZPLG_HOME="${ZHOMEDIR:-$HOME}/zinit"
 fi
 
 if ! test -d "$ZPLG_HOME"; then
     mkdir "$ZPLG_HOME"
     chmod g-rwX "$ZPLG_HOME"
-    git clone --depth 10 https://github.com/zdharma/zplugin.git ${ZPLG_HOME}/bin
+    git clone --depth 10 https://github.com/zdharma/zinit.git ${ZPLG_HOME}/bin
 fi
 
 typeset -gAH ZPLGM
 ZPLGM[HOME_DIR]="${ZPLG_HOME}"
-source "$ZPLG_HOME/bin/zplugin.zsh"
-autoload -Uz _zplugin
-(( ${+_comps} )) && _comps[zplugin]=_zplugin
+source "$ZPLG_HOME/bin/zinit.zsh"
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
 
 
 #==============================================================#
@@ -29,102 +29,102 @@ autoload -Uz _zplugin
 #--------------------------------#
 git_version=$(git --version | head -n1 | cut -d" " -f3)
 if [[ "$(version3 "$git_version")" -ge "$(version3 "2.11.0")" ]]; then
-  zplugin ice atload"source $ZHOMEDIR/rc/pluginconfig/git-prompt_atload.zsh"
-  zplugin light 'woefe/git-prompt.zsh'
+  zinit ice atload"source $ZHOMEDIR/rc/pluginconfig/git-prompt_atload.zsh"
+  zinit light 'woefe/git-prompt.zsh'
 else
-  zplugin ice atload"source $ZHOMEDIR/rc/pluginconfig/zsh-git-prompt_atload.zsh"
-  zplugin light 'olivierverdier/zsh-git-prompt'
+  zinit ice atload"source $ZHOMEDIR/rc/pluginconfig/zsh-git-prompt_atload.zsh"
+  zinit light 'olivierverdier/zsh-git-prompt'
 fi
 
-zplugin ice wait'!0' lucid if"(( ${ZSH_VERSION%%.*} > 4.4))" atinit"zpcompinit; zpcdreplay"
-zplugin light 'zdharma/fast-syntax-highlighting'
+zinit ice wait'!0' lucid if"(( ${ZSH_VERSION%%.*} > 4.4))" atinit"zpcompinit; zpcdreplay"
+zinit light 'zdharma/fast-syntax-highlighting'
 
 #--------------------------------#
 # completion
 #--------------------------------#
-zplugin ice ver:pu atload"source $ZHOMEDIR/rc/pluginconfig/auto-fu.zsh_atload.zsh"
-zplugin light 'hchbaw/auto-fu.zsh'
+zinit ice ver:pu atload"source $ZHOMEDIR/rc/pluginconfig/auto-fu.zsh_atload.zsh"
+zinit light 'hchbaw/auto-fu.zsh'
 
-zplugin ice wait'!0' lucid as"completion" atload"source $ZHOMEDIR/rc/pluginconfig/zsh-completions_atload.zsh"
-zplugin light 'zsh-users/zsh-completions'
+zinit ice wait'!0' lucid as"completion" atload"source $ZHOMEDIR/rc/pluginconfig/zsh-completions_atload.zsh"
+zinit light 'zsh-users/zsh-completions'
 
 #--------------------------------#
 # history
 #--------------------------------#
-zplugin ice wait'!0' lucid if"(( ${ZSH_VERSION%%.*} > 4.4))"
-zplugin light 'zsh-users/zsh-history-substring-search'
+zinit ice wait'!0' lucid if"(( ${ZSH_VERSION%%.*} > 4.4))"
+zinit light 'zsh-users/zsh-history-substring-search'
 
-zplugin ice wait'!0' lucid
-zplugin light 'larkery/zsh-histdb'
+zinit ice wait'!0' lucid
+zinit light 'larkery/zsh-histdb'
 
 #--------------------------------#
 # alias
 #--------------------------------#
-zplugin ice wait'!0' lucid
-zplugin light 'unixorn/git-extra-commands'
+zinit ice wait'!0' lucid
+zinit light 'unixorn/git-extra-commands'
 
-zplugin ice wait'!0' lucid atload"source $ZHOMEDIR/rc/pluginconfig/zsh-abbrev-alias_atinit.zsh"
-zplugin light 'momo-lab/zsh-abbrev-alias'
+zinit ice wait'!0' lucid atload"source $ZHOMEDIR/rc/pluginconfig/zsh-abbrev-alias_atinit.zsh"
+zinit light 'momo-lab/zsh-abbrev-alias'
 
 #--------------------------------#
 # environment variable
 #--------------------------------#
-zplugin light 'Tarrasch/zsh-autoenv'
+zinit light 'Tarrasch/zsh-autoenv'
 
 #--------------------------------#
 # improve cd
 #--------------------------------#
-zplugin ice atinit"source $ZHOMEDIR/rc/pluginconfig/zsh-z_atinit.zsh"
-zplugin light 'agkozak/zsh-z'
+zinit ice atinit"source $ZHOMEDIR/rc/pluginconfig/zsh-z_atinit.zsh"
+zinit light 'agkozak/zsh-z'
 
-zplugin ice wait'!0' lucid
-zplugin light 'mollifier/cd-gitroot'
+zinit ice wait'!0' lucid
+zinit light 'mollifier/cd-gitroot'
 
-zplugin ice wait'!0' lucid
-zplugin light 'peterhurford/up.zsh'
+zinit ice wait'!0' lucid
+zinit light 'peterhurford/up.zsh'
 
-zplugin ice wait'!0' lucid
-zplugin light 'Tarrasch/zsh-bd'
+zinit ice wait'!0' lucid
+zinit light 'Tarrasch/zsh-bd'
 
-zplugin ice wait'!0' lucid
-zplugin light 'jocelynmallon/zshmarks'
+zinit ice wait'!0' lucid
+zinit light 'jocelynmallon/zshmarks'
 
 #--------------------------------#
 # enhancive command
 #--------------------------------#
-zplugin ice wait'!0' lucid
-zplugin light 'supercrabtree/k' # ls
+zinit ice wait'!0' lucid
+zinit light 'supercrabtree/k' # ls
 
-zplugin ice wait'!0' lucid pick"fzf-extras.zsh"
-zplugin light 'atweiden/fzf-extras' # fzf
+zinit ice wait'!0' lucid pick"fzf-extras.zsh"
+zinit light 'atweiden/fzf-extras' # fzf
 
 #--------------------------------#
 # git
 #--------------------------------#
-# zplugin ice wait'!0' lucid # -> using as a alias in gitconfig
-zplugin light 'caarlos0/zsh-git-sync'
+# zinit ice wait'!0' lucid # -> using as a alias in gitconfig
+zinit light 'caarlos0/zsh-git-sync'
 
 #--------------------------------#
 # extension
 #--------------------------------#
-zplugin ice wait'!0' lucid atload"source $ZHOMEDIR/rc/pluginconfig/emoji-cli_atload.zsh"
-zplugin light 'b4b4r07/emoji-cli'
+zinit ice wait'!0' lucid atload"source $ZHOMEDIR/rc/pluginconfig/emoji-cli_atload.zsh"
+zinit light 'b4b4r07/emoji-cli'
 
-zplugin ice wait'!0' lucid
-zplugin light 't413/zsh-background-notify'
+zinit ice wait'!0' lucid
+zinit light 't413/zsh-background-notify'
 
 # Too slow on ssh
-# zplugin ice wait'!0' lucid atload"source $ZHOMEDIR/rc/pluginconfig/zsh-command-time_atload.zsh"
-# zplugin light 'popstas/zsh-command-time'
+# zinit ice wait'!0' lucid atload"source $ZHOMEDIR/rc/pluginconfig/zsh-command-time_atload.zsh"
+# zinit light 'popstas/zsh-command-time'
 
 #--------------------------------#
 # program
 #--------------------------------#
-zplugin ice from"gh-r" as"program"
-zplugin load "junegunn/fzf-bin"
+zinit ice from"gh-r" as"program"
+zinit load "junegunn/fzf-bin"
 
-zplugin ice wait'!0' lucid pick"asdf.sh" atload"source $ZPLG_HOME/plugins/asdf-vm---asdf/completions/asdf.bash" blockf nocompletions
-zplugin light 'asdf-vm/asdf'
+zinit ice wait'!0' lucid pick"asdf.sh" atload"source $ZPLG_HOME/plugins/asdf-vm---asdf/completions/asdf.bash" blockf nocompletions
+zinit light 'asdf-vm/asdf'
 
 
 #==============================================================#
@@ -132,18 +132,18 @@ zplugin light 'asdf-vm/asdf'
 #==============================================================#
 
 # asdf
-#zplugin ice wait'!0' lucid as"program" pick:"bin/anyenv" if"[[ -d "$HOME/.config/anyenv/anyenv-install" ]]" atload'eval "$(anyenv init -)"'
-#zplugin light anyenv/anyenv
+#zinit ice wait'!0' lucid as"program" pick:"bin/anyenv" if"[[ -d "$HOME/.config/anyenv/anyenv-install" ]]" atload'eval "$(anyenv init -)"'
+#zinit light anyenv/anyenv
 # don't maintain
-# zplugin ice pick"*.sh" atinit"source $ZHOMEDIR/rc/pluginconfig/z_atinit.zsh"
-# zplugin light 'rupa/z'
+# zinit ice pick"*.sh" atinit"source $ZHOMEDIR/rc/pluginconfig/z_atinit.zsh"
+# zinit light 'rupa/z'
 # git-prompt
-# zplugin ice lucid wait"0" atload"source $ZHOMEDIR/rc/pluginconfig/zsh-async_atload.zsh && set_async"
-# zplugin light mafredri/zsh-async
+# zinit ice lucid wait"0" atload"source $ZHOMEDIR/rc/pluginconfig/zsh-async_atload.zsh && set_async"
+# zinit light mafredri/zsh-async
 
 # don't use
-# zplugin ice wait'!0' lucid
-# zplugin light 'b4b4r07/zsh-gomi'
+# zinit ice wait'!0' lucid
+# zinit light 'b4b4r07/zsh-gomi'
 #zsh-users/zsh-syntax-highlighting # -> zdharma/fast-syntax-highlighting
 # move
 # zplug 'hchbaw/zce.zsh' # -> don't move
