6e6651dfda46f46cc631d834258b63e78c302f1a
Yuta Katayama <yutakatay@gmail.com>
2020-02-12 22:37:36 +0900

    Fix status bar layout without plugin

--- .tmux/conf/scripts/status-right.sh
+++ .tmux/conf/scripts/status-right.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+if [[ "$1" =~ ^:: ]]; then
+  SYS_STATUS=""
+else
+  SYS_STATUS=" [$1]"
+fi
+
 if [[ $(tmux display -p "#{client_width}") -ge 150 ]]; then
-  STATUS_RIGHT_LONG="#[bg=colour=236 fg=colour240]${TMUX_POWERLINE_SYMBOL_LEFT_FULL} #[bg=colour240 fg=colour231] #h ${1} #[bg=colour240 fg=colour252]${TMUX_POWERLINE_SYMBOL_LEFT_FULL}#[fg=colour235 bg=colour252 nobold] $(LANG=C date '+%Y-%m-%d(%a) %H:%M') "
+  STATUS_RIGHT_LONG="#[bg=colour=236 fg=colour240]${TMUX_POWERLINE_SYMBOL_LEFT_FULL} #[bg=colour240 fg=colour231] #h${SYS_STATUS} #[bg=colour240 fg=colour252]${TMUX_POWERLINE_SYMBOL_LEFT_FULL}#[fg=colour235 bg=colour252 nobold] $(LANG=C date '+%Y-%m-%d(%a) %H:%M') "
 else
   STATUS_RIGHT_LONG=""
 fi
