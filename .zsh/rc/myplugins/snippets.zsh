e214a73dcec71f42742ae8b91fdef25408dc0d42
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2020-03-02 23:20:49 +0900

    Update snippet plugin

--- .zsh/rc/myplugins/snippets.zsh
+++ .zsh/rc/myplugins/snippets.zsh
@@ -4,21 +4,21 @@
 
 # ycino's config
 function snippet-selection() {
-  local SNIP_FILE="$HOME/.config/snippets/snippets"
+  local SNIP_FILE="$HOME/.zsh/snippets/snippets"
   if [[ ! -f $SNIP_FILE ]]; then
     echo "Snippet file is not found [$SNIP_FILE]"
     return
   fi
 
   local out snippet key file
-  IFS=$'\n' out=($(\grep -v "^#" $SNIP_FILE | \grep -v "^\s*$" | fzf --query "$LBUFFER" --prompt="Snippet> " --preview="bat ~/.config/snippets/{1}" --expect=ctrl-e))
+  out=($(\grep -v "^#" $SNIP_FILE | \grep -v "^\s*$" | fzf --query "$LBUFFER" --prompt="Snippet> " --height 40% --reverse --preview="bat $SNIP_FILE" --expect=ctrl-e))
   key=$(echo "$out" | head -1)
   snippet=$(echo "$out" | head -2 | tail -1)
 
   if [[ $snippet != "" ]]; then
     if [[ $key == "ctrl-e" ]]; then
       file=$(echo $snippet | cut -d' ' -f 1)
-      vi ~/.config/snippets/$file
+      vi $SNIP_FILE
     else
       snippet=$(echo $snippet | awk '{for(i=2;i<NF;i++){printf("%s%s",$i,OFS=" ")}print $NF}')
       BUFFER=$snippet
