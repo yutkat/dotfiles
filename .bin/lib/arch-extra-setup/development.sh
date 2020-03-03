6a1a7abaec330f2f0a9e48ae498f329581e7aa23
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2020-03-03 14:03:49 +0900

    Update rust development environment

--- .bin/lib/arch-extra-setup/development.sh
+++ .bin/lib/arch-extra-setup/development.sh
@@ -2,4 +2,7 @@
 sudo pacman -S --noconfirm --needed rustup clang gcc gdb make cmake
 sudo pacman -S --noconfirm --needed docker xorg-xhost jq
 
+rustup install stable
+rustup component add rls rust-analysis rust-src rustfmt clippy
+
 yay -S --noconfirm --needed ghq-bin
