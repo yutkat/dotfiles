a4261ea4ec8d0ba388c817d9d83753c0ce8871b1
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2020-03-10 19:04:42 +0900

    zsh: Add command to find color code

--- .zsh/rc/function.zsh
+++ .zsh/rc/function.zsh
@@ -276,6 +276,11 @@ function 256color() {
   done
 }
 
+function ascii_color_code() {
+  seq 30 47 | xargs -n 1 -i{} printf "\x1b[%dm#\x1b[0m %d\n" {} {}
+}
+
+
 function find_no_new_line_at_end_of_file() {
   find * -type f -print0 | xargs -0 -L1 bash -c 'test "$(tail -c 1 "$0")" && echo "No new line at end of $0"'
 }
