7e25288725d947bb14a73eabc5fdbdc8333b1d4e
Yuta Katayama <yutakatay@gmail.com>
2020-03-24 01:22:54 +0900

    misc: Support wl-clipboard

--- .config/rofi/script/get_phrase.sh
+++ .config/rofi/script/get_phrase.sh
@@ -10,10 +10,16 @@ fi
 if [ -z "$@" ]; then
   cat $phrase_file
 else
-  if builtin command -v xsel > /dev/null 2>&1; then
-    echo $@ | tr -d '\n' | xsel -i -b
-  elif builtin command -v xclip > /dev/null 2>&1; then
-    echo $@ | tr -d '\n' | xclip -i -selection clipboard
+  if [ "$WAYLAND_DISPLAY" != "" ]; then
+    if builtin command -v wl-copy > /dev/null 2>&1; then
+      echo $@ | tr -d '\n' | wl-copy
+    fi
+  else
+    if builtin command -v xsel > /dev/null 2>&1; then
+      echo $@ | tr -d '\n' | xsel -i -b
+    elif builtin command -v xclip > /dev/null 2>&1; then
+      echo $@ | tr -d '\n' | xclip -i -selection clipboard
+    fi
   fi
 fi
 
