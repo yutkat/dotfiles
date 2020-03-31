5e5be9643c45104589e386d8800d771b2813d0a7
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2020-03-31 14:06:31 +0900

    installer: Add diff-highlight

--- .bin/lib/dotsinstaller/gitconfig.sh
+++ .bin/lib/dotsinstaller/gitconfig.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-git config --global include.path "~/.gitconfig_shared"
+git config --global include.path "$HOME/.gitconfig_shared"
+
+if [[ -x /usr/share/git/diff-highlight/diff-highlight ]]; then
+  sudo ln -snf /usr/share/git/diff-highlight/diff-highlight /usr/local/bin/
+  git config --global pager.log 'diff-highlight | less'
+  git config --global pager.show 'diff-highlight | less'
+  git config --global pager.diff 'diff-highlight | less'
+fi
