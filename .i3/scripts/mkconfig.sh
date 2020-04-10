a8452f9ba50604b4d1cce03b7918362bf71d587a
Yuta Katayama <yutakatay@gmail.com>
2020-04-10 10:27:34 +0900

    sway: Fix the script

--- .i3/scripts/mkconfig.sh
+++ .i3/scripts/mkconfig.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 ROOT_DIR=$(cd $(dirname $0)/..; pwd)
 CONFIG="$ROOT_DIR/config"
@@ -6,8 +6,9 @@ CONFIG="$ROOT_DIR/config"
 [ -e "$CONFIG" ] && rm -f $CONFIG
 cat "$ROOT_DIR/config.base" > $CONFIG
 
-if ls $ROOT_DIR/config.[^base]* > /dev/null 2>&1; then
-  cat $ROOT_DIR/config.[^base]*  >> $CONFIG
+shopt -s extglob
+if ls $ROOT_DIR/config.!(base*) > /dev/null 2>&1; then
+  cat $ROOT_DIR/config.!(base*)  >> $CONFIG
 fi
 
 chmod 444 $CONFIG
