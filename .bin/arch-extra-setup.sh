a2d2a610ca01cee41c2c1bf714eeb6136c08365e
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2020-05-03 16:35:21 +0900

    sway: Add easyfocus

--- .bin/arch-extra-setup.sh
+++ .bin/arch-extra-setup.sh
@@ -51,8 +51,8 @@ function main() {
     shift
   done
 
-  source $current_dir/lib/arch-extra-setup/utils.sh
   source $current_dir/lib/arch-extra-setup/development.sh
+  source $current_dir/lib/arch-extra-setup/utils.sh
 
   if [ -f /sys/module/battery/initstate ] || [ -d /proc/acpi/battery/BAT0 ]; then
     source $current_dir/lib/arch-extra-setup/laptop.sh
--- .bin/lib/arch-extra-setup/utils.sh
+++ .bin/lib/arch-extra-setup/utils.sh
@@ -9,7 +9,7 @@ if ! builtin command -v yay > /dev/null 2>&1; then
   (cd /tmp/yay && makepkg -si --noconfirm && yay -Syy)
 fi
 
-yay -S --noconfirm --needed i3-easyfocus-git clipmenu light-git
+yay -S --noconfirm --needed i3-easyfocus-git wmfocus clipmenu light-git
 if ! builtin command -v urxvt > /dev/null 2>&1; then
   yay -S --noconfirm --needed urxvt-resize-font-git
 fi
