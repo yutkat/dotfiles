9c9a378bbb82674bd3091e1bd8403858a7014855
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2020-05-09 18:48:24 +0900

    installer: Fix shellcheck

--- .bin/lib/dotsinstaller/gitconfig.sh
+++ .bin/lib/dotsinstaller/gitconfig.sh
@@ -3,8 +3,10 @@
 git config --global include.path "$HOME/.gitconfig_shared"
 
 if [[ -x /usr/share/git/diff-highlight/diff-highlight ]]; then
-  sudo ln -snf /usr/share/git/diff-highlight/diff-highlight /usr/local/bin/
-  git config --global pager.log 'diff-highlight | less'
-  git config --global pager.show 'diff-highlight | less'
-  git config --global pager.diff 'diff-highlight | less'
+  if [[ ! -e "/usr/local/bin/diff-highlight" ]]; then
+    sudo ln -snf /usr/share/git/diff-highlight/diff-highlight /usr/local/bin/
+    git config --global pager.log 'diff-highlight | less'
+    git config --global pager.show 'diff-highlight | less'
+    git config --global pager.diff 'diff-highlight | less'
+  fi
 fi
