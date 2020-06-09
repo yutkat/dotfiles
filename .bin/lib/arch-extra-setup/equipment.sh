c1195e40e6eea1d4f7419bbd24bf6fb4f9c5e3aa
Yuta Katayama <yutakatay@gmail.com>
2020-06-09 23:56:55 +0900

    i3: Update sound manager

--- .bin/lib/arch-extra-setup/equipment.sh
+++ .bin/lib/arch-extra-setup/equipment.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo pacman -S --noconfirm --needed cups cups-pdf bind-tools
+sudo pacman -S --noconfirm --needed cups cups-pdf bind-tools pavucontrol
 sudo systemctl enable systemd-timesyncd.service
 sudo systemctl start systemd-timesyncd.service
 
