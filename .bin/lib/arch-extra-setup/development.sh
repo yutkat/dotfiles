ec517ddcc59777944aee1c6ce7f3f44b9ef47047
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2020-06-14 20:06:22 +0900

    installer: Remove rls

--- .bin/lib/arch-extra-setup/development.sh
+++ .bin/lib/arch-extra-setup/development.sh
@@ -3,5 +3,5 @@ sudo pacman -S --noconfirm --needed rustup clang gcc gdb make cmake
 sudo pacman -S --noconfirm --needed docker xorg-xhost jq
 
 rustup install stable
-rustup component add rls rust-analysis rust-src rustfmt clippy
+rustup component add rust-analysis rust-src rustfmt clippy
 
