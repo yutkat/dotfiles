a0dcc8b7098be8ca2993451e72fe4abf82905a09
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2020-06-17 11:41:53 +0900

    zsh: Fix autocomplete included double quote

--- .zsh/rc/pluginconfig/zsh-autocomplete_atload.zsh
+++ .zsh/rc/pluginconfig/zsh-autocomplete_atload.zsh
@@ -2,6 +2,7 @@ zstyle ':autocomplete:list-choices:*' max-lines 90%
 # quotation is missing bug eg. `git commit -m "xxx yyy"``
 # zstyle ':autocomplete:*' groups always
 zstyle ':autocomplete:tab:*' completion cycle
+zstyle ':autocomplete:*' magic off
 
 function precmd_remove_up_down_bindkey() {
   bindkey '^[OA' up-line-or-history
