8e6687d34456809e2d9d8c4e4932fcb5bf53a029
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2020-06-25 10:16:35 +0900

    zsh: Add a bindkey for autosuggestion

--- .zsh/rc/pluginconfig/zsh-autosuggestions_atload.zsh
+++ .zsh/rc/pluginconfig/zsh-autosuggestions_atload.zsh
@@ -1,3 +1,11 @@
 # ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#ff00ff,bg=cyan,bold,underline"
 
 ZSH_AUTOSUGGEST_STRATEGY=(completion history)
+
+bindkey "^Y" autosuggest-accept
+#bindkey autosuggest-execute
+#bindkey autosuggest-clear
+#bindkey autosuggest-fetch
+#bindkey autosuggest-disable
+#bindkey autosuggest-enable
+#bindkey autosuggest-toggle
