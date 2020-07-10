75281cdea367d3ab271d8ebe4d3ef18119c6b5b1
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2020-07-10 16:17:19 +0900

    git: Update git diff wrapper

--- .bin/lib/dotsinstaller/gitconfig.sh
+++ .bin/lib/dotsinstaller/gitconfig.sh
@@ -2,15 +2,3 @@
 
 git config --global include.path "$HOME/.gitconfig_shared"
 
-if builtin command -v "delta" > /dev/null 2>&1; then
-  git config --global pager.log 'delta | less'
-  git config --global pager.show 'delta | less'
-  git config --global pager.diff 'delta | less'
-elif [[ -x /usr/share/git/diff-highlight/diff-highlight ]]; then
-  if [[ ! -e "/usr/local/bin/diff-highlight" ]]; then
-    sudo ln -snf /usr/share/git/diff-highlight/diff-highlight /usr/local/bin/
-    git config --global pager.log 'diff-highlight | less'
-    git config --global pager.show 'diff-highlight | less'
-    git config --global pager.diff 'diff-highlight | less'
-  fi
-fi
