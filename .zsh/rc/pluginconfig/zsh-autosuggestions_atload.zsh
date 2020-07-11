5a122a2dcc154b7b4ee526babebf7268d2020b36
Yuta Katayama <yutakatay@gmail.com>
2020-07-12 00:15:09 +0900

    zsh: Fix the autosuggestions config

--- .zsh/rc/pluginconfig/zsh-autosuggestions_atload.zsh
+++ .zsh/rc/pluginconfig/zsh-autosuggestions_atload.zsh
@@ -1,7 +1,8 @@
 # ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#ff00ff,bg=cyan,bold,underline"
 
 # ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(buffer-empty bracketed-paste accept-line push-line-or-edit)
-ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+# ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+ZSH_AUTOSUGGEST_STRATEGY=(completion)
 ZSH_AUTOSUGGEST_USE_ASYNC=true
 
 bindkey "^Y" autosuggest-accept
