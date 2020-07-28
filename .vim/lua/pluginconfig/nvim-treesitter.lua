08aa8e9680fd12613f544441db33f462da8d123f
Yuta Katayama <yutakatay@gmail.com>
2020-07-28 20:40:11 +0900

    vim: Update latest config for nvim-treesitter

--- .vim/lua/pluginconfig/nvim-treesitter.lua
+++ .vim/lua/pluginconfig/nvim-treesitter.lua
@@ -1,31 +1,69 @@
 require'nvim-treesitter.configs'.setup {
     highlight = {
-        enable = true,                    -- false will disable the whole extension
-        disable = { 'c' },        -- list of language that will be disabled
+      enable = true,                    -- false will disable the whole extension
+      disable = {  },        -- list of language that will be disabled
+      custom_captures = {               -- mapping of user defined captures to highlight groups
+        -- ["foo.bar"] = "Identifier"   -- highlight own capture @foo.bar with highlight group "Identifier", see :h nvim-treesitter-query-extensions
+      },
     },
     incremental_selection = {
-        enable = true,
-        disable = { 'cpp', 'lua' },
-        keymaps = {                       -- mappings for incremental selection (visual mappings)
-            init_selection = 'gnn',         -- maps in normal mode to init the node/scope selection
-            node_incremental = "grn",       -- increment to the upper named parent
-            scope_incremental = "grc",      -- increment to the upper scope (as defined in locals.scm)
-            node_decremental = "grm",       -- decrement to the previous node
-        }
+      enable = true,
+      disable = { 'cpp', 'lua' },
+      keymaps = {                       -- mappings for incremental selection (visual mappings)
+        init_selection = 'gnn',         -- maps in normal mode to init the node/scope selection
+        node_incremental = "grn",       -- increment to the upper named parent
+        scope_incremental = "grc",      -- increment to the upper scope (as defined in locals.scm)
+        node_decremental = "grm",       -- decrement to the previous node
+      }
     },
     refactor = {
-        highlight_defintions = {
-            enable = true
-        },
-        smart_rename = {
-            enable = true,
-            smart_rename = "grr"              -- mapping to rename reference under cursor
-        },
-        navigation = {
-            enable = true,
-            goto_definition = "gnd",          -- mapping to go to definition of symbol under cursor
-            list_definitions = "gnD"          -- mapping to list all definitions in current file
+      highlight_definitions = {
+        enable = true
+      },
+      highlight_current_scope = {
+        enable = true
+      },
+      smart_rename = {
+        enable = true,
+        keymaps = {
+          smart_rename = "grr"          -- mapping to rename reference under cursor
         }
+      },
+      navigation = {
+        enable = true,
+        keymaps = {
+          goto_definition = "gnd",      -- mapping to go to definition of symbol under cursor
+          list_definitions = "gnD"      -- mapping to list all definitions in current file
+        }
+      }
+    },
+    textobjects = { -- syntax-aware textobjects
+    enable = true,
+    disable = {},
+    keymaps = {
+        ["iL"] = { -- you can define your own textobjects directly here
+        python = "(function_definition) @function",
+        cpp = "(function_definition) @function",
+        c = "(function_definition) @function",
+        java = "(method_declaration) @function"
+        },
+        -- or you use the queries from supported languages with textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["aC"] = "@class.outer",
+        ["iC"] = "@class.inner",
+        ["ac"] = "@conditional.outer",
+        ["ic"] = "@conditional.inner",
+        ["ae"] = "@block.outer",
+        ["ie"] = "@block.inner",
+        ["al"] = "@loop.outer",
+        ["il"] = "@loop.inner",
+        ["is"] = "@statement.inner",
+        ["as"] = "@statement.outer",
+        ["ad"] = "@comment.outer",
+        ["am"] = "@call.outer",
+        ["im"] = "@call.inner"
+      }
     },
     ensure_installed = 'all' -- one of 'all', 'language', or a list of languages
 }
