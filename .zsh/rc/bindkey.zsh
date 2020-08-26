23a7d8291c923eba2d97c4b194b9da4c25396b5e
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2020-08-26 15:14:08 +0900

    zsh: Fix bindkeys

--- .zsh/rc/bindkey.zsh
+++ .zsh/rc/bindkey.zsh
@@ -38,8 +38,9 @@ bindkey '^[[1;5D' backward-word
 autoload -U history-search-end
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
-bindkey '^P' history-beginning-search-backward-end
-bindkey '^N' history-beginning-search-forward-end
+# broken with auto-suggestion
+# bindkey '^P' history-beginning-search-backward-end
+# bindkey '^N' history-beginning-search-forward-end
 bindkey '^[[A' history-beginning-search-backward-end
 bindkey '^[[B' history-beginning-search-forward-end
 # history incremental search
