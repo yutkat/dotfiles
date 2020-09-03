d8d1bc74890c746d27dedfdf968b146cdbe26b50
Yuta Katayama <yutakatay@users.noreply.github.com>
2020-09-03 14:35:10 +0900

    zsh: Don't use teraterm lately

--- .zsh/rc/option.zsh
+++ .zsh/rc/option.zsh
@@ -13,7 +13,7 @@ setopt hist_ignore_dups      # 直前と同じコマンドラインはヒスト�
 setopt hist_ignore_space     # スペースで始まるコマンド行はヒストリリストから削除
 # (→ 先頭にスペースを入れておけば、ヒストリに保存されない)
 unsetopt hist_verify         # ヒストリを呼び出してから実行する間に一旦編集可能を止める
-#setopt hist_reduce_blanks    # 余分な空白は詰めて記録<-teratermで履歴かおかしくなる
+setopt hist_reduce_blanks    # 余分な空白は詰めて記録<-teratermで履歴かおかしくなる
 setopt hist_save_no_dups     # ヒストリファイルに書き出すときに、古いコマンドと同じものは無視する。
 setopt hist_no_store         # historyコマンドは履歴に登録しない
 
