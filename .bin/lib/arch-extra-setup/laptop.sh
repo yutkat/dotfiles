e02f268acecbf0b53d59ca50f37d5c7b3acc1516
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2020-10-19 17:42:55 +0900

    i3: Change bluetooth frontend

--- .bin/lib/arch-extra-setup/laptop.sh
+++ .bin/lib/arch-extra-setup/laptop.sh
@@ -3,7 +3,7 @@ sudo pacman -S --noconfirm --needed xf86-input-libinput xorg-xinput tpacpi-bat a
 sudo pacman -S --noconfirm --needed tlp acpi
 sudo systemctl enable tlp
 sudo pacman -S --noconfirm --needed networkmanager network-manager-applet
-sudo pacman -S --noconfirm --needed blueberry
+sudo pacman -S --noconfirm --needed blueman
 sudo systemctl start bluetooth
 sudo systemctl enable bluetooth
 sudo pacman -S --noconfirm --needed pasystray
