460f94d443022cb39d79bdbfcd95ac9484a81c92
Yuta Katayama <yutakatay@users.noreply.github.com>
2020-10-22 16:36:37 +0900

    zsh: Add zsh-abbr but it disabled

---
+++ .zsh/rc/pluginconfig/zsh-abbr_atinit.zsh
@@ -0,0 +1,3 @@
+abbr add -g --quiet --force G="| grep"
+abbr add --quiet --force gs="git status"
+abbr add --quiet -g ~~='$(git rev-parse --show-cdup)'
--- .zsh/rc/pluginconfig/zsh-abbrev-alias_atinit.zsh
+++ .zsh/rc/pluginconfig/zsh-abbrev-alias_atinit.zsh
@@ -1,2 +1,3 @@
 abbrev-alias -g G="| grep"
 abbrev-alias gs="git status"
+abbrev-alias -eg ~~='$(git rev-parse --show-cdup)'
