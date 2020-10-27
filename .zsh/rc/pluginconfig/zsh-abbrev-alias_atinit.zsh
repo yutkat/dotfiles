39197fc972f93f082767047f5bae6984f49400bc
Yuta Katayama <yutakatay@users.noreply.github.com>
2020-10-27 11:09:32 +0900

    zsh: Add abbr

--- .zsh/rc/pluginconfig/zsh-abbrev-alias_atinit.zsh
+++ .zsh/rc/pluginconfig/zsh-abbrev-alias_atinit.zsh
@@ -1,3 +1,10 @@
 abbrev-alias -g G="| grep"
-abbrev-alias gs="git status"
 abbrev-alias -eg ~~='$(git rev-parse --show-cdup)'
+
+# git
+abbrev-alias gc='git commit'
+abbrev-alias gb='git branch'
+abbrev-alias gd='git diff HEAD'
+abbrev-alias gs='git status --short'
+abbrev-alias pr='gh pr view -w || gh pr create -w'
+
