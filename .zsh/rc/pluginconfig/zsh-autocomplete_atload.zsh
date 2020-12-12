7c9ccf951b669e4260d8f9d4efff51e826f5beaf
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2020-12-12 16:02:46 +0900

    zsh: Overwrite the completion option

--- .zsh/rc/pluginconfig/zsh-autocomplete_atload.zsh
+++ .zsh/rc/pluginconfig/zsh-autocomplete_atload.zsh
@@ -5,3 +5,12 @@ bindkey -M menuselect '^H' vi-backward-char
 bindkey -M menuselect '^K' vi-up-line-or-history
 bindkey -M menuselect '^L' vi-forward-char
 bindkey -M menuselect '^J' vi-down-line-or-history
+
+function precmd_overwrite_options() {
+  # $ZDATADIR/zinit/plugins/marlonrichert---zsh-autocomplete/module/.autocomplete.config
+  zstyle ':completion:*:complete:*' matcher-list '' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' '+l:|=* r:|=*'
+}
+
+autoload -Uz add-zsh-hook
+add-zsh-hook precmd precmd_overwrite_options
+
