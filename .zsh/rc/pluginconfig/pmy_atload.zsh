3298058a3e0697583cc348183ddd2aa77f5a3dcb
Yuta Katayama <yutakatay@users.noreply.github.com>
2021-01-04 01:33:30 +0900

    zsh: Add pmy

---
+++ .zsh/rc/pluginconfig/pmy_atload.zsh
@@ -0,0 +1,6 @@
+export PMY_TRIGGER_KEY="^y"
+export PMY_RULE_PATH="$XDG_CONFIG_HOME/pmy/rules"
+export PMY_SNIPPET_PATH="$XDG_CONFIG_HOME/pmy/snippets"
+export PMY_LOG_PATH="$XDG_CACHE_HOME/pmy/log.txt"
+
+eval "$(pmy init)"
