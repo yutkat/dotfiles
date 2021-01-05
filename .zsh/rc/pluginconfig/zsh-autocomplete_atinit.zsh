16fd249b13eeb23fcc57771d8b13efc62d45ffec
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-01-05 16:19:55 +0900

    zsh: Fetch zsh-autocomplete config

--- .zsh/rc/pluginconfig/zsh-autocomplete_atinit.zsh
+++ .zsh/rc/pluginconfig/zsh-autocomplete_atinit.zsh
@@ -1,17 +1,21 @@
 zstyle ':completion:*' extra-verbose yes
-#zstyle ':completion:list-expand:*' extra-verbose yes
-zstyle ':autocomplete:*' max-lines 30%
+zstyle ':completion:list-expand:*' extra-verbose yes
+zstyle ':autocomplete:*' max-lines 80%
 #zstyle ':autocomplete:tab:*' insert-unambiguous yes
 #zstyle ':autocomplete:tab:*' widget-style menu-select
 #zstyle ':autocomplete:*' config off
-zstyle ':autocomplete:*' min-delay .5
+#zstyle ':autocomplete:*' min-delay .3
 zstyle ':autocomplete:tab:*' widget-style menu-complete
 zstyle ':autocomplete:tab:*' fzf-completion no
 # zstyle ':autocomplete:*' magic off
 # zstyle ':autocomplete:*' fuzzy-search off
 # zstyle ':autocomplete:*' key-binding off
 # zstyle ':autocomplete:*' config off
-zstyle ':completion:*' tag-order '! history-words recent-directories recent-files' '-'
+#zstyle ':completion:*' tag-order '! history-words recent-directories recent-files' '-'
+zstyle ':completion:*:complete:*:' tag-order \
+  '! history-words ancestor-directories recent-directories recent-files' -
+zstyle ':completion:*:complete:*:' group-order \
+  options arguments values local-directories files builtins history-words
 
 autoload -Uz add-zle-hook-widget
 if ! builtin command -v compinit > /dev/null 2>&1; then
