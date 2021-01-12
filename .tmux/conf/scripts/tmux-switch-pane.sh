a5f00898dc0d95175117973d320dfd52c201155a
Yuta Katayama <yutakatay@users.noreply.github.com>
2021-01-12 11:14:52 +0900

    CI: Fix lint

--- .tmux/conf/scripts/tmux-switch-pane.sh
+++ .tmux/conf/scripts/tmux-switch-pane.sh
@@ -12,6 +12,6 @@ TARGET_SPEC="#{session_name}:#{window_id}:#{pane_id}:"
 # select pane
 LINE=$(tmux list-panes -a -F "$TARGET_SPEC $LIST_DATA" | $FZF_COMMAND) || exit 0
 # split the result
-args=(${LINE//:/ })
+args=("${LINE//:/ }")
 # activate session/window/pane
 tmux select-pane -t ${args[2]} && tmux select-window -t ${args[1]} && tmux switch-client -t ${args[0]}
