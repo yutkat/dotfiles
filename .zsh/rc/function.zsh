ea30662714ea9aa15287de60af8ea4cfe3417cf7
Yuta Katayama <yutakatay@users.noreply.github.com>
2021-02-07 19:02:09 +0900

    zsh: Change update command for neovim

--- .zsh/rc/function.zsh
+++ .zsh/rc/function.zsh
@@ -401,7 +401,7 @@ function plugupdate() {
   fi
 
   print_info "Update $EDITOR plugins"
-  $EDITOR -c 'silent PlugUpdate --sync' -c 'silent CocUpdateSync' -c 'qall'
+  $EDITOR -c 'silent PackerUpdate' -c 'silent CocUpdateSync' -c 'qall'
   print_info "Finish vim plugins"
 }
 
