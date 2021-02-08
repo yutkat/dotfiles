4a6963668ec2aa0d950898fce4cd72c9941f127f
Yuta Katayama <yutakatay@users.noreply.github.com>
2021-02-09 01:35:57 +0900

    vim: Change indent plugin

--- .vim/lua/pluginconfig/packer.lua
+++ .vim/lua/pluginconfig/packer.lua
@@ -1,1 +1,4 @@
-vim.cmd [[autocmd BufWritePost .vim/lua/pluginlist.lua,.vim/rc/pluginconfig/*.vim,.vim/rc/pluginsetup/*.vim,.vim/lua/pluginconfig/*.lua PackerCompile]]
+vim.cmd('augroup vimrc_packer')
+vim.cmd('autocmd!')
+vim.cmd('autocmd BufWritePost .vim/lua/pluginlist.lua,.vim/rc/pluginconfig/*.vim,.vim/rc/pluginsetup/*.vim,.vim/lua/pluginconfig/*.lua PackerCompile')
+vim.cmd('augroup END')
