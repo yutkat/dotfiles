40035d3916303b97b18938c4b828119d423a9b91
Yuta Katayama <yutakatay@users.noreply.github.com>
2021-02-08 23:50:42 +0900

    vim: Fix hop with whitespace

--- .vim/lua/pluginconfig/hop.lua
+++ .vim/lua/pluginconfig/hop.lua
@@ -1,1 +1,3 @@
-vim.api.nvim_set_keymap('n', 'SS', "<cmd>lua require'hop'.jump_words()<cr>", {})
+vim.api.nvim_set_keymap('n', 'SS', "<cmd>lua vim.cmd('DisableWhitespace'); require'hop'.jump_words()<cr>", {})
+-- FIXME
+vim.cmd [[autocmd InsertEnter * EnableWhitespace]]
