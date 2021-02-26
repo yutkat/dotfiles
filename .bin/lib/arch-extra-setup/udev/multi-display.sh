65033197c0735b84657dcb68f72a505dffbd0a62
Yuta Katayama <yutkat@users.noreply.github.com>
2021-02-26 11:05:20 +0900

    installer: Apply XDG_CONFIG for i3

--- .bin/lib/arch-extra-setup/udev/multi-display.sh
+++ .bin/lib/arch-extra-setup/udev/multi-display.sh
@@ -3,7 +3,7 @@
 function register_display_hotplug() {
   local current_dir
   current_dir=$(dirname "${BASH_SOURCE[0]:-$0}")
-  sudo ln -snf $HOME/.i3/scripts/detect_displays.sh /usr/local/bin/detect_displays.sh
+  sudo ln -snf $HOME/.config/i3/scripts/detect_displays.sh /usr/local/bin/detect_displays.sh
   sudo cp $current_dir/rules.d/95-monitor-hotplug.rules /etc/udev/rules.d/
 
   sudo udevadm control --reload-rules
