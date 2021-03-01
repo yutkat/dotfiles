882c96f5754e33f508707545ecbdaac1b5137145
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-03-01 19:07:05 +0900

    vim: Fix packer config

--- .config/nvim/lua/rc/pluginsetup/packer.lua
+++
@@ -1,6 +0,0 @@
-vim.api.nvim_exec([[
-let g:plug = map(split(glob('~/.local/share/nvim/site/pack/packer/*/*'), "\n"), {key, val ->  fnamemodify(val, ":t")})
-function! IsPluginInstalled(name) abort
-  return index(g:plug, a:name) >= 0
-endfunction
-]], true)
