49e463bbfe9a5143cb2e6f5dbe0bedce0434388c
Yuta Katayama <yutkat@users.noreply.github.com>
2021-03-09 15:55:21 +0900

    tmux: Fix config for latest version

--- .config/tmux/conf/scripts/popuptmux.sh
+++ .config/tmux/conf/scripts/popuptmux.sh
@@ -5,5 +5,5 @@ height=${2:-80%}
 if [ "$(tmux display-message -p -F "#{session_name}")" = "popup" ]; then
   tmux detach-client
 else
-  tmux popup -d '#{pane_current_path}' -xC -yC -w$width -h$height -K -E -R "tmux attach -t popup || tmux new -s popup"
+  tmux popup -d '#{pane_current_path}' -xC -yC -w$width -h$height -E "tmux attach -t popup || tmux new -s popup"
 fi
