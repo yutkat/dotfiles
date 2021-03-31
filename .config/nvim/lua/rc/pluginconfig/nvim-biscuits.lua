2d3ec789fb1e0daacafd7ecfc2193ed09353a8bb
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-03-31 12:02:30 +0900

    vim: Add treesitter plugin

---
+++ .config/nvim/lua/rc/pluginconfig/nvim-biscuits.lua
@@ -0,0 +1,16 @@
+require('nvim-biscuits').setup({
+  default_config = {
+    max_length = 12,
+    min_destance = 5,
+    prefix_string = " 📎 "
+  },
+  language_config = {
+    html = {
+      prefix_string = " 🌐 "
+    },
+    javascript = {
+      prefix_string = " ✨ ",
+      max_length = 80
+    }
+  }
+})
