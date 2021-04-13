4b169886de8648b4b8fa660920ab5e8803c09a8e
Yuta Katayama <yutkat@users.noreply.github.com>
2021-04-13 15:48:09 +0900

    zsh: Fix cd-ghq on alias

--- .config/zsh/rc/pluginconfig/ghq_atload.zsh
+++ .config/zsh/rc/pluginconfig/ghq_atload.zsh
@@ -4,6 +4,14 @@ export GHQ_ROOT=$HOME/.ghq
 alias ghq-repos="ghq list -p | fzf --prompt 'GHQ> ' --height 40% --reverse"
 alias ghq-repo='cd $(ghq-repos)'
 
+function cd-fzf-ghqlist() {
+  local REPO
+  REPO=$(ghq list -p | xargs ls -dt1 | sed -e 's;'${GHQ_ROOT}/';;g' | fzf +m --prompt 'GHQ> ' --height 40% --reverse --preview "bat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*")
+  if [ -n "${REPO}" ]; then
+    cd ${GHQ_ROOT}/${REPO}
+  fi
+}
+
 function cd-fzf-ghqlist-widget() {
   local REPO
   REPO=$(ghq list -p | xargs ls -dt1 | sed -e 's;'${GHQ_ROOT}/';;g' | fzf +m --prompt 'GHQ> ' --height 40% --reverse --preview "bat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*")
@@ -12,7 +20,8 @@ function cd-fzf-ghqlist-widget() {
   fi
   zle accept-line
 }
+
 zle -N cd-fzf-ghqlist-widget
 bindkey '^Xq' cd-fzf-ghqlist-widget
 bindkey '^X^q' cd-fzf-ghqlist-widget
-alias g=cd-fzf-ghqlist-widget
+alias g=cd-fzf-ghqlist
