26a3eb9aaccff15a4109869e6eab11a1f632743b
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-04-14 16:02:09 +0900

    installer: Cleanup files

--- install_scripts/lib/arch-extra-setup/development.sh
+++ install_scripts/lib/arch-extra-setup/development.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo pacman -S --noconfirm --needed rustup clang gcc gdb make cmake
-sudo pacman -S --noconfirm --needed docker xorg-xhost jq
+sudo pacman -S --noconfirm --needed docker jq
 
 rustup install stable
 rustup component add rust-analysis rust-src rustfmt clippy
