7f993041ab1b0e3175c1f41654ba58e2e196ac5c
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-04-16 12:35:02 +0900

    vim: Change formatter plugin

--- .config/nvim/lua/rc/pluginconfig/format.lua
+++ .config/nvim/lua/rc/pluginconfig/format.lua
@@ -1,3 +1,53 @@
+require "format".setup {
+  -- ["*"] = {
+  --   {cmd = {"sed -i 's/[ \t]*$//'"}} -- remove trailing whitespace
+  -- },
+  -- vim = {
+  --   {
+  --     cmd = {"luafmt -w replace"},
+  --     start_pattern = "^lua << EOF$",
+  --     end_pattern = "^EOF$"
+  --   }
+  -- },
+  -- vimwiki = {
+  --   {
+  --     cmd = {"prettier -w --parser babel"},
+  --     start_pattern = "^{{{javascript$",
+  --     end_pattern = "^}}}$"
+  --   }
+  -- },
+  lua = {
+    {
+      cmd = {
+        function(file)
+          return string.format("luafmt -l %s -w replace %s", vim.bo.textwidth, file)
+        end
+      }
+    }
+  },
+  -- go = {
+  --   {
+  --     cmd = {"gofmt -w", "goimports -w"},
+  --     tempfile_postfix = ".tmp"
+  --   }
+  -- },
+  -- javascript = {
+  --   {cmd = {"prettier -w", "./node_modules/.bin/eslint --fix"}}
+  -- },
+  -- markdown = {
+  --   {cmd = {"prettier -w"}},
+  --   {
+  --     cmd = {"black"},
+  --     start_pattern = "^```python$",
+  --     end_pattern = "^```$",
+  --     target = "current"
+  --   }
+  -- },
+  sh = {
+    {cmd = {"shfmt -w -i 2 -sr -ci"}}
+  },
+}
+
 vim.api.nvim_exec([[
 augroup vimrc_format
   autocmd!
