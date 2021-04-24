408ae488c08f3ec73bf88033ad703673b36a8b68
Yuta Katayama <yutkat@users.noreply.github.com>
2021-04-24 13:51:58 +0900

    vim: Add symbols-outline

---
+++ .config/nvim/lua/rc/pluginconfig/symbols-outline.lua
@@ -0,0 +1,13 @@
+local opts = {
+    -- whether to highlight the currently hovered symbol
+    -- disable if your cpu usage is higher than you want it
+    -- or you just hate the highlight
+    -- default: true
+    highlight_hovered_item = true,
+
+    -- whether to show outline guides
+    -- default: true
+    show_guides = true,
+}
+
+require('symbols-outline').setup(opts)
