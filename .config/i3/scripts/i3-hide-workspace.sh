f5386b0c1f97f956eeb54c88df1da6fefd3e5210
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-05-07 10:34:55 +0900

    i3: Add the bindsym

---
+++ .config/i3/scripts/i3-hide-workspace.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+prev_workspace=$(i3-msg -t get_workspaces | tr '}' '\n' | grep '"focused":true' | tr , '\n' | grep '"num":' | cut -d : -f 2)
+i3-msg workspace 1
+i3-msg workspace $(($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
+i3-msg workspace $prev_workspace
