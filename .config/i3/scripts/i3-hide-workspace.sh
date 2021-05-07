4f35912ef10a8c46faa65d8acc7dcc24bb3168c7
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-05-07 14:46:37 +0900

    i3: Add the icon for concentration

--- .config/i3/scripts/i3-hide-workspace.sh
+++ .config/i3/scripts/i3-hide-workspace.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 prev_workspace=$(i3-msg -t get_workspaces | tr '}' '\n' | grep '"focused":true' | tr , '\n' | grep '"num":' | cut -d : -f 2)
+
 i3-msg workspace 1
 i3-msg workspace $(($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
-i3-msg workspace $prev_workspace
+
+if [[ $prev_workspace != 1 ]]; then
+  i3-msg workspace $prev_workspace
+fi
