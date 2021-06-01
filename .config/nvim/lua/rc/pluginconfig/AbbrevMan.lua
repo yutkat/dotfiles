672a41ee0e4388c106cfd0e867bcc7f622337752
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-06-01 17:16:59 +0900

    vim: Add iswap

--- .config/nvim/lua/rc/pluginconfig/AbbrevMan.lua
+++ .config/nvim/lua/rc/pluginconfig/AbbrevMan.lua
@@ -1,12 +1,14 @@
-local isas = require("isas")
 
-isas.setup({
-  load_natural_dictionaries_at_startup = true,
-  load_programming_dictionaries_at_startup = true,
-  natural_dictionaries = {
-    ["nt_en"] = {}
-  },
-  programming_dictionaries = {
-    ["pr_py"] = {}
-  }
+local abbrev_man = require("abbrev-man")
+
+abbrev_man.setup({
+	load_natural_dictionaries_at_startup = true,
+	load_programming_dictionaries_at_startup = true,
+	natural_dictionaries = {
+		["nt_en"] = {}
+	},
+	programming_dictionaries = {
+		["pr_py"] = {}
+	}
+
 })
