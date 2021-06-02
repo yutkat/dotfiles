a93320ff39756d384163836dbe9e1080db2053bf
Yuta Katayama <yutkat@users.noreply.github.com>
2021-06-02 16:08:24 +0900

    i3: Change bar to i3status-rust

--- install_scripts/lib/dotsinstaller/install-i3.sh
+++ install_scripts/lib/dotsinstaller/install-i3.sh
@@ -15,7 +15,7 @@ function install_i3() {
   elif [[ $distro == "arch" ]]; then
     sudo pacman -S --noconfirm --needed xorg-server xorg-xinit
     sudo pacman -S --noconfirm --needed lightdm lightdm-gtk-greeter
-    sudo pacman -S --noconfirm --needed i3-gaps feh i3status i3blocks
+    sudo pacman -S --noconfirm --needed i3-gaps feh i3status-rust
     sudo pacman -S --noconfirm --needed dmenu xorg-xprop rofi dunst compton
     sudo pacman -S --noconfirm --needed xorg-xbacklight lm_sensors xclip
     sudo pacman -S --noconfirm --needed xautolock unclutter
@@ -34,7 +34,6 @@ function setup_i3() {
     sudo yum install -y scrot || true
   elif [[ $distro == "arch" ]]; then
     sudo pacman -S --noconfirm --needed scrot flameshot
-    (cd /usr/share && sudo ln -snf /usr/lib/i3blocks/ .)
   elif [[ $distro == "alpine" ]]; then
     :
   fi
@@ -50,4 +49,4 @@ function install_i3blocks_contrib() {
 install_i3
 setup_i3
 sudo python3 -m pip install -U i3ipc
-install_i3blocks_contrib
+# install_i3blocks_contrib
