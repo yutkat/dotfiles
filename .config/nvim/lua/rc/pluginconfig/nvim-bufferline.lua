2f4634104865554b3c6d436ab53106d93c2119d7
Yuta Katayama <yutkat@users.noreply.github.com>
2021-06-05 13:44:43 +0900

    vim: Update nvim-bufferline's config

--- .config/nvim/lua/rc/pluginconfig/nvim-bufferline.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-bufferline.lua
@@ -2,23 +2,23 @@ require('bufferline').setup {
   options = {
     numbers = "ordinal",
     number_style = {"none", "subscript"},
-    mappings = true,
+    -- mappings = true,
     -- NOTE: this plugin is designed with this icon in mind,
     -- and so changing this is NOT recommended, this is intended
     -- as an escape hatch for people who cannot bear it for whatever reason
-    indicator_icon = '▎',
-    buffer_close_icon = '',
-    modified_icon = '●',
-    close_icon = '',
-    left_trunc_marker = '',
-    right_trunc_marker = '',
-    max_name_length = 18,
-    max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
-    tab_size = 18,
-    diagnostics = false,
-    diagnostics_indicator = function(count, level, diagnostics_dict, context)
-      return "(" .. count .. ")"
-    end,
+    -- indicator_icon = '▎',
+    -- buffer_close_icon = '',
+    -- modified_icon = '●',
+    -- close_icon = '',
+    -- left_trunc_marker = '',
+    -- right_trunc_marker = '',
+    -- max_name_length = 18,
+    -- max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+    -- tab_size = 18,
+    -- diagnostics = false,
+    -- diagnostics_indicator = function(count, level, diagnostics_dict, context)
+    --   return "(" .. count .. ")"
+    -- end,
     -- NOTE: this will be called a lot so don't do any heavy processing here
     -- custom_filter = function(buf_number)
     --   -- filter out filetypes you don't want to see
@@ -38,16 +38,16 @@ require('bufferline').setup {
     -- offsets = {
     --   {filetype = "NvimTree", text = "File Explorer", text_align = "left" | "center" | "right"}
     -- },
-    show_buffer_icons = true,
+    -- show_buffer_icons = true,
     show_buffer_close_icons = false,
     show_close_icon = false,
-    show_tab_indicators = true,
-    persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+    -- show_tab_indicators = true
+    -- persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
     -- can also be a table containing 2 custom separators
     -- [focused and unfocused]. eg: { '|', '|' }
-    separator_style = "thick",
-    enforce_regular_tabs = false,
-    always_show_bufferline = true
+    -- separator_style = "thick",
+    enforce_regular_tabs = true
+    -- always_show_bufferline = true
     -- sort_by = 'relative_directory'
   }
 }
