a1aabe77d5b24be8e44a5fb6771dfe1df0da3444
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-07-09 12:29:47 +0900

    vim: Update latest plugin config

--- .config/nvim/lua/rc/pluginconfig/telescope.lua
+++ .config/nvim/lua/rc/pluginconfig/telescope.lua
@@ -40,7 +40,7 @@ require('telescope').setup {
     file_sorter = require'telescope.sorters'.get_fuzzy_file,
     file_ignore_patterns = {"node_modules/*"},
     generic_sorter = require'telescope.sorters'.get_generic_fuzzy_sorter,
-    shorten_path = true,
+    path_display = {},
     winblend = 0,
     border = {},
     borderchars = {
