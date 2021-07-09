1bb92259f47bdfd34b0cc7713aceda7e95f79337
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-07-09 14:48:25 +0900

    zsh: Fix alias

--- .config/zsh/rc/alias.zsh
+++ .config/zsh/rc/alias.zsh
@@ -45,8 +45,8 @@ alias gre='grep -H -n -I --color=auto'
 
 ## application ##
 # vi
-alias vi="$EDITOR"
-alias sv="sudo $EDITOR"
+alias vi='$EDITOR'
+alias sv='sudo $EDITOR'
 alias nv='nvim'
 alias v='nvim -c "SessionLoad"'
 
