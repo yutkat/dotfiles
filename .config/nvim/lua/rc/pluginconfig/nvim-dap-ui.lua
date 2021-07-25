57ced85b0e52ae286de38b432814435dc077b2a1
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-07-25 16:28:43 +0900

    vim: Fix nvim-dap config

--- .config/nvim/lua/rc/pluginconfig/nvim-dap-ui.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-dap-ui.lua
@@ -1,3 +1,3 @@
-require("dapui").setup()
+require("dapui").setup({})
 vim.api.nvim_set_keymap('n', '<debugger>q', "<Cmd>lua require'dap'.disconnect({})<CR>",
                         {noremap = true, silent = true})
