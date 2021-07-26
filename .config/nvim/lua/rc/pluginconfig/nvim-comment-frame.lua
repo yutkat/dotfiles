5ef7d8a98c107cc05e249d9caf0dd7da0d6008d4
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-07-26 15:07:59 +0900

    vim: Add comment-frame plugin

---
+++ .config/nvim/lua/rc/pluginconfig/nvim-comment-frame.lua
@@ -0,0 +1,23 @@
+require('nvim-comment-frame').setup({
+  -- if true, <leader>cf keymap will be disabled
+  disable_default_keymap = false,
+  -- adds custom keymap
+  keymap = '<leader>cc',
+  multiline_keymap = '<leader>C',
+  -- start the comment with this string
+  start_str = '//',
+  -- end the comment line with this string
+  end_str = '//',
+  -- fill the comment frame border with this character
+  fill_char = '-',
+  -- width of the comment frame
+  frame_width = 70,
+  -- wrap the line after 'n' characters
+  line_wrap_len = 50,
+  -- automatically indent the comment frame based on the line
+  auto_indent = true,
+  -- add comment above the current line
+  add_comment_above = true,
+  -- configurations for individual language goes here
+  languages = {}
+})
