93504288efa59147929c0a115b791adef370e557
Yuta Katayama <yutkat@users.noreply.github.com>
2021-08-24 01:53:03 +0900

    vim: Add a plugin

---
+++ .config/nvim/lua/rc/pluginconfig/treesitter-unit.lua
@@ -0,0 +1,10 @@
+vim.api.nvim_set_keymap('x', 'iu', ':lua require"treesitter-unit".select()<CR>', {noremap = true})
+vim.api.nvim_set_keymap('x', 'au', ':lua require"treesitter-unit".select(true)<CR>',
+                        {noremap = true})
+vim.api.nvim_set_keymap('o', 'iu', ':<c-u>lua require"treesitter-unit".select()<CR>',
+                        {noremap = true})
+vim.api.nvim_set_keymap('o', 'au', ':<c-u>lua require"treesitter-unit".select(true)<CR>',
+                        {noremap = true})
+vim.api.nvim_set_keymap('x', '/', ':lua require"treesitter-unit".select()<CR>', {noremap = true})
+vim.api.nvim_set_keymap('o', '/', ':<c-u>lua require"treesitter-unit".select()<CR>',
+                        {noremap = true})
