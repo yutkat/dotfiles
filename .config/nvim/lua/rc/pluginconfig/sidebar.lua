bd1ef2e277992b2f0878ef9c1e6e68c2c414c8b6
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-09-20 13:18:29 +0900

    vim: Add a sidebar plugin

---
+++ .config/nvim/lua/rc/pluginconfig/sidebar.lua
@@ -0,0 +1,11 @@
+require("sidebar-nvim").setup({
+  disable_default_keybindings = 0,
+  bindings = {["q"] = function() require("sidebar-nvim").close() end},
+  open = false,
+  side = "right",
+  initial_width = 35,
+  update_interval = 1000,
+  section_separator = "-----"
+})
+
+vim.api.nvim_set_keymap('n', 'gs', "<Cmd>SidebarNvimToggle<CR>", {noremap = true, silent = true})
