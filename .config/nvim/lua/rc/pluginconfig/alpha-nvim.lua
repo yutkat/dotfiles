0cda8975aa5d5655a8aeccbbdb2e5857db2dca48
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-10-06 10:03:13 +0900

    vim: Fix the start screen

--- .config/nvim/lua/rc/pluginconfig/alpha-nvim.lua
+++ .config/nvim/lua/rc/pluginconfig/alpha-nvim.lua
@@ -3,7 +3,7 @@ local dashboard = require 'alpha.themes.dashboard'
 dashboard.section.header.val = vim.fn.readfile(vim.fn.expand(
                                                    '~/.config/nvim/rc/files/dashboard_custom_header.txt'))
 dashboard.section.buttons.val = {
-  dashboard.button('l', ' Open last session', ':RestoreSession<CR>'),
+  dashboard.button('l', ' Open last session', ':SearchSession<CR>'),
   dashboard.button('h', ' Recently opened files', ':Telescope my_mru<CR>'),
   dashboard.button('f', ' Find file', ':Telescope find_files<CR>'),
   dashboard.button('e', ' New file', ':enew<CR>'),
