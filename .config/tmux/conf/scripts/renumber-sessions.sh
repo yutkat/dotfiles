85d68c2132809585f6b0d8b80764fcbceef2f046
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-10-22 15:02:07 +0900

    tmux: Add the script for renumbering session

---
+++ .config/tmux/conf/scripts/renumber-sessions.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# https://github.com/maximbaz/dotfiles/commit/925a5b88a8263805a5a24c6198dad23bfa62f44d
+
+sessions=$(tmux list-sessions -F '#S' | grep '^[0-9]\+$' | sort -n)
+
+new=1
+for old in $sessions; do
+  tmux rename -t "$old" "$new"
+  ((new++))
+done
