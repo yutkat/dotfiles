31f0f5131a3547c5679408bcc95bd3c1b0047ca6
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-10-28 10:36:27 +0900

    vim: Add copilot.vim

--- .config/nvim/lua/rc/pluginconfig/stabilize.lua
+++ .config/nvim/lua/rc/pluginconfig/stabilize.lua
@@ -1,1 +1,8 @@
-require("stabilize").setup()
+require("stabilize").setup({
+  force = true,
+  forcemark = nil,
+  ignore = {
+    filetype = {"help", "list", "Trouble", "packer"},
+    buftype = {"terminal", "quickfix", "loclist", "nofile"}
+  }
+})
