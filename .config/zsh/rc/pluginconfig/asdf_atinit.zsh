68004958e0b714986c57be9bf8c007d46259b19d
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-11-03 14:56:32 +0900

    zsh: Fix asdf

--- .config/zsh/rc/pluginconfig/asdf_atinit.zsh
+++ .config/zsh/rc/pluginconfig/asdf_atinit.zsh
@@ -1,3 +1,3 @@
 export ASDF_CONFIG_FILE=$XDG_CONFIG_HOME/asdf/config
-export ASDF_DIR=$XDG_DATA_HOME/asdf
+export ASDF_DIR=$ZPLG_HOME/plugins/asdf-vm---asdf
 export ASDF_DATA_DIR=$XDG_DATA_HOME/asdf
