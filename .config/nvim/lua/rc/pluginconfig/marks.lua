73b21a6ec94cf5a04daeccd7ecededd7c9081063
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-11-10 18:11:34 +0900

    vim: Update marks

--- .config/nvim/lua/rc/pluginconfig/marks.lua
+++ .config/nvim/lua/rc/pluginconfig/marks.lua
@@ -1,6 +1,6 @@
 require'marks'.setup {
   default_mappings = false, -- whether to map keybinds or not. default true
-  builtin_marks = {".", "<", ">", "^"}, -- which builtin marks to show. default {}
+  builtin_marks = {}, -- which builtin marks to show. default {}
   cyclic = true, -- whether movements cycle back to the beginning/end of buffer. default true
   force_write_shada = false, -- whether the shada file is updated after modifying uppercase marks. default false
   bookmark_0 = { -- marks.nvim allows you to configure up to 10 bookmark groups, each with its own sign/virttext
