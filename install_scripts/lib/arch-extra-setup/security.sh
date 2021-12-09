9f35fbaa0e3c610ee70f2c54b441640dc7fdd6ac
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-12-09 20:06:30 +0900

    i3: Change the app for lockscreen

--- install_scripts/lib/arch-extra-setup/security.sh
+++ install_scripts/lib/arch-extra-setup/security.sh
@@ -3,9 +3,9 @@
 function setting_lock() {
   local s_file
   s_file="/etc/systemd/system/i3lock@$(whoami).service"
-  envsubst <"$HOME"/.config/i3/systemd-instances/i3lock@user.service | sudo tee "$s_file" >/dev/null
+  envsubst < "$HOME"/.config/i3/systemd-instances/i3lock@user.service | sudo tee "$s_file" > /dev/null
   sudo systemctl enable "$(basename "$s_file")"
 }
 
-paru -S --noconfirm multilockscreen-git
+paru -S --noconfirm betterlockscreen
 setting_lock
