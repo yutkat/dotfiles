96f78dd487a09c7c28d4e3ddc4c54aacb11606ab
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-12-23 19:39:59 +0900

    vim: Add plugins

---
+++ .config/nvim/lua/rc/pluginconfig/HighStr.lua
@@ -0,0 +1,19 @@
+local high_str = require("high-str")
+
+high_str.setup({
+  verbosity = 0,
+  saving_path = "/tmp/highstr/",
+  highlight_colors = {
+    -- color_id = {"bg_hex_code",<"fg_hex_code"/"smart">}
+    color_0 = {"#0c0d0e", "smart"}, -- Cosmic charcoal
+    color_1 = {"#e5c07b", "smart"}, -- Pastel yellow
+    color_2 = {"#7FFFD4", "smart"}, -- Aqua menthe
+    color_3 = {"#8A2BE2", "smart"}, -- Proton purple
+    color_4 = {"#FF4500", "smart"}, -- Orange red
+    color_5 = {"#008000", "smart"}, -- Office green
+    color_6 = {"#0000FF", "smart"}, -- Just blue
+    color_7 = {"#FFC0CB", "smart"}, -- Blush pink
+    color_8 = {"#FFF9E3", "smart"}, -- Cosmic latte
+    color_9 = {"#7d5c34", "smart"} -- Fallow brown
+  }
+})
