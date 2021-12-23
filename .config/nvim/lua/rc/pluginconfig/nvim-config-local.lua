5a556af071ac65e4d89a04388b572d893f7fefd2
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2021-12-23 19:39:59 +0900

    vim: Add plugins

---
+++ .config/nvim/lua/rc/pluginconfig/nvim-config-local.lua
@@ -0,0 +1,7 @@
+require('config-local').setup {
+  config_files = {'local.vim', 'local.lua', '.vim/local.vim', '.vim/local.lua'}, -- Config file patterns to load (lua supported)
+  hashfile = vim.fn.stdpath("data") .. "/config-local", -- Where the plugin keeps files data
+  autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
+  commands_create = true, -- Create commands (ConfigSource, ConfigEdit, ConfigTrust, ConfigIgnore)
+  silent = false -- Disable plugin messages (Config loaded/ignored)
+}
