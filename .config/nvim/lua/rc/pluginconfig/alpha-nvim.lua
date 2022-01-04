e5f532d11beb7f2d8b984e4eb7b766f45a54dd93
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-01-04 17:05:46 +0900

    vim: Change the note taking plugin to zk

--- .config/nvim/lua/rc/pluginconfig/alpha-nvim.lua
+++ .config/nvim/lua/rc/pluginconfig/alpha-nvim.lua
@@ -8,7 +8,7 @@ dashboard.section.buttons.val = {
   dashboard.button('f', ' Find file', ':Telescope find_files<CR>'),
   dashboard.button('e', ' New file', ':enew<CR>'),
   dashboard.button('b', ' Jump to bookmarks', ':Telescope marks<CR>'),
-  dashboard.button('n', ' Memo New', ':NeuronNew<CR>'),
-  dashboard.button('m', ' Memo List', ':NeuronFindZettels<CR>')
+  dashboard.button('n', ' Memo New', ':Zknew<CR>'),
+  dashboard.button('m', ' Memo List', ':ZkNotes<CR>')
 }
 alpha.setup(dashboard.opts)
