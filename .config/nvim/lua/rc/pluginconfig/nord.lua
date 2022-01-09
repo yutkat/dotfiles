b07ad004c6c3b880a21d1a4ba7190ab6eecb1ded
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-01-09 23:17:05 +0900

    vim: Change colorsheme to onedark

---
+++ .config/nvim/lua/rc/pluginconfig/nord.lua
@@ -0,0 +1,5 @@
+vim.g.nord_pkgs = {
+  'coc', 'tree-sitter', 'vim-gitgutter', 'vim-indent-guides', 'vim-javascript', 'vim-markdown',
+  'vim-signature', 'vim-signify', 'vimwiki', 'vim-yaml', 'yats'
+}
+vim.cmd [[ colorscheme nord ]]
