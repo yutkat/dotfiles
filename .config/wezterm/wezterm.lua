bbe044c7d91671332312cfaffb690a36d5cd8276
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-01-10 16:09:23 +0900

    terminal: Change the terminal to wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,9 @@
+local wezterm = require 'wezterm';
+
 return {
+  font = wezterm.font("Cica"),
   use_ime = true,
-  font_size = 9.0,
-  color_scheme = "Gruvbox Dark",
+  font_size = 10.0,
+  color_scheme = "OneHalfDark",
   hide_tab_bar_if_only_one_tab = true
 }
