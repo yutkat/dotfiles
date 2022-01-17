ad4b19afec00972cd5515f1f45d179de7a9c8d36
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-01-17 11:44:20 +0900

    vim: Add null-ls

--- .config/nvim/lua/rc/pluginconfig/null-ls.lua
+++ .config/nvim/lua/rc/pluginconfig/null-ls.lua
@@ -1,6 +1,20 @@
 require("null-ls").setup({
   sources = {
-    require("null-ls").builtins.formatting.stylua, require("null-ls").builtins.diagnostics.eslint,
+    -- LuaFormatter off
+    require("null-ls").builtins.formatting.stylua,
+    require("null-ls").builtins.diagnostics.eslint,
+    require("null-ls").builtins.diagnostics.shellcheck,
     require("null-ls").builtins.completion.spell
-  }
+    -- LuaFormatter on
+  },
+  on_attach = function(client)
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd([[
+            augroup LspFormatting
+                autocmd! * <buffer>
+                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+            augroup END
+            ]])
+    end
+  end
 })
