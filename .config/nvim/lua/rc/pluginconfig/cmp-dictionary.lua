80bf1f87959192d96ca7ae801f25c21882fd6d60
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-01-25 18:27:28 +0900

    vim: Update cmp

--- .config/nvim/lua/rc/pluginconfig/cmp-dictionary.lua
+++ .config/nvim/lua/rc/pluginconfig/cmp-dictionary.lua
@@ -1,5 +1,10 @@
+local file = vim.fn.stdpath('data') .. "/zsh/dictionary/my.dict"
+local dic = {}
+if vim.fn.filereadable(file) ~= 0 then
+  dic = file
+end
 require("cmp_dictionary").setup({
-  dic = {["*"] = "/usr/share/dict/cracklib-small"},
+  dic = {["*"] = dic},
   -- The following are default values, so you don't need to write them if you don't want to change them
   exact = 2,
   async = false,
