20e256988d247225ef5d13093cecc11174bc8163
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-01-27 15:51:22 +0900

    vim: Update null-ls

--- .config/nvim/lua/rc/pluginconfig/null-ls.lua
+++ .config/nvim/lua/rc/pluginconfig/null-ls.lua
@@ -1,10 +1,15 @@
-require("null-ls").setup({
+local null_ls = require("null-ls")
+
+null_ls.setup({
 	sources = {
 		-- LuaFormatter off
-		require("null-ls").builtins.formatting.stylua,
-		require("null-ls").builtins.diagnostics.eslint,
-		require("null-ls").builtins.diagnostics.shellcheck,
-		require("null-ls").builtins.completion.spell,
+		null_ls.builtins.diagnostics.shellcheck,
+		-- null_ls.builtins.completion.spell,
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.rustfmt,
+		null_ls.builtins.diagnostics.eslint,
+		null_ls.builtins.diagnostics.codespell,
 		-- LuaFormatter on
 	},
 	on_attach = function(client)
@@ -14,40 +19,7 @@ require("null-ls").setup({
 				autocmd! * <buffer>
 				autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
 			augroup END
-]])
+			]])
 		end
 	end,
 })
-
-local null_ls = require("null-ls")
-local api = vim.api
-
-local no_really = {
-	method = null_ls.methods.DIAGNOSTICS,
-	filetypes = { "markdown", "text" },
-	generator = {
-		fn = function(params)
-			local diagnostics = {}
-			-- sources have access to a params object
-			-- containing info about the current file and editor state
-			for i, line in ipairs(params.content) do
-				local col, end_col = line:find("really")
-				if col and end_col then
-					-- null-ls fills in undefined positions
-					-- and converts source diagnostics into the required format
-					table.insert(diagnostics, {
-						row = i,
-						col = col,
-						end_col = end_col,
-						source = "no-really",
-						message = "Don't use 'really!'",
-						severity = 2,
-					})
-				end
-			end
-			return diagnostics
-		end,
-	},
-}
-
-null_ls.register(no_really)
