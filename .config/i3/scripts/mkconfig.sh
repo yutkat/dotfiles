19ee22acb8b62a2c7eb9006c651b4f8d54119e14
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-02-04 11:02:41 +0900

    i3: Change the file extension

--- .config/i3/scripts/mkconfig.sh
+++ .config/i3/scripts/mkconfig.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 ROOT_DIR=$(
-  cd "$(dirname "$0")"/.. || exit
-  pwd
+	cd "$(dirname "$0")"/.. || exit
+	pwd
 )
 CONFIG="$ROOT_DIR/config"
 
 [ -e "$CONFIG" ] && rm -f "$CONFIG"
-cat "$ROOT_DIR/config.base" >"$CONFIG"
+cat "$ROOT_DIR/base.config" >"$CONFIG"
 
 shopt -s extglob
-raw_config=$(ls "$ROOT_DIR"/config.!(base*) 2>&1)
+raw_config=$(ls "$ROOT_DIR"/!(base*).config 2>&1)
 
 if [[ -n "$raw_config" ]]; then
-  echo "$raw_config" | xargs cat >>"$CONFIG"
+	echo "$raw_config" | xargs cat >>"$CONFIG"
 fi
 
 chmod 444 "$CONFIG"
