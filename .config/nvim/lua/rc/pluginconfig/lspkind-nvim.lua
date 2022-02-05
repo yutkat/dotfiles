567689c628d972d3582a9377cafd2bd6df438644
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-02-05 16:37:21 +0900

    vim: Fix lspkind

--- .config/nvim/lua/rc/pluginconfig/lspkind-nvim.lua
+++ .config/nvim/lua/rc/pluginconfig/lspkind-nvim.lua
@@ -2,7 +2,7 @@ require("lspkind").init({
 	-- enables text annotations
 	--
 	-- default: true
-	with_text = true,
+	mode = "symbol_text",
 
 	-- default symbol map
 	-- can be either 'default' (requires nerd-fonts font) or
