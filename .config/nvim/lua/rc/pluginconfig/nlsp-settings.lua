8c8a04330dc3c720e3a5304d80407f26d7f6a38b
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-02-09 13:38:23 +0900

    neovim: Change the directory name for project local

--- .config/nvim/lua/rc/pluginconfig/nlsp-settings.lua
+++ .config/nvim/lua/rc/pluginconfig/nlsp-settings.lua
@@ -1,6 +1,11 @@
 require("nlspsettings").setup({
 	config_home = vim.fn.stdpath("config") .. "/nlsp-settings",
+	local_settings_dir = ".nvim/nlsp-settings",
 	local_settings_root_markers = { ".git" },
 	-- append default schemas
 	jsonls_append_default_schemas = true,
+	nvim_notify = {
+		enable = true,
+		timeout = 5000,
+	},
 })
--- .config/nvim/lua/rc/pluginconfig/nvim-config-local.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-config-local.lua
@@ -1,5 +1,5 @@
 require("config-local").setup({
-	config_files = { "local.vim", "local.lua", ".vim/local.vim", ".vim/local.lua" }, -- Config file patterns to load (lua supported)
+	config_files = { "local.vim", "local.lua", ".nvim/local.vim", ".nvim/local.lua" }, -- Config file patterns to load (lua supported)
 	hashfile = vim.fn.stdpath("data") .. "/config-local", -- Where the plugin keeps files data
 	autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
 	commands_create = true, -- Create commands (ConfigSource, ConfigEdit, ConfigTrust, ConfigIgnore)
