65abc6e71c367d9571f953620225d9fcdc60f645
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-02-14 14:22:36 +0900

    neovim: Update nvim-cmp

--- .config/nvim/lua/rc/pluginconfig/nvim-cmp.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-cmp.lua
@@ -123,7 +123,7 @@ cmp.setup({
 		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
 		["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
 		["<C-e>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
-		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	},
 	-- LuaFormatter off
 	sources = cmp.config.sources({
