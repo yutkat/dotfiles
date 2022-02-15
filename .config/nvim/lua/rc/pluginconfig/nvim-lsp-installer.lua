503faeb45cc326f73461dee99a153ec292041411
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-02-15 19:15:30 +0900

    neovim: Fix the plugin depnedency

--- .config/nvim/lua/rc/pluginconfig/nvim-lsp-installer.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-lsp-installer.lua
@@ -1,4 +1,5 @@
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local lspconfig = require("lspconfig")
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
