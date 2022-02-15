6321896bad61caa9c7a6f95f2057b58b29c5e466
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-02-15 22:51:49 +0900

    neovim: Update lsp settings for lua

--- .config/nvim/lua/rc/pluginconfig/nvim-lsp-installer.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-lsp-installer.lua
@@ -73,6 +73,8 @@ local server_configs = {
 					-- Make the server aware of Neovim runtime files
 					-- library = vim.api.nvim_get_runtime_file("", true),
 					preloadFileSize = 500,
+					-- very slow
+					-- library = vim.api.nvim_get_runtime_file("", true),
 				},
 				-- Do not send telemetry data containing a randomized but unique identifier
 				telemetry = { enable = false },
