4c83c1341c437171c887ba9f9526243155f03e4e
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-02-15 10:39:56 +0900

    neovim: Add lightspeed

--- .config/nvim/lua/rc/pluginconfig/lightspeed.lua
+++ .config/nvim/lua/rc/pluginconfig/lightspeed.lua
@@ -1,9 +1,29 @@
+vim.g.lightspeed_no_default_keymaps = true
+
 require("lightspeed").setup({
-	jump_to_first_match = true,
-	jump_on_partial_input_safety_timeout = 400,
-	highlight_unique_chars = false,
-	grey_out_search_area = true,
+	ignore_case = false,
+	exit_after_idle_msecs = { unlabeled = 2000, labeled = nil },
+	--- s/x ---
+	jump_to_unique_chars = { safety_timeout = 400 },
 	match_only_the_start_of_same_char_seqs = true,
-	limit_ft_matches = 5,
-	full_inclusive_prefix_key = "<c-x>",
+	force_beacons_into_match_width = false,
+	-- Display characters in a custom way in the highlighted matches.
+	substitute_chars = { ["\r"] = "¬" },
+	-- Leaving the appropriate list empty effectively disables "smart" mode,
+	-- and forces auto-jump to be on or off.
+	-- These keys are captured directly by the plugin at runtime.
+	special_keys = {
+		next_match_group = "<space>",
+		prev_match_group = "<tab>",
+	},
+	--- f/t ---
+	limit_ft_matches = 4,
+	repeat_ft_with_target_char = false,
 })
+
+vim.cmd([[
+nmap <expr> f reg_recording() . reg_executing() == "" ? "<Plug>Lightspeed_f" : "f"
+nmap <expr> F reg_recording() . reg_executing() == "" ? "<Plug>Lightspeed_F" : "F"
+nmap <expr> t reg_recording() . reg_executing() == "" ? "<Plug>Lightspeed_t" : "t"
+nmap <expr> T reg_recording() . reg_executing() == "" ? "<Plug>Lightspeed_T" : "T"
+]])
