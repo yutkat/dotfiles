0a585fb18aec1610bf06f0996d6ea87f07b1bcc7
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-02-18 10:17:47 +0900

    neovim: Add the csv plugin

---
+++ .config/nvim/lua/rc/pluginconfig/csv-tools.lua
@@ -0,0 +1,10 @@
+require("csvtools").setup({
+	before = 10,
+	after = 10,
+	clearafter = true,
+	-- this will clear the highlight of buf after move
+	showoverflow = true,
+	-- this will provide a overflow show
+	titelflow = true,
+	-- add an alone title
+})
---
+++ .config/nvim/lua/rc/pluginconfig/nvim-test.lua
@@ -0,0 +1,1 @@
+require("nvim-test").setup()
