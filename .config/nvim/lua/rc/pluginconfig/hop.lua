19f6beadcf6067629f52c64cb6dbcef1103c9dd8
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-02-21 11:42:54 +0900

    neovim: Fix the whitespace

--- .config/nvim/lua/rc/pluginconfig/hop.lua
+++ .config/nvim/lua/rc/pluginconfig/hop.lua
@@ -1,14 +1,4 @@
 require("hop").setup({})
 
-vim.api.nvim_set_keymap("n", "SS", "<cmd>lua vim.cmd('DisableWhitespace'); require'hop'.hint_words()<CR>", {})
-vim.api.nvim_set_keymap("x", "SS", "<cmd>lua vim.cmd('DisableWhitespace'); require'hop'.hint_words()<CR>", {})
--- FIXME
-vim.api.nvim_exec(
-	[[
-augroup vimrc_hop
-  autocmd!
-  autocmd InsertEnter * EnableWhitespace
-augroup END
-]],
-	true
-)
+vim.api.nvim_set_keymap("n", "SS", "<cmd>lua require'hop'.hint_words()<CR>", {})
+vim.api.nvim_set_keymap("x", "SS", "<cmd>lua require'hop'.hint_words()<CR>", {})
