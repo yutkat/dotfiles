77ae0c6c7c99a2a7bc8f8680a9b14e4fb81ed191
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-02-24 03:43:40 +0900

    wezterm: Use as a terminal multiplexer

---
+++ .config/wezterm/utils.lua
@@ -0,0 +1,41 @@
+local M = {}
+
+function M.basename(s)
+	return string.gsub(s, "(.*[/\\])(.*)", "%2")
+end
+
+function M.merge_tables(t1, t2)
+	for k, v in pairs(t2) do
+		if (type(v) == "table") and (type(t1[k] or false) == "table") then
+			merge_tables(t1[k], t2[k])
+		else
+			t1[k] = v
+		end
+	end
+	return t1
+end
+
+function M.merge_lists(t1, t2)
+	local result = {}
+	for _, v in pairs(t1) do
+		table.insert(result, v)
+	end
+	for _, v in pairs(t2) do
+		table.insert(result, v)
+	end
+	return result
+end
+
+function M.exists(tab, element)
+	local v
+	for _, v in pairs(tab) do
+		if v == element then
+			return true
+		elseif type(v) == "table" then
+			return M.exists(v, element)
+		end
+	end
+	return false
+end
+
+return M
