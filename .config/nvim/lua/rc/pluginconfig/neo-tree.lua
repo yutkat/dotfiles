253e7c901caf0e6a05e012318787b71e3e2305fe
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-02-28 18:17:54 +0900

    neovim: Change filer from nvim-tree to neo-tree

---
+++ .config/nvim/lua/rc/pluginconfig/neo-tree.lua
@@ -0,0 +1,128 @@
+-- See ":help neo-tree-highlights" for a list of available highlight groups
+vim.cmd([[
+        hi link NeoTreeDirectoryName Directory
+        hi link NeoTreeDirectoryIcon NeoTreeDirectoryName
+      ]])
+
+require("neo-tree").setup({
+	close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+	popup_border_style = "rounded",
+	enable_git_status = true,
+	enable_diagnostics = true,
+	default_component_configs = {
+		indent = {
+			indent_size = 2,
+			padding = 1, -- extra padding on left hand side
+			with_markers = true,
+			indent_marker = "│",
+			last_indent_marker = "└",
+			highlight = "NeoTreeIndentMarker",
+		},
+		icon = {
+			folder_closed = "",
+			folder_open = "",
+			folder_empty = "ﰊ",
+			default = "*",
+		},
+		name = {
+			trailing_slash = false,
+			use_git_status_colors = true,
+		},
+		git_status = {
+			highlight = "NeoTreeDimText", -- if you remove this the status will be colorful
+		},
+	},
+	filesystem = {
+		filters = { --These filters are applied to both browsing and searching
+			show_hidden = false,
+			respect_gitignore = true,
+		},
+		follow_current_file = false, -- This will find and focus the file in the active buffer every
+		-- time the current file is changed while the tree is open.
+		use_libuv_file_watcher = false, -- This will use the OS level file watchers
+		-- to detect changes instead of relying on nvim autocmd events.
+		hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
+		-- in whatever position is specified in window.position
+		-- "open_split",  -- netrw disabled, opening a directory opens within the
+		-- window like netrw would, regardless of window.position
+		-- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
+		window = {
+			position = "left",
+			width = 30,
+			mappings = {
+				["<2-LeftMouse>"] = "open",
+				["<cr>"] = "open",
+				["S"] = "open_split",
+				["s"] = "open_vsplit",
+				["C"] = "close_node",
+				["<bs>"] = "navigate_up",
+				["."] = "set_root",
+				["H"] = "toggle_hidden",
+				["I"] = "toggle_gitignore",
+				["R"] = "refresh",
+				["/"] = "fuzzy_finder",
+				--["/"] = "filter_as_you_type", -- this was the default until v1.28
+				--["/"] = "none" -- Assigning a key to "none" will remove the default mapping
+				["f"] = "filter_on_submit",
+				["<c-x>"] = "clear_filter",
+				["a"] = "add",
+				["d"] = "delete",
+				["r"] = "rename",
+				["c"] = "copy_to_clipboard",
+				["x"] = "cut_to_clipboard",
+				["p"] = "paste_from_clipboard",
+				["m"] = "move", -- takes text input for destination
+				["q"] = "close_window",
+			},
+		},
+	},
+	buffers = {
+		show_unloaded = true,
+		window = {
+			position = "left",
+			mappings = {
+				["<2-LeftMouse>"] = "open",
+				["<cr>"] = "open",
+				["S"] = "open_split",
+				["s"] = "open_vsplit",
+				["<bs>"] = "navigate_up",
+				["."] = "set_root",
+				["R"] = "refresh",
+				["a"] = "add",
+				["d"] = "delete",
+				["r"] = "rename",
+				["c"] = "copy_to_clipboard",
+				["x"] = "cut_to_clipboard",
+				["p"] = "paste_from_clipboard",
+				["bd"] = "buffer_delete",
+			},
+		},
+	},
+	git_status = {
+		window = {
+			position = "float",
+			mappings = {
+				["<2-LeftMouse>"] = "open",
+				["<cr>"] = "open",
+				["S"] = "open_split",
+				["s"] = "open_vsplit",
+				["C"] = "close_node",
+				["R"] = "refresh",
+				["d"] = "delete",
+				["r"] = "rename",
+				["c"] = "copy_to_clipboard",
+				["x"] = "cut_to_clipboard",
+				["p"] = "paste_from_clipboard",
+				["A"] = "git_add_all",
+				["gu"] = "git_unstage_file",
+				["ga"] = "git_add_file",
+				["gr"] = "git_revert_file",
+				["gc"] = "git_commit",
+				["gp"] = "git_push",
+				["gg"] = "git_commit_and_push",
+			},
+		},
+	},
+})
+vim.keymap.set("n", "gx", "<Cmd>NeoTreeRevealToggle<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "G,", "<Cmd>NeoTreeFloatToggle git_status<CR>", { noremap = true, silent = true })
--- .config/nvim/lua/rc/pluginconfig/nvim-tree.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-tree.lua
@@ -1,45 +1,44 @@
-local tree_cb = require("nvim-tree.config").nvim_tree_callback
-
-function NvimTreeEditClose()
-	require("nvim-tree").on_keypress("edit")
-	require("nvim-tree").close()
+local function create_open_close(node)
+	require("nvim-tree.actions.create-file").fn(node)
+	require("nvim-tree.actions").on_keypress("edit")
+	-- require("nvim-tree.view").close()
 end
 
 -- default mappings
 -- LuaFormatter off
 local list = {
-	{ key = { "<CR>", "o", "<2-LeftMouse>" }, cb = tree_cb("edit") },
-	{ key = { "<2-RightMouse>", "<C-]>" }, cb = tree_cb("cd") },
-	{ key = "<C-v>", cb = tree_cb("vsplit") },
-	{ key = "<C-x>", cb = tree_cb("split") },
-	{ key = "<C-t>", cb = tree_cb("tabnew") },
-	{ key = "<", cb = tree_cb("prev_sibling") },
-	{ key = ">", cb = tree_cb("next_sibling") },
-	{ key = "P", cb = tree_cb("parent_node") },
-	{ key = "<BS>", cb = tree_cb("close_node") },
-	{ key = "<Tab>", cb = tree_cb("preview") },
-	{ key = "K", cb = tree_cb("first_sibling") },
-	{ key = "J", cb = tree_cb("last_sibling") },
-	{ key = "I", cb = tree_cb("toggle_ignored") },
-	{ key = "H", cb = tree_cb("toggle_dotfiles") },
-	{ key = "R", cb = tree_cb("refresh") },
-	{ key = "a", cb = tree_cb("create") },
-	{ key = "d", cb = tree_cb("remove") },
-	{ key = "D", cb = tree_cb("trash") },
-	{ key = "r", cb = tree_cb("rename") },
-	{ key = "<C-r>", cb = tree_cb("full_rename") },
-	{ key = "x", cb = tree_cb("cut") },
-	{ key = "c", cb = tree_cb("copy") },
-	{ key = "p", cb = tree_cb("paste") },
-	{ key = "y", cb = tree_cb("copy_name") },
-	{ key = "Y", cb = tree_cb("copy_path") },
-	{ key = "gy", cb = tree_cb("copy_absolute_path") },
-	{ key = "[c", cb = tree_cb("prev_git_item") },
-	{ key = "]c", cb = tree_cb("next_git_item") },
-	{ key = "-", cb = tree_cb("dir_up") },
-	{ key = "s", cb = tree_cb("system_open") },
-	{ key = "q", cb = tree_cb("close") },
-	{ key = "g?", cb = tree_cb("toggle_help") },
+	{ key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
+	{ key = { "<2-RightMouse>", "<C-]>" }, action = "cd" },
+	{ key = "<C-v>", action = "vsplit" },
+	{ key = "<C-x>", action = "split" },
+	{ key = "<C-t>", action = "tabnew" },
+	{ key = "<", action = "prev_sibling" },
+	{ key = ">", action = "next_sibling" },
+	{ key = "P", action = "parent_node" },
+	{ key = "<BS>", action = "close_node" },
+	{ key = "<Tab>", action = "preview" },
+	{ key = "K", action = "first_sibling" },
+	{ key = "J", action = "last_sibling" },
+	{ key = "I", action = "toggle_ignored" },
+	{ key = "H", action = "toggle_dotfiles" },
+	{ key = "R", action = "refresh" },
+	{ key = "a", action = "create_and_close", action_cb = create_open_close },
+	{ key = "d", action = "remove" },
+	{ key = "D", action = "trash" },
+	{ key = "r", action = "rename" },
+	{ key = "<C-r>", action = "full_rename" },
+	{ key = "x", action = "cut" },
+	{ key = "c", action = "copy" },
+	{ key = "p", action = "paste" },
+	{ key = "y", action = "copy_name" },
+	{ key = "Y", action = "copy_path" },
+	{ key = "gy", action = "copy_absolute_path" },
+	{ key = "[c", action = "prev_git_item" },
+	{ key = "]c", action = "next_git_item" },
+	{ key = "-", action = "dir_up" },
+	{ key = "s", action = "system_open" },
+	{ key = "q", action = "close" },
+	{ key = "g?", action = "toggle_help" },
 }
 -- LuaFormatter on
 
