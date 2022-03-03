606d995e89326e7877f11d2cc6cfa5570f649fcd
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-03-03 12:53:30 +0900

    wezterm: Add the function to rename the title

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -60,6 +60,13 @@ end
 --- wezterm on
 ---------------------------------------------------------------
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local user_title = tab.active_pane.user_vars.panetitle
+	if user_title ~= nil and #user_title > 0 then
+		return {
+			{ Text = tab.tab_index + 1 .. ":" .. user_title },
+		}
+	end
+
 	local title = wezterm.truncate_right(utils.basename(tab.active_pane.foreground_process_name), max_width)
 	if title == "" then
 		local uri = utils.convert_home_dir(tab.active_pane.current_working_dir)
