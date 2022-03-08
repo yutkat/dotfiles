ff363df9564839a7da7dc677e534e0c621a16e93
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-03-08 19:18:37 +0900

    neovim: Temporary disable petertriho/nvim-scrollbar

--- .config/nvim/lua/rc/pluginconfig/nvim-scrollbar.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-scrollbar.lua
@@ -1,24 +1,63 @@
 require("scrollbar").setup({
 	show = true,
+	set_highlights = true,
 	handle = {
 		text = " ",
 		color = "#3F4A5A",
+		cterm = nil,
+		highlight = "CursorColumn",
 		hide_if_all_visible = true, -- Hides handle if all lines are visible
 	},
 	marks = {
-		Search = { text = { "-", "=" }, priority = 0, color = "orange" },
-		Error = { text = { "-", "=" }, priority = 1, color = "red" },
-		Warn = { text = { "-", "=" }, priority = 2, color = "yellow" },
-		Info = { text = { "-", "=" }, priority = 3, color = "blue" },
-		Hint = { text = { "-", "=" }, priority = 4, color = "green" },
-		Misc = { text = { "-", "=" }, priority = 5, color = "purple" },
+		Search = {
+			text = { "-", "=" },
+			priority = 0,
+			color = nil,
+			cterm = nil,
+			highlight = "Search",
+		},
+		Error = {
+			text = { "-", "=" },
+			priority = 1,
+			color = nil,
+			cterm = nil,
+			highlight = "DiagnosticVirtualTextError",
+		},
+		Warn = {
+			text = { "-", "=" },
+			priority = 2,
+			color = nil,
+			cterm = nil,
+			highlight = "DiagnosticVirtualTextWarn",
+		},
+		Info = {
+			text = { "-", "=" },
+			priority = 3,
+			color = nil,
+			cterm = nil,
+			highlight = "DiagnosticVirtualTextInfo",
+		},
+		Hint = {
+			text = { "-", "=" },
+			priority = 4,
+			color = nil,
+			cterm = nil,
+			highlight = "DiagnosticVirtualTextHint",
+		},
+		Misc = {
+			text = { "-", "=" },
+			priority = 5,
+			color = nil,
+			cterm = nil,
+			highlight = "Normal",
+		},
+	},
+	excluded_buftypes = {
+		"terminal",
 	},
 	excluded_filetypes = {
 		"prompt",
 		"TelescopePrompt",
-	},
-	excluded_buftypes = {
-		"terminal",
 	},
 	autocmd = {
 		render = {
@@ -34,6 +73,6 @@ require("scrollbar").setup({
 	},
 	handlers = {
 		diagnostic = true,
-		search = true, -- Requires hlslens to be loaded
+		search = true, -- Requires hlslens to be loaded, will run require("scrollbar.handlers.search").setup() for you
 	},
 })
--- .config/nvim/rc/pluginconfig/nvim-hlslens.vim
+++ .config/nvim/rc/pluginconfig/nvim-hlslens.vim
@@ -1,4 +1,3 @@
-execute "lua require('hlslens').setup()"
 noremap n <Cmd>execute('normal! ' . v:count1 . 'n')<CR>
       \<Cmd>lua require('hlslens').start()<CR>
 noremap N <Cmd>execute('normal! ' . v:count1 . 'N')<CR>
