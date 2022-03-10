3a1010c6f5145e1e3f3bfde69bd0924c9905609b
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-03-10 10:28:42 +0900

    neovim: Fix the redundant statement

--- .config/nvim/lua/rc/pluginlist.lua
+++ .config/nvim/lua/rc/pluginlist.lua
@@ -1,10 +1,6 @@
-local execute = vim.api.nvim_command
-local fn = vim.fn
-
-local install_path = fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
-
-if fn.empty(fn.glob(install_path)) > 0 then
-	execute("silent !git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+	vim.api.nvim_command("silent !git clone https://github.com/wbthomason/packer.nvim " .. install_path)
 end
 if vim.fn.executable("python3") == 1 then
 	vim.cmd(
