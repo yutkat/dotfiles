a8c8a4ffd1bd0691d101aac7645e9051e4f07d6f
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-03-16 14:29:45 +0900

    neovim: Use nighfox's cache

--- .config/nvim/lua/rc/pluginconfig/nightfox.lua
+++ .config/nvim/lua/rc/pluginconfig/nightfox.lua
@@ -1,1 +1,7 @@
 vim.cmd([[ colorscheme nordfox ]])
+local nightfox = require("nightfox")
+local compile_path = vim.fn.stdpath("cache") .. "/nightfox"
+
+if vim.fn.isdirectory(compile_path) == 0 then
+	nightfox.compile()
+end
