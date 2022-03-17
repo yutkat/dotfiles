962565153813639acb6706a52c570f06218ba538
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-03-18 01:19:59 +0900

    neovim: Try other.nvim

---
+++ .config/nvim/lua/rc/pluginconfig/other.lua
@@ -0,0 +1,22 @@
+require("other-nvim").setup({
+	mappings = {
+		-- builtin mappings
+		"livewire",
+		"angular",
+		"laravel",
+		-- custom mapping
+		{
+			pattern = ".config/nvim/lua/rc/pluginconfig/(.*).lua$",
+			target = "../.local/share/nvim/site/pack/packer/opt/%1/README.md",
+			-- transformer = "lowercase",
+		},
+	},
+	transformers = {
+		-- defining a custom transformer
+		lowercase = function(inputString)
+			return inputString:lower()
+		end,
+	},
+})
+
+vim.api.nvim_set_keymap("n", "<F6>", "<cmd>:Other<CR>", { noremap = true, silent = true })
