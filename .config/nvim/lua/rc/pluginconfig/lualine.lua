94aca6c0296b714c8f5ae2cc9d1ce18a1c0b18be
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-03-18 21:44:06 +0900

    neovim: Use laststatus=3

--- .config/nvim/lua/rc/pluginconfig/lualine.lua
+++ .config/nvim/lua/rc/pluginconfig/lualine.lua
@@ -154,6 +154,7 @@ require("lualine").setup({
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = {},
 		always_divide_middle = true,
+		globalstatus = true,
 	},
 	sections = sections_1,
 	inactive_sections = {
