ded240a8e748690b0b816463ccbfdeba5b10d405
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-03-18 11:44:32 +0900

    neovim: Cleanup plugins

--- .config/i3/scripts/focus-last.py
+++ .config/i3/scripts/focus-last.py
@@ -8,7 +8,7 @@ import threading
 from argparse import ArgumentParser
 import i3ipc
 
-SOCKET_FILE = '/tmp/i3_focus_last'
+SOCKET_FILE = "/tmp/i3_focus_last"
 MAX_WIN_HISTORY = 15
 UPDATE_DELAY = 0.5
 
@@ -22,33 +22,33 @@ UPDATE_DELAY = 0.5
 #    except OSError:
 #      exit("Could not create a child process")
 
+
 def daemonize():
-  def fork():
-    if os.fork():
-      sys.exit()
+    def fork():
+        if os.fork():
+            sys.exit()
 
-  def throw_away_io():
-    stdin = open(os.devnull, 'rb')
-    stdout = open(os.devnull, 'ab+')
-    stderr = open(os.devnull, 'ab+', 0)
+    def throw_away_io():
+        stdin = open(os.devnull, "rb")
+        stdout = open(os.devnull, "ab+")
+        stderr = open(os.devnull, "ab+", 0)
 
-    for (null_io, std_io) in zip((stdin, stdout, stderr),
-                                 (sys.stdin, sys.stdout, sys.stderr)):
-      os.dup2(null_io.fileno(), std_io.fileno())
+        for (null_io, std_io) in zip(
+            (stdin, stdout, stderr), (sys.stdin, sys.stdout, sys.stderr)
+        ):
+            os.dup2(null_io.fileno(), std_io.fileno())
 
-  fork()
-  os.setsid()
-  fork()
-  throw_away_io()
+    fork()
+    os.setsid()
+    fork()
+    throw_away_io()
 
 
 class FocusWatcher:
-
     def __init__(self):
         self.i3 = i3ipc.Connection(auto_reconnect=True)
-        self.i3.on('window::focus', self.on_window_focus)
-        self.listening_socket = socket.socket(socket.AF_UNIX,
-            socket.SOCK_STREAM)
+        self.i3.on("window::focus", self.on_window_focus)
+        self.listening_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         self.listening_socket.settimeout(None)
         if os.path.exists(SOCKET_FILE):
             os.remove(SOCKET_FILE)
@@ -71,9 +71,9 @@ class FocusWatcher:
             if UPDATE_DELAY != 0.0:
                 if self.focus_timer is not None:
                     self.focus_timer.cancel()
-                self.focus_timer = threading.Timer(UPDATE_DELAY,
-                                                self.update_windowlist,
-                                                [event.container.id])
+                self.focus_timer = threading.Timer(
+                    UPDATE_DELAY, self.update_windowlist, [event.container.id]
+                )
                 self.focus_timer.start()
             else:
                 self.update_windowlist(event.container.id)
@@ -90,7 +90,7 @@ class FocusWatcher:
 
         def read(conn):
             data = conn.recv(1024)
-            if data == b'switch':
+            if data == b"switch":
                 with self.window_list_lock:
                     tree = self.i3.get_tree()
                     # don't recovery from BrokenPipe
@@ -104,7 +104,7 @@ class FocusWatcher:
                         if window_id not in windows:
                             self.window_list.remove(window_id)
                         else:
-                            self.i3.command('[con_id=%s] focus' % window_id)
+                            self.i3.command("[con_id=%s] focus" % window_id)
                             break
             elif not data:
                 selector.unregister(conn)
@@ -123,18 +123,26 @@ class FocusWatcher:
         for t in (t_i3, t_server):
             t.start()
 
-if __name__ == '__main__':
-    parser = ArgumentParser(prog='focus-last.py',
-        description='''
+
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        prog="focus-last.py",
+        description="""
         Focus last focused window.
 
         This script should be launch from the .xsessionrc without argument.
 
         Then you can bind this script with the `--switch` option to one of your
         i3 keybinding.
-        ''')
-    parser.add_argument('--switch', dest='switch', action='store_true',
-        help='Switch to the previous window', default=False)
+        """,
+    )
+    parser.add_argument(
+        "--switch",
+        dest="switch",
+        action="store_true",
+        help="Switch to the previous window",
+        default=False,
+    )
     args = parser.parse_args()
 
     if not args.switch:
@@ -144,5 +152,5 @@ if __name__ == '__main__':
     else:
         client_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         client_socket.connect(SOCKET_FILE)
-        client_socket.send(b'switch')
+        client_socket.send(b"switch")
         client_socket.close()
