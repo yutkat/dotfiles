45ae8466c184e0c4117d830df8e117361a4bfe18
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-03-20 14:30:41 +0900

    zsh: Add alias for git

--- .config/zsh/rc/pluginconfig/zsh-abbrev-alias_atinit.zsh
+++ .config/zsh/rc/pluginconfig/zsh-abbrev-alias_atinit.zsh
@@ -7,8 +7,10 @@ abbrev-alias -eg ~~='$(git rev-parse --show-cdup)'
 
 # git
 abbrev-alias gc='git commit'
+abbrev-alias ga='git add .'
 abbrev-alias gb='git branch'
 abbrev-alias gd='git diff HEAD'
 abbrev-alias gs='git status --short'
+abbrev-alias gr='gh renew'
 abbrev-alias pr='gh pr view -w || gh pr create -w'
 
