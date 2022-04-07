83e6305d828917f4ef6cd688928dc6defa0ca724
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-04-07 21:50:47 +0900

    neovim: Disable the mysterious black.vim

--- .config/nvim/lua/rc/base.lua
+++ .config/nvim/lua/rc/base.lua
@@ -31,5 +31,6 @@ vim.g.loaded_zipPlugin = 1
 vim.g.loaded_python_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
+vim.g.load_black = 1
 vim.g.do_filetype_lua = 1
 vim.g.did_load_filetypes = 0
