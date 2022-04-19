547fb9a6818dfdd1e7ac9b4c5147a667065abafb
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-04-19 16:12:09 +0900

    installer: Change font install directory to local

--- install_scripts/lib/dotsinstaller/install-font.sh
+++ install_scripts/lib/dotsinstaller/install-font.sh
@@ -9,16 +9,18 @@ source $(dirname "${BASH_SOURCE[0]:-$0}")/utilfuncs.sh
 # sudo curl -L https://github.com/tomokuni/Myrica/raw/master/product/MyricaM.zip -o /tmp/MyricaM.zip
 # (cd /tmp && sudo unzip -o MyricaM.zip -d /usr/share/fonts/myrica)
 
+mkdir -p ~/.local/share/fonts
+
 # udev gothic
-sudo mkdir -p /usr/share/fonts/UDEVGothic
+mkdir -p ~/.local/share/fonts/UDEVGothic
 UDEV_GOTHIC_RELEASES_URL="https://api.github.com/repos/yuru7/udev-gothic/releases"
-sudo curl -sfL "${UDEV_GOTHIC_RELEASES_URL}" | jq -r '.[0].assets | .[].browser_download_url' | grep _NF_ | xargs -I{} curl -fL -o /tmp/UDEVGothic.zip "{}"
-(cd /tmp && sudo unzip -j -o UDEVGothic.zip -d /usr/share/fonts/UDEVGothic)
+curl -sfL "${UDEV_GOTHIC_RELEASES_URL}" | jq -r '.[0].assets | .[].browser_download_url' | grep _NF_ | xargs -I{} curl -fL -o /tmp/UDEVGothic.zip "{}"
+(cd /tmp && unzip -j -o UDEVGothic.zip -d ~/.local/share/fonts/UDEVGothic)
 
 # cica
-sudo mkdir -p /usr/share/fonts/cica
+mkdir -p ~/.local/share/fonts/cica
 CICA_RELEASES_URL="https://api.github.com/repos/miiton/Cica/releases"
-sudo curl -sfL "${CICA_RELEASES_URL}" | jq -r '.[0].assets | .[].browser_download_url' | grep -i without_emoji.zip | xargs -I{} curl -fL -o /tmp/Cica.zip "{}"
-(cd /tmp && sudo unzip -o Cica.zip -d /usr/share/fonts/cica)
+curl -sfL "${CICA_RELEASES_URL}" | jq -r '.[0].assets | .[].browser_download_url' | grep -i without_emoji.zip | xargs -I{} curl -fL -o /tmp/Cica.zip "{}"
+(cd /tmp && unzip -o Cica.zip -d ~/.local/share/fonts/cica)
 
-sudo fc-cache -vf
+fc-cache -vf
