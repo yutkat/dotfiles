c7d6c939345deaa03416ff24844a4aa3c67e277c
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-04-29 19:38:42 +0900

    zsh: Update the function to create minimal init.lua

--- .config/zsh/rc/function.zsh
+++ .config/zsh/rc/function.zsh
@@ -608,10 +608,12 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 end
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function()
-  use {'wbthomason/packer.nvim', opt = true}
-end)
---- do not write under this line because it was returned
+local function load_plugins()
+  return require('packer').startup(function()
+    use {'wbthomason/packer.nvim', opt = true}
+  end)
+end
+load_plugins()
 EOF
 
   pwd
