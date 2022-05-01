3852b1a52e3b87f69a1dd94fb648d8025e0d1005
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-05-01 18:53:02 +0900

    neovim: Update spellchecker

--- .config/nvim/lua/rc/pluginconfig/null-ls.lua
+++ .config/nvim/lua/rc/pluginconfig/null-ls.lua
@@ -1,5 +1,15 @@
 local null_ls = require("null-ls")
 
+local function file_exists(fname)
+	local stat = vim.loop.fs_stat(vim.fn.expand(fname))
+	return (stat and stat.type) or false
+end
+
+local spell_args = { "-" }
+if file_exists("./.nvim/ignore_codespell.txt") then
+	spell_args = { "--ignore-words=./.nvim/ignore_codespell.txt", "-" }
+end
+
 null_ls.setup({
 	sources = {
 		-- LuaFormatter off
@@ -12,7 +22,10 @@ null_ls.setup({
 		null_ls.builtins.diagnostics.eslint,
 		null_ls.builtins.formatting.shfmt,
 		null_ls.builtins.diagnostics.shellcheck,
-		null_ls.builtins.diagnostics.codespell,
+		null_ls.builtins.diagnostics.codespell.with({
+			args = spell_args,
+		}),
+		-- create
 		null_ls.builtins.formatting.markdownlint,
 		null_ls.builtins.code_actions.gitsigns,
 		-- LuaFormatter on
