e49ccdbd56850389643324b1113a9d317e783ba5
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-05-02 02:12:28 +0900

    neovim: Change spellchecker from codespell to cspell

---
+++ .config/nvim/lua/rc/utils.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+function M.merge_tables(t1, t2)
+	for k, v in pairs(t2) do
+		if (type(v) == "table") and (type(t1[k] or false) == "table") then
+			M.merge_tables(t1[k], t2[k])
+		else
+			t1[k] = v
+		end
+	end
+	return t1
+end
+
+function M.merge_lists(t1, t2)
+	local result = {}
+	for _, v in pairs(t1) do
+		table.insert(result, v)
+	end
+	for _, v in pairs(t2) do
+		table.insert(result, v)
+	end
+	return result
+end
+
+return M
