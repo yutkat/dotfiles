88d2f167b43e26097a17866381115a584c0726a1
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-05-04 19:36:43 +0900

    neovim: Convert the my plugins to lua

---
+++ .config/nvim/lua/rc/myplugins/hexedit.lua
@@ -0,0 +1,21 @@
+vim.api.nvim_create_user_command("HexEditSplit16BytePerLine", function()
+	vim.cmd([[normal! 0]])
+	while vim.fn.len(vim.fn.expand("<cword>")) ~= 0 do
+		local cur = vim.fn.line(".")
+		vim.cmd([[normal! 016E]])
+		if vim.fn.line(".") ~= cur then
+			break
+		end
+		vim.cmd([[execute "normal! a\<CR>"]])
+	end
+end, { force = true, bar = true, range = "%" })
+
+vim.api.nvim_create_user_command("HexEditAdd0x", function()
+	vim.cmd([['<,'>s/\%V\(\w\)\(\w\)/0x\1\2/g]])
+end, { force = true, bar = true, range = "%" })
+vim.api.nvim_create_user_command("HexEditAddCommaEachWord", function()
+	vim.cmd([['<,'>s/\%V\(\<\w*\>\)/\1\,/g]])
+end, { force = true, bar = true, range = "%" })
+vim.api.nvim_create_user_command("HexEditConvertHexForProg", function()
+	vim.cmd([['<,'>HexEditAdd0x | '<,'>HexEditAddCommaEachWord | '<,'>HexEditSplit16BytePerLine]])
+end, { force = true, bar = true, range = "%" })
