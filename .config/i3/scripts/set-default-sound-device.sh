49324b7811117bf72e077f53b79574885e29c34f
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-05-12 03:10:51 +0900

    sway: Fix pipewire-pulse

--- .config/i3/scripts/set-default-sound-device.sh
+++ .config/i3/scripts/set-default-sound-device.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
-# https://github.com/vivien/i3blocks-contrib/blob/0d1cbe08d5864b361682d0eee9724f9bbcbe9bd9/volume-pulseaudio/volume-pulseaudio#L89-L106
-function move_sinks_to_new_default() {
-  DEFAULT_SINK=$1
-  pacmd list-sink-inputs | grep index: | grep -o '[0-9]\+' | while read -r SINK; do
-    pacmd move-sink-input "$SINK" "$DEFAULT_SINK"
-  done
+# https://github.com/f-viktor/dotfiles/blob/94e1fc9940840c0ba471087340447c631bb4d009/config/polybar/audio-output/audio-output.sh
+function move_sinks_to_new_default {
+	local DEFAULT_SINK=$1
+	local SINK
+	pactl list sink-inputs short | awk '{print $1}' | while read -r SINK; do
+		pactl move-sink-input "$SINK" "$DEFAULT_SINK"
+	done
 }
 
-function set_default_playback_device_next() {
-  inc=1
-  num_devices=$(pacmd list-sinks | grep -c index:)
-  mapfile -t sink_arr < <(pacmd list-sinks | grep index: | grep -o '[0-9]\+')
-  default_sink_index=$(($(pacmd list-sinks | grep index: | grep -no '*' | grep -o '^[0-9]\+') - 1))
-  default_sink_index=$((("$default_sink_index" + "$num_devices" + "$inc") % "$num_devices"))
-  default_sink=${sink_arr[$default_sink_index]}
-  pacmd set-default-sink "$default_sink"
-  move_sinks_to_new_default "$default_sink"
+function set_default_playback_device_next {
+	local inc=-1
+	num_devices=$(pactl list sinks short | wc -l)
+	mapfile -t sink_arr < <(pactl list sinks short | awk '{print $1}')
+	default_sink_index=$(($(pactl list sinks short | grep -no "$(pactl get-default-sink)" | grep -o '^[0-9]\+') - 1))
+	default_sink_index=$((("$default_sink_index" + "$num_devices" + "$inc") % "$num_devices"))
+	default_sink=${sink_arr[$default_sink_index]}
+	pactl set-default-sink "$default_sink"
+	move_sinks_to_new_default "$default_sink"
 }
 
 set_default_playback_device_next
