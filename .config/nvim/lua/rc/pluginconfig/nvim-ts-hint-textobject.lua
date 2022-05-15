c776270d1de8882806f099971fa866ee77067baa
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-05-15 23:54:05 +0900

    neovim: Reduce plugins

--- .config/nvim/lua/rc/pluginconfig/nvim-ts-hint-textobject.lua
+++
@@ -1,2 +0,0 @@
-vim.api.nvim_set_keymap("o", "m", "<Cmd>lua require('tsht').nodes()<CR>", { noremap = false, silent = false })
-vim.api.nvim_set_keymap("x", "m", "<Cmd>lua require('tsht').nodes()<CR>", { noremap = true, silent = false })
