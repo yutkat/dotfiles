2696c2c458f20e2c61896149b7cbc59045deb9d1
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-05-28 02:17:21 +0900

    neovim: Fix lastposition

--- .config/nvim/lua/rc/myplugins/start/lastposition.lua
+++ .config/nvim/lua/rc/myplugins/start/lastposition.lua
@@ -1,12 +1,12 @@
 local groupname = "lastposition"
 vim.api.nvim_create_augroup(groupname, { clear = true })
 
-vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+vim.api.nvim_create_autocmd({ "BufRead" }, {
 	group = groupname,
 	pattern = "*",
 	callback = function()
 		if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
-			vim.cmd([[normal g`"]])
+			vim.cmd([[normal! g`"]])
 		end
 	end,
 	once = false,
