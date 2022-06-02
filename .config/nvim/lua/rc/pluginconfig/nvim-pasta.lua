78a8fa77e7dfd755144e3caf4cead73b350c6a08
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-06-02 17:29:11 +0900

    neovim: Add hrsh7th/nvim-pasta

---
+++ .config/nvim/lua/rc/pluginconfig/nvim-pasta.lua
@@ -0,0 +1,12 @@
+vim.keymap.set("n", "p", require("pasta.mappings").p)
+vim.keymap.set("n", "P", require("pasta.mappings").P)
+
+-- This is the default. You can omit `setup` call if you don't want to change this.
+require("pasta").setup({
+	converters = {
+		require("pasta.converters").indentation,
+	},
+	paste_mode = true,
+	next_key = vim.api.nvim_replace_termcodes("<C-p>", true, true, true),
+	prev_key = vim.api.nvim_replace_termcodes("<C-n>", true, true, true),
+})
