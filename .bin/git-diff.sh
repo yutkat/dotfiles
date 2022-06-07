6d158e17bb40c4a4a870e961cf5a503cee1603cd
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-06-07 23:55:30 +0900

    git: Fix the wrong pager command #2911

--- .bin/git-diff.sh
+++ .bin/git-diff.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-
 if [[ -x ~/.zsh/zfunc/delta ]]; then
-  ~/.zsh/zfunc/delta "$@"
+	~/.zsh/zfunc/delta "$@"
 elif builtin command -v "delta" >/dev/null 2>&1; then
-  delta "$@"
+	delta "$@"
 elif [[ -x /usr/share/git/diff-highlight/diff-highlight ]]; then
-  /usr/share/git/diff-highlight/diff-highlight "$@"
+	/usr/share/git/diff-highlight/diff-highlight "$@"
+elif builtin command -v "bat" >/dev/null 2>&1; then
+	bat "$@"
 else
-  diff "$@"
+	cat "$@"
 fi
