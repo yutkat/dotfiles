9463fcf75d995f1eb97a715841fa5116b9c186d6
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-06-08 17:26:30 +0900

    neovim: Add xiyaowong/nvim-cursorword

--- .config/nvim/lua/rc/pluginconfig/nvim-lsp-installer.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-lsp-installer.lua
@@ -36,7 +36,7 @@ local on_attach = function(client, bufnr)
 	buf_set_keymap("n", "[lsp]f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 
 	-- require("lsp_signature").on_attach()
-	require("illuminate").on_attach(client)
+	-- require("illuminate").on_attach(client)
 end
 
 local lspconfig = require("lspconfig")
