36d51bbc7e8d6c387708c300ae2ccec2e88f7d40
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-06-18 03:48:53 +0900

    neovim: Update hop's mappings

--- .config/nvim/lua/rc/pluginconfig/hop.lua
+++ .config/nvim/lua/rc/pluginconfig/hop.lua
@@ -1,4 +1,26 @@
 require("hop").setup({})
 
-vim.api.nvim_set_keymap("n", "S", "<cmd>lua require'hop'.hint_words()<CR>", {})
-vim.api.nvim_set_keymap("x", "S", "<cmd>lua require'hop'.hint_words()<CR>", {})
+vim.api.nvim_set_keymap(
+	"n",
+	"Ss",
+	"<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR })<CR>",
+	{}
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"SS",
+	"<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR })<CR>",
+	{}
+)
+vim.api.nvim_set_keymap(
+	"x",
+	"Ss",
+	"<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR })<CR>",
+	{}
+)
+vim.api.nvim_set_keymap(
+	"x",
+	"SS",
+	"<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR })<CR>",
+	{}
+)
