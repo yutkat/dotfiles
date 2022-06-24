cda1a725d2376efee0c67af8a95caa2ea6851999
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-06-24 11:32:55 +0900

    zsh: Use osc7

--- .config/zsh/rc/pluginlist.zsh
+++ .config/zsh/rc/pluginlist.zsh
@@ -447,6 +447,9 @@ zinit wait'1' lucid \
 zinit wait'1' lucid \
   atload"source $ZHOMEDIR/rc/pluginconfig/mru.zsh_atload.zsh" \
   light-mode for "$ZHOMEDIR/rc/myplugins/mru.zsh/"
+zinit wait'1' lucid \
+  pick"*.sh" \
+  light-mode for "$ZHOMEDIR/rc/myplugins/vte/"
 # zinit wait'2' lucid \
 #   light-mode for "$ZHOMEDIR/rc/myplugins/coc-project.zsh/"
 
