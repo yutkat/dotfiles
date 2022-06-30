7da9f7f8ac4d358908654fee1d6ca300d92d00d5
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-07-01 02:32:03 +0900

    installer: Fix to not use appimage neovim

--- install_scripts/lib/dotsinstaller/install-neovim.sh
+++ install_scripts/lib/dotsinstaller/install-neovim.sh
@@ -2,50 +2,9 @@
 
 set -ue
 
-source $(dirname "${BASH_SOURCE[0]:-$0}")/utilfuncs.sh
-
-# stable
-# if ! builtin command -v nvim > /dev/null 2>&1; then
-#   checkinstall neovim
-# fi
-
 function neovim_nightly() {
-	# nightly
-	curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-	chmod u+x nvim.appimage
-	sudo mv nvim.appimage /usr/local/bin
-	sudo ln -snf /usr/local/bin/nvim.appimage /usr/local/bin/nvim
-	checkinstall fuse
+	mkdir -p ~/.local/
+	curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz | sudo tar zx --strip-components 1 -C ~/.local/
 }
 
-distro=$(whichdistro)
-if [[ $distro == "redhat" ]]; then
-	sudo dnf install -y fuse-sshfs
-	neovim_nightly
-	# sudo python3 -m pip install -U pynvim || true
-	# curl -sL install-node.now.sh/lts | sudo bash -s -- -f # coc.nvim
-elif [[ $distro == "arch" ]]; then
-	sudo pacman -S --noconfirm --needed base-devel
-	# sudo python3 -m pip install -U pynvim
-	# checkinstall nodejs # coc.nvim
-
-	# sudo pacman -S --noconfirm --needed fuse
-	# sudo modprobe fuse
-	# sudo groupadd fuse
-	# user="$(whoami)"
-	# sudo usermod -a -G fuse $user
-elif [[ $distro == "alpine" ]]; then
-	neovim_nightly
-	sudo apk add python3 gcc libc-dev procps perl ncurses coreutils python3-dev
-	# sudo python3 -m pip install -U pynvim
-	# checkinstall nodejs # coc.nvim
-elif [[ $distro == "debian" ]]; then
-	neovim_nightly
-	sudo pip3 install -U setuptools
-	# sudo pip3 install -U pynvim
-	# curl -sL install-node.now.sh/lts | sudo bash -s -- -f # coc.nvim
-else
-	neovim_nightly
-	sudo python3 -m pip install -U pynvim
-	# curl -sL install-node.now.sh/lts | sudo bash -s -- -f # coc.nvim
-fi
+neovim_nightly
