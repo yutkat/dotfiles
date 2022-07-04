4a3b2db1bd9665d0d0c35bc10b31df7386488533
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-07-04 17:38:49 +0900

    installer: Fix the permission on ~/.local (#2977)

--- install_scripts/lib/dotsinstaller/install-neovim.sh
+++ install_scripts/lib/dotsinstaller/install-neovim.sh
@@ -4,7 +4,7 @@ set -ue
 
 function neovim_nightly() {
 	mkdir -p ~/.local/
-	curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz | sudo tar zx --strip-components 1 -C ~/.local/
+	curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz | tar zx --strip-components 1 -C ~/.local/
 }
 
 neovim_nightly
