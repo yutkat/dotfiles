c8d7765b07c8a543ef057b41f570f117c8b9c53e
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-07-05 22:21:30 +0900

    neovim: Change the plugin from nvim-gps to nvim-navic

--- .config/nvim/lua/rc/pluginconfig/nvim-lsp-installer.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-lsp-installer.lua
@@ -37,6 +37,7 @@ local on_attach = function(client, bufnr)
 
 	-- require("lsp_signature").on_attach()
 	-- require("illuminate").on_attach(client)
+	require("nvim-navic").attach(client, bufnr)
 end
 
 local lspconfig = require("lspconfig")
