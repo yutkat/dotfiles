7c6d884ffbd2654a73065229ed1f6c75cf52d7e7
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-07-19 12:35:09 +0900

    neovim: Update nvim-surround

--- .config/nvim/lua/rc/pluginconfig/nvim-surround.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-surround.lua
@@ -56,3 +56,21 @@ require("nvim-surround").setup({
 		duration = 0,
 	},
 })
+
+-- vim.keymap.set("o", ";", function()
+-- 	local current_operator = vim.v.operator
+-- 	local utils = require("nvim-surround.utils")
+-- 	local sel = utils.get_nearest_selections(";")
+-- 	if sel == nil or sel.left == nil then
+-- 		return
+-- 	end
+-- 	local char = vim.api.nvim_buf_get_text(
+-- 		0,
+-- 		sel.left.first_pos[1] - 1,
+-- 		sel.left.first_pos[2] - 1,
+-- 		sel.left.first_pos[1] - 1,
+-- 		sel.left.first_pos[2],
+-- 		{}
+-- 	)
+-- 	vim.cmd(current_operator .. "i" .. char[1])
+-- end, { noremap = false, expr = false, silent = true })
