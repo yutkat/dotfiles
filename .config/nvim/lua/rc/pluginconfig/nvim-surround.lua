b528437b14c7f950648d515acc6225edcf1b0e47
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-08-06 15:11:34 +0900

    neovim: Update nvim-surround

--- .config/nvim/lua/rc/pluginconfig/nvim-surround.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-surround.lua
@@ -10,50 +10,15 @@ require("nvim-surround").setup({
 		delete = "sd",
 		change = "sr",
 	},
-	delimiters = {
-		pairs = {
-			["("] = { "( ", " )" },
-			[")"] = { "(", ")" },
-			["{"] = { "{ ", " }" },
-			["}"] = { "{", "}" },
-			["<"] = { "< ", " >" },
-			[">"] = { "<", ">" },
-			["["] = { "[ ", " ]" },
-			["]"] = { "[", "]" },
-			-- Define pairs based on function evaluations!
-			["i"] = function()
-				return {
-					require("nvim-surround.utils").get_input("Enter the left delimiter: "),
-					require("nvim-surround.utils").get_input("Enter the right delimiter: "),
-				}
-			end,
-			["f"] = function()
-				return {
-					require("nvim-surround.utils").get_input("Enter the function name: ") .. "(",
-					")",
-				}
-			end,
-		},
-		separators = {
-			["'"] = { "'", "'" },
-			['"'] = { '"', '"' },
-			["`"] = { "`", "`" },
-		},
-		HTML = {
-			["t"] = "type", -- Change just the tag type
-			["T"] = "whole", -- Change the whole tag contents
-		},
-		aliases = {
-			["a"] = "a",
-			["b"] = "b",
-			["B"] = "B",
-			["r"] = "r",
-			-- Table aliases only apply for changes/deletions
-			["q"] = { '"', "'", "`" }, -- Any quote character
-			[";"] = { ")", "]", "}", ">", "'", '"', "`" }, -- Any surrounding delimiter
-		},
+	aliases = {
+		["a"] = "a",
+		["b"] = "b",
+		["B"] = "B",
+		["r"] = "r",
+		["q"] = { '"', "'", "`" }, -- Any quote character
+		[";"] = { ")", "]", "}", ">", "'", '"', "`" }, -- Any surrounding delimiter
 	},
-	highlight_motion = { -- Highlight before inserting/changing surrounds
+	highlight = { -- Highlight before inserting/changing surrounds
 		duration = 0,
 	},
 })
