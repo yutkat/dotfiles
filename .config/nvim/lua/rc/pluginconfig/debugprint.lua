1a975b00075ccac5b29a2a811cdb15d443e1dd51
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-08-09 09:52:28 +0900

    neovim: Add andrewferrier/debugprint.nvim

---
+++ .config/nvim/lua/rc/pluginconfig/debugprint.lua
@@ -0,0 +1,7 @@
+require("debugprint").setup({ create_keymaps = false })
+vim.keymap.set("n", "sp", function()
+  require("debugprint").debugprint()
+end)
+vim.keymap.set("n", "sP", function()
+  require("debugprint").debugprint({ above = true })
+end)
