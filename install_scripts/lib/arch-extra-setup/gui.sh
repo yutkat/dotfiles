a10f27086ec9a9c90707c5a776dafd0aa2ef6de5
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-09-06 12:14:39 +0000

    installer: Fix installation on new PC

--- install_scripts/lib/arch-extra-setup/gui.sh
+++ install_scripts/lib/arch-extra-setup/gui.sh
@@ -6,5 +6,7 @@ sudo pacman -S --noconfirm --needed pipewire-pulse
 paru -S --noconfirm --needed i3-easyfocus-git wmfocus clipmenu
 paru -S --noconfirm --needed adwaita-qt
 paru -S --noconfirm --needed fcitx5-skin-adwaita-dark
+# flameshot on wayland https://github.com/flameshot-org/flameshot/blob/master/docs/Sway and wlroots support.md
+paru -S --noconfirm --needed xdg-desktop-portal xdg-desktop-portal-wlr grim
 
 #paru -S --noconfirm --needed hyprland-bin waybar-hyprland-git hyprpaper brightnessctl wlogout
