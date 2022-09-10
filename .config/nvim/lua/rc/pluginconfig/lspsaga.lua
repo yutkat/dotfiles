23b3f32f835e9624b6b41327a58f00b77deb7496
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-09-10 22:17:36 +0900

    neovim: Fix lspsaga's config

--- .config/nvim/lua/rc/pluginconfig/lspsaga.lua
+++ .config/nvim/lua/rc/pluginconfig/lspsaga.lua
@@ -1,47 +1,7 @@
 local lspsaga = require("lspsaga")
 lspsaga.init_lsp_saga({ -- defaults ...
-	-- Options with default value
-	-- "single" | "double" | "rounded" | "bold" | "plus"
-	border_style = "single",
-	--the range of 0 for fully opaque window (disabled) to 100 for fully
-	--transparent background. Values between 0-30 are typically most useful.
-	saga_winblend = 0,
-	-- when cursor in saga window you config these to move
-	move_in_saga = { prev = "<C-p>", next = "<C-n>" },
-	-- Error, Warn, Info, Hint
-	-- use emoji like
-	-- { "🙀", "😿", "😾", "😺" }
-	-- or
-	-- { "😡", "😥", "😤", "😐" }
-	-- and diagnostic_header can be a function type
-	-- must return a string and when diagnostic_header
-	-- is function type it will have a param `entry`
-	-- entry is a table type has these filed
-	-- { bufnr, code, col, end_col, end_lnum, lnum, message, severity, source }
-	diagnostic_header = { " ", " ", " ", "ﴞ " },
-	-- show diagnostic source
-	show_diagnostic_source = true,
-	-- add bracket or something with diagnostic source, just have 2 elements
-	diagnostic_source_bracket = {},
-	-- preview lines of lsp_finder and definition preview
-	max_preview_lines = 10,
 	-- use emoji lightbulb in default
 	code_action_icon = " ",
-	-- if true can press number to execute the codeaction in codeaction window
-	code_action_num_shortcut = true,
-	-- same as nvim-lightbulb but async
-	code_action_lightbulb = {
-		enable = true,
-		sign = true,
-		sign_priority = 20,
-		virtual_text = true,
-	},
-	-- finder icons
-	finder_icons = {
-		def = "  ",
-		ref = "諭 ",
-		link = "  ",
-	},
 	-- custom finder title winbar function type
 	-- param is current word with symbol icon string type
 	-- return a winbar format string like `%#CustomFinder#Test%*`
@@ -53,33 +13,6 @@ lspsaga.init_lsp_saga({ -- defaults ...
 		quit = "q",
 		scroll_down = "<C-f>",
 		scroll_up = "<C-b>", -- quit can be a table
-	},
-	code_action_keys = {
-		quit = "q",
-		exec = "<CR>",
-	},
-	rename_action_quit = "<C-c>",
-	definition_preview_icon = "  ",
-	-- show symbols in winbar must nightly
-	symbol_in_winbar = {
-		in_custom = false,
-		enable = false,
-		separator = " ",
-		show_file = true,
-		click_support = false,
-	},
-	-- show outline
-	show_outline = {
-		win_position = "right",
-		-- set the special filetype in there which in left like nvimtree neotree defx
-		left_with = "",
-		win_width = 30,
-		auto_enter = true,
-		auto_preview = true,
-		virt_text = "┃",
-		jump_key = "o",
-		-- auto refresh when change buffer
-		auto_refresh = true,
 	},
 })
 
