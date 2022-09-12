a3392811a8b0d07690225734285acd6ba044c7a1
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-09-12 13:42:14 +0900

    installer: Fix adwaita-qt

--- install_scripts/lib/arch-extra-setup/gui.sh
+++ install_scripts/lib/arch-extra-setup/gui.sh
@@ -5,7 +5,7 @@ sudo pacman -S --noconfirm --needed xorg-xhost ttf-font-awesome gnome-themes-ext
 sudo pacman -S --noconfirm --needed pipewire-pulse
 #paru -S --noconfirm --needed i3-easyfocus-git
 paru -S --noconfirm --needed wmfocus clipmenu
-paru -S --noconfirm --needed adwaita-qt
+sudo pacman -S --noconfirm --needed adwaita-qt5 adwaita-qt6
 paru -S --noconfirm --needed fcitx5-skin-adwaita-dark
 # flameshot on wayland https://github.com/flameshot-org/flameshot/blob/master/docs/Sway and wlroots support.md
 paru -S --noconfirm --needed xdg-desktop-portal xdg-desktop-portal-wlr grim
