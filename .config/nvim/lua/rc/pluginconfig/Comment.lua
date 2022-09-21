f637ce4a7001b3a7bb02291d629091f6aa2a943c
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-09-21 15:34:34 +0900

    wezterm: Update copy mode's keybinds

--- .config/nvim/lua/rc/pluginconfig/Comment.lua
+++ .config/nvim/lua/rc/pluginconfig/Comment.lua
@@ -50,3 +50,6 @@ require("Comment").setup({
 vim.api.nvim_set_keymap("n", "<C-_>", "<Cmd>lua require('Comment.api').toggle.linewise.current()<CR>", {})
 vim.api.nvim_set_keymap("i", "<C-_>", "<Esc>:<C-u>lua require('Comment.api').toggle.linewise.current()<CR>\"_cc", {})
 vim.api.nvim_set_keymap("v", "<C-_>", "gc", {})
+vim.api.nvim_set_keymap("n", "<C-/>", "<Cmd>lua require('Comment.api').toggle.linewise.current()<CR>", {})
+vim.api.nvim_set_keymap("i", "<C-/>", "<Esc>:<C-u>lua require('Comment.api').toggle.linewise.current()<CR>\"_cc", {})
+vim.api.nvim_set_keymap("v", "<C-/>", "gc", {})
