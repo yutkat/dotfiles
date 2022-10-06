feda2dfbf0ce02d74b03deb65ff0f1bce2e778c5
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-10-06 23:26:26 +0900

    neovim: Add the splitkeep option

--- .config/nvim/lua/rc/pluginconfig/live-command.lua
+++ .config/nvim/lua/rc/pluginconfig/live-command.lua
@@ -1,5 +1,15 @@
-require("live_command").setup({
+require("live-command").setup({
 	commands = {
 		Norm = { cmd = "norm" },
+	},
+	defaults = {
+		enable_highlighting = true,
+		inline_highlighting = true,
+		hl_groups = {
+			insertion = "DiffAdd",
+			deletion = "DiffDelete",
+			change = "DiffChange",
+		},
+		debug = false,
 	},
 })
