d6899d01720495019909a8367159eebf2c643dd0
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-11-10 13:50:57 +0900

    neovim: Fix prefix

--- .config/nvim/lua/rc/pluginconfig/qf_helper.lua
+++ .config/nvim/lua/rc/pluginconfig/qf_helper.lua
@@ -1,3 +1,3 @@
 require("qf_helper").setup()
-vim.keymap.set("n", "[SubLeader]q", "<Cmd>QFToggle!<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "[SubLeader]l", "<Cmd>LLToggle!<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "[_SubLeader]q", "<Cmd>QFToggle!<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "[_SubLeader]l", "<Cmd>LLToggle!<CR>", { noremap = true, silent = true })
