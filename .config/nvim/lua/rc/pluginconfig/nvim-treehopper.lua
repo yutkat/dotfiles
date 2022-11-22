4080f437919f738472e0a7f7a1567375ec3e33c5
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-11-22 22:07:44 +0900

    neovim: Fix keymaps

--- .config/nvim/lua/rc/pluginconfig/nvim-treehopper.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-treehopper.lua
@@ -1,6 +1,4 @@
 vim.keymap.set({ "o" }, "m", function()
 	require("tsht").nodes()
 end, { noremap = false, expr = false, silent = true })
-vim.keymap.set({ "x" }, "m", function()
-	require("tsht").nodes()
-end, { noremap = true, expr = false, silent = true })
+vim.keymap.set({ "x" }, "m", ":lua require('tsht').nodes()<CR>", { noremap = true, expr = false, silent = true })
