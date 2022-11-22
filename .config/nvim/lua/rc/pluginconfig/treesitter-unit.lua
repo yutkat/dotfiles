05e62bceab8687076a51ae66ddf41794ea43a8ec
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-11-22 22:07:44 +0900

    neovim: Fix keymaps

--- .config/nvim/lua/rc/pluginconfig/treesitter-unit.lua
+++ .config/nvim/lua/rc/pluginconfig/treesitter-unit.lua
@@ -2,5 +2,5 @@ vim.api.nvim_set_keymap("x", "iu", ':lua require"treesitter-unit".select()<CR>',
 vim.api.nvim_set_keymap("x", "au", ':lua require"treesitter-unit".select(true)<CR>', { noremap = true })
 vim.api.nvim_set_keymap("o", "iu", ':<c-u>lua require"treesitter-unit".select()<CR>', { noremap = true })
 vim.api.nvim_set_keymap("o", "au", ':<c-u>lua require"treesitter-unit".select(true)<CR>', { noremap = true })
-vim.api.nvim_set_keymap("x", "/", ':lua require"treesitter-unit".select()<CR>', { noremap = true })
-vim.api.nvim_set_keymap("o", "/", ':<c-u>lua require"treesitter-unit".select()<CR>', { noremap = true })
+vim.api.nvim_set_keymap("x", "u", ':lua require"treesitter-unit".select()<CR>', { noremap = true })
+vim.api.nvim_set_keymap("o", "u", ':<c-u>lua require"treesitter-unit".select()<CR>', { noremap = true })
