87222dcfcc5332852f13a8e92c860894fd87f694
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-11-24 17:59:48 +0900

    neovim: Update nvim-test

--- .config/nvim/lua/rc/pluginconfig/nvim-test.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-test.lua
@@ -1,1 +1,10 @@
-require("nvim-test").setup()
+require("nvim-test").setup({
+	termOpts = {
+		direction = "horizontal", -- terminal's direction ("horizontal"|"vertical"|"float")
+		height = 30, -- terminal's height (for horizontal|float)
+		go_back = true,
+	},
+})
+
+vim.keymap.set("n", "[_Make]n", "<Cmd>TestSuite<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "[_Make]l", "<Cmd>TestLast<CR>", { noremap = true, silent = true })
