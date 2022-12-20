1e02c1cc4a698d584e9c67eb7245bc2346b8144d
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2022-12-20 15:30:58 +0900

    neovim: Fix autopair

--- .config/nvim/lua/rc/pluginconfig/nvim-cmp.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-cmp.lua
@@ -217,4 +217,4 @@ cmp.setup.cmdline(":", {
 
 -- autopairs
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
