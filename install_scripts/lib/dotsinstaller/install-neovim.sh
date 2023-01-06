e4c69b96e8c8214a0a79b21b8c0715799dccbc0b
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2023-01-06 23:40:07 +0900

    neovim: Fix benchmark

--- install_scripts/lib/dotsinstaller/install-neovim.sh
+++ install_scripts/lib/dotsinstaller/install-neovim.sh
@@ -5,6 +5,9 @@ set -ue
 function neovim_nightly() {
 	mkdir -p ~/.local/
 	curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz | tar zx --strip-components 1 -C ~/.local/
+	# for nvim-treesitter
+	# https://github.com/nvim-treesitter/nvim-treesitter/blob/68e8181dbcf29330716d380e5669f2cd838eadb5/lua/nvim-treesitter/install.lua#L14
+	sudo pacman -S --noconfirm --needed zig
 }
 
 neovim_nightly
