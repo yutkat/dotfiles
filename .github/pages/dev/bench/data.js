045021b155e21f56c60a51f12f512f1b574c05d1
github-action-benchmark <github@users.noreply.github.com>
2023-01-07 17:18:49 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 383b2d35b80b0cb6069811b31ff5c87ceb5b0436

--- .github/pages/dev/bench/data.js
+++ .github/pages/dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1673110124281,
+  "lastUpdate": 1673111929117,
   "repoUrl": "https://github.com/yutkat/dotfiles",
   "entries": {
     "Benchmark": [
@@ -20601,6 +20601,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "neovim load time",
             "value": 0.572,
+            "unit": "Second"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "8683947+yutkat@users.noreply.github.com",
+            "name": "Yuta Katayama",
+            "username": "yutkat"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "383b2d35b80b0cb6069811b31ff5c87ceb5b0436",
+          "message": "Merge pull request #3223 from yutkat/v44.022\n\nCI: Fix the benchmark path",
+          "timestamp": "2023-01-08T02:01:59+09:00",
+          "tree_id": "ec099613fe58937ef71d6be64dbfa221e46b16a1",
+          "url": "https://github.com/yutkat/dotfiles/commit/383b2d35b80b0cb6069811b31ff5c87ceb5b0436"
+        },
+        "date": 1673111927573,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time",
+            "value": 1.85,
+            "unit": "Second"
+          },
+          {
+            "name": "neovim load time",
+            "value": 0.534,
             "unit": "Second"
           }
         ]
