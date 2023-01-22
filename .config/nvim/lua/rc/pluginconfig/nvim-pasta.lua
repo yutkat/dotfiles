c5fd75b9d655427244d798dc5be0f41876243d4d
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2023-01-22 16:05:26 +0900

    neovim: Update lua dev

--- .config/nvim/lua/rc/pluginconfig/nvim-pasta.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-pasta.lua
@@ -3,9 +3,7 @@ vim.keymap.set({ "n", "x" }, "P", require("pasta.mappings").P)
 
 -- This is the default. You can omit `setup` call if you don't want to change this.
 require("pasta").setup({
-	converters = {
-		require("pasta.converters").indentation,
-	},
+	converters = {},
 	paste_mode = true,
 	next_key = vim.api.nvim_replace_termcodes("<C-p>", true, true, true),
 	prev_key = vim.api.nvim_replace_termcodes("<C-n>", true, true, true),
