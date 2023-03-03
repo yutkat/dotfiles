7e14bea5c9cd251b39032bf3e1e83f56a381b9f1
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2023-03-04 01:37:25 +0900

    neovim: Fix portal.nvim

--- .config/nvim/lua/rc/pluginconfig/portal.lua
+++ .config/nvim/lua/rc/pluginconfig/portal.lua
@@ -1,3 +1,3 @@
 require("portal").setup()
-vim.keymap.set("n", "<leader>o", require("portal").jump_backward, {})
-vim.keymap.set("n", "<leader>i", require("portal").jump_forward, {})
+vim.keymap.set("n", "<Leader>o", "<Cmd>Portal jumplist backward<CR>")
+vim.keymap.set("n", "<Leader>i", "<Cmd>Portal jumplist forward<CR>")
