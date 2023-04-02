d9ca8e990709e51bb5ab0b15ca3e234c8da27697
github-action-benchmark <github@users.noreply.github.com>
2023-04-02 07:49:27 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for fc9b5ff0ea7fb36cc3c91554031e290451b2b9d5

--- .github/pages/dev/bench/data.js
+++ .github/pages/dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680421188989,
+  "lastUpdate": 1680421767140,
   "repoUrl": "https://github.com/yutkat/dotfiles",
   "entries": {
     "Benchmark": [
@@ -23146,6 +23146,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "zsh load time",
             "value": 1.63,
+            "unit": "Second"
+          },
+          {
+            "name": "neovim load time",
+            "value": 0,
+            "unit": "Second"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "8683947+yutkat@users.noreply.github.com",
+            "name": "Yuta Katayama",
+            "username": "yutkat"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fc9b5ff0ea7fb36cc3c91554031e290451b2b9d5",
+          "message": "Merge pull request #3301 from yutkat/v44.099\n\nmisc: Remove unused files",
+          "timestamp": "2023-04-02T16:41:29+09:00",
+          "tree_id": "37b40775ccc9a2af7880c5af16524ef6062d9bd6",
+          "url": "https://github.com/yutkat/dotfiles/commit/fc9b5ff0ea7fb36cc3c91554031e290451b2b9d5"
+        },
+        "date": 1680421765852,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time",
+            "value": 2.73,
             "unit": "Second"
           },
           {
