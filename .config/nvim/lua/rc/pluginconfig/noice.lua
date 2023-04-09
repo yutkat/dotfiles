672b23cd981ce858b3a797d1578d9969e37d7a2c
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2023-04-10 02:16:01 +0900

    neovim: Update noice.nvim

--- .config/nvim/lua/rc/pluginconfig/noice.lua
+++ .config/nvim/lua/rc/pluginconfig/noice.lua
@@ -1,13 +1,34 @@
 require("noice").setup({
 	popupmenu = {
+		-- cmp-cmdline has more sources and can be extended
 		backend = "cmp", -- backend to use to show regular cmdline completions
 	},
 	lsp = {
+		-- can not filter null-ls's data
+		-- j-hui/fidget.nvim
 		progress = {
 			enabled = false,
 		},
 	},
 	messages = {
+		-- Using kevinhwang91/nvim-hlslens because virtualtext is hard to read
 		view_search = false,
 	},
 })
+
+vim.keymap.set("c", "<S-Enter>", function()
+	require("noice").redirect(vim.fn.getcmdline())
+end, { desc = "Redirect Cmdline" })
+
+-- lspsaga
+-- vim.keymap.set({ "n", "i", "s" }, "<c-f>", function()
+-- 	if not require("noice.lsp").scroll(4) then
+-- 		return "<c-f>"
+-- 	end
+-- end, { silent = true, expr = true })
+--
+-- vim.keymap.set({ "n", "i", "s" }, "<c-b>", function()
+-- 	if not require("noice.lsp").scroll(-4) then
+-- 		return "<c-b>"
+-- 	end
+-- end, { silent = true, expr = true })
