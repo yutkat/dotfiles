3e6dddd163927f2dd88540e77c1dbabe66d16951
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2023-04-12 14:43:41 +0900

    neovim: Fix lint

--- .config/nvim/lua/rc/pluginconfig/nvim-cmp.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-cmp.lua
@@ -4,7 +4,8 @@ local cmp = require("cmp")
 local types = require("cmp.types")
 local luasnip = require("luasnip")
 local has_words_before = function()
-	local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+	local unpack = unpack or table.unpack ---@diagnostic disable-line: deprecated
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
