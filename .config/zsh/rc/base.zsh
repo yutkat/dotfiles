505111835c043324ff712db4915987358a47e5fe
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2023-06-19 13:15:28 +0900

    zsh: Fix zsh-autocomplete

--- .config/zsh/rc/base.zsh
+++ .config/zsh/rc/base.zsh
@@ -9,7 +9,7 @@ HISTSIZE=10000                    # Number of histories in memory
 SAVEHIST=100000                   # Number of histories to be saved
 HISTORY_IGNORE="(ls|cd|pwd|zsh|exit|cd ..)"
 LISTMAX=1000                      # number of completion listings to ask for (1=shut up, 0=ask when window overflows)
-KEYTIMEOUT=1
+# KEYTIMEOUT=1 # conflict with zsh-autocomplete
 
 # Do not add root commands to history
 # if [ "$UID" = 0 ]; then
