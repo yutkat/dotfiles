ddc30caa8e3e28f7c1d66d2d76c0f58ea61829f1
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2023-09-30 00:41:35 +0900

    neovim: Update indent-blankline's config

--- .config/nvim/lua/rc/pluginconfig/indent-blankline.lua
+++ .config/nvim/lua/rc/pluginconfig/indent-blankline.lua
@@ -1,45 +1,30 @@
 vim.g.indent_blankline_enabled = false
 
-require("indent_blankline").setup({
-	show_current_context = false,
-	use_treesitter = true,
-	buftype_exclude = { "terminal" },
-	filetype_exclude = {
-		"help",
-		"dashboard",
-		"dashpreview",
-		"NvimTree",
-		"neo-tree",
-		"vista",
-		"sagahover",
-		"sagasignature",
-		"packer",
-		"lazy",
-		"log",
-		"lspsagafinder",
-		"lspinfo",
-		"dapui_scopes",
-		"dapui_breakpoints",
-		"dapui_stacks",
-		"dapui_watches",
-		"dap-repl",
-		"toggleterm",
-		"alpha",
-		"coc-explorer",
-	},
-	-- char_highlight_list = {
-	--   "IndentBlanklineIndent1", "IndentBlanklineIndent1", "IndentBlanklineIndent1",
-	--   "IndentBlanklineIndent1", "IndentBlanklineIndent1", "IndentBlanklineIndent1",
-	--   "IndentBlanklineChar", "IndentBlanklineChar", "IndentBlanklineChar", "IndentBlanklineChar",
-	--   "IndentBlanklineChar", "IndentBlanklineChar", "IndentBlanklineChar", "IndentBlanklineChar",
-	--   "IndentBlanklineChar", "IndentBlanklineChar", "IndentBlanklineChar", "IndentBlanklineChar",
-	--   "IndentBlanklineChar", "IndentBlanklineChar", "IndentBlanklineChar", "IndentBlanklineChar",
-	--   "IndentBlanklineChar", "IndentBlanklineChar", "IndentBlanklineChar", "IndentBlanklineChar",
-	--   "IndentBlanklineChar", "IndentBlanklineChar", "IndentBlanklineChar", "IndentBlanklineChar",
-	--   "IndentBlanklineChar", "IndentBlanklineChar", "IndentBlanklineChar", "IndentBlanklineChar",
-	--   "IndentBlanklineChar", "IndentBlanklineChar", "IndentBlanklineChar", "IndentBlanklineChar",
-	--   "IndentBlanklineChar", "IndentBlanklineChar"
-	-- }
+require("ibl").overwrite({
+	exclude = {
+		buftypes = { "terminal" },
+		filetypes = {
+			"help",
+			"dashboard",
+			"dashpreview",
+			"NvimTree",
+			"neo-tree",
+			"vista",
+			"sagahover",
+			"sagasignature",
+			"packer",
+			"lazy",
+			"log",
+			"lspsagafinder",
+			"lspinfo",
+			"dapui_scopes",
+			"dapui_breakpoints",
+			"dapui_stacks",
+			"dapui_watches",
+			"dap-repl",
+			"toggleterm",
+			"alpha",
+			"coc-explorer",
+		},
+	}
 })
-
--- vim.api.nvim_clear_autocmds({ event = { "TextChanged", "TextChangedI" }, group = "IndentBlanklineAutogroup" })
