0291074158da81d501eb041fbd69858a95d235e2
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2023-10-27 12:12:07 +0900

    neovim: Fix mojibake

--- .config/nvim/lua/rc/pluginconfig/nvim-lspconfig.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-lspconfig.lua
@@ -1,4 +1,5 @@
-local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+local signs = { Error = "󰅚", Warn = "󰀪 ", Hint = "󰌶 ", Info = "󰋽 " }
+
 for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
