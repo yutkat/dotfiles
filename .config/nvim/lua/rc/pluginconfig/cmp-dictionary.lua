9030b32806a1e7025f4e635747a78ed871255a1c
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2024-01-22 11:16:38 +0900

    neovim: Fix cmp-dictionary

--- .config/nvim/lua/rc/pluginconfig/cmp-dictionary.lua
+++ .config/nvim/lua/rc/pluginconfig/cmp-dictionary.lua
@@ -1,25 +1,10 @@
 local dict = require("cmp_dictionary")
 dict.setup({
-	-- The following are default values.
-	exact = 2,
+	paths = { vim.fn.stdpath("data") .. "/../zsh/dictionary/my.dict" },
+	exact_length = 2,
 	first_case_insensitive = false,
-	document = false,
-	document_command = "wn %s -over",
-	async = false,
-	sqlite = false,
-	max_items = -1,
-	capacity = 5,
-	debug = false,
-})
-
-local file = vim.fn.stdpath("data") .. "/../zsh/dictionary/my.dict"
-local dic = {}
-if vim.fn.filereadable(file) ~= 0 then
-	dic = { file }
-end
-
-dict.switcher({
-	spelllang = {
-		en = dic,
+	document = {
+		enable = true,
+		command = { "wn", "${label}", "-over" },
 	},
 })
