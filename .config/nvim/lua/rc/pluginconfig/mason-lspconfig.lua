a67131a044a5055d27f65cd890b4520880e1bc1e
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2024-04-20 22:50:24 +0900

    neovim: Fix vim.lsp.inlay_hint.enable

--- .config/nvim/lua/rc/pluginconfig/mason-lspconfig.lua
+++ .config/nvim/lua/rc/pluginconfig/mason-lspconfig.lua
@@ -42,7 +42,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local bufnr = args.buf
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		if client.supports_method("textDocument/inlayHint") then
-			vim.lsp.inlay_hint.enable(bufnr, true)
+			vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
 
 			-- vim.api.nvim_create_autocmd("InsertEnter", {
 			-- 	buffer = bufnr,
