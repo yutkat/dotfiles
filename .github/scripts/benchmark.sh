4c7d7dd39d790d328299bd8ade112a1841ee733e
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2024-05-04 16:52:20 +0900

    CI: Fix benchmark

--- .github/scripts/benchmark.sh
+++ .github/scripts/benchmark.sh
@@ -2,22 +2,36 @@
 # shellcheck disable=SC2034
 # https://zenn.dev/odan/articles/17a86574b724c9
 set -eu
-{ for i in $(seq 1 10); do /usr/bin/time --format="%e" -o /dev/stdout zsh -li --rcs ~/.config/zsh/.zshrc -c exit; done; } >/tmp/zsh-load-time.txt 2>/dev/null
-ZSH_LOAD_TIME=$(awk '{ total += $1 } END { print total/NR }' /tmp/zsh-load-time.txt)
 
+# zsh
+if [ -e "/tmp/zsh-bench" ]; then
+  rm -rf "/tmp/zsh-bench"
+fi
+git clone --depth 1  https://github.com/romkatv/zsh-bench.git /tmp/zsh-bench
+ZSHRC_BENCH=true /tmp/zsh-bench/zsh-bench -i 1 | tee /tmp/zsh-bench.txt
+first_prompt_lag_ms="$(cat /tmp/zsh-bench.txt | grep 'first_prompt_lag_ms' | sed -n 's/.*=\(.*\)/\1/p')"
+first_command_lag_ms="$(cat /tmp/zsh-bench.txt | grep 'first_command_lag_ms' | sed -n 's/.*=\(.*\)/\1/p')"
+
+# neovim
 { for i in $(seq 1 10); do /usr/bin/time --format="%e" ~/.local/share/zsh/zinit/plugins/neovim---neovim/nvim-linux64/bin/nvim --headless -c "qall"; done; } >/dev/null 2>/tmp/nvim-load-time.txt
-NVIM_LOAD_TIME=$(awk '{ total += $1 } END { print total/NR }' /tmp/nvim-load-time.txt)
+NVIM_LOAD_TIME=$(awk '{ total += $1 } END { print total*1000/NR }' /tmp/nvim-load-time.txt)
 
-cat <<EOJ
+# result
+cat <<EOJ | tee /tmp/result-benchmark.json
 [
     {
-        "name": "zsh load time",
-        "unit": "Second",
-        "value": ${ZSH_LOAD_TIME}
+        "name": "zsh first prompt",
+        "unit": "ms",
+        "value": ${first_prompt_lag_ms}
+    },
+    {
+        "name": "zsh first command",
+        "unit": "ms",
+        "value": ${first_command_lag_ms}
     },
     {
         "name": "neovim load time",
-        "unit": "Second",
+        "unit": "ms",
         "value": ${NVIM_LOAD_TIME}
     }
 ]
