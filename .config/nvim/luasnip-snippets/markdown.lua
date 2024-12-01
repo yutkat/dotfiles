ca0139b5390b6a88d7754abfd903de5519482d2e
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2024-12-01 15:07:14 +0900

    neovim: Update luasnip

--- .config/nvim/luasnip-snippets/markdown.lua
+++ .config/nvim/luasnip-snippets/markdown.lua
@@ -40,4 +40,32 @@ return {
 			)
 		end, { 1 }),
 	}),
+	s({ trig = "table(%d+)x(%d+)", regTrig = true }, {
+		d(1, function(args, snip)
+			local nodes = {}
+			local i_counter = 0
+			local hlines = ""
+			for _ = 1, snip.captures[2] do
+				i_counter = i_counter + 1
+				table.insert(nodes, t("| "))
+				table.insert(nodes, i(i_counter, "Column" .. i_counter))
+				table.insert(nodes, t(" "))
+				hlines = hlines .. "|---"
+			end
+			table.insert(nodes, t { "|", "" })
+			hlines = hlines .. "|"
+			table.insert(nodes, t { hlines, "" })
+			for _ = 1, snip.captures[1] do
+				for _ = 1, snip.captures[2] do
+					i_counter = i_counter + 1
+					table.insert(nodes, t("| "))
+					table.insert(nodes, i(i_counter))
+					print(i_counter)
+					table.insert(nodes, t(" "))
+				end
+				table.insert(nodes, t { "|", "" })
+			end
+			return sn(nil, nodes)
+		end
+		) })
 }
