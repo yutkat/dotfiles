6e17ea7c3fb6f26690d730b26a845c8931bae9bb
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2025-02-10 15:51:19 +0900

    ci: Fix to create the directory (#3621)

--- install_scripts/dotsinstaller.sh
+++ install_scripts/dotsinstaller.sh
@@ -90,6 +90,7 @@ function main() {
 
 	if [[ "$is_update" = true ]]; then
 		source $current_dir/lib/dotsinstaller/install-basic-packages.sh
+		mkdir -p ~/.local/
 		ln -snf $current_dir/lib/dotsinstaller/bin/* ~/.local/bin/
 
 		if [[ "$with_gui" = true ]]; then
