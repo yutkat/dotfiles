b095ca5dc66ac0ab002b6a62df11d70fa2823b9e
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2025-04-27 17:52:41 +0900

    feat(neovim): Change the plugin for current word highlight (#3633)

--- .config/nvim/lua/rc/pluginconfig/snacks.lua
+++ .config/nvim/lua/rc/pluginconfig/snacks.lua
@@ -26,7 +26,7 @@ require("snacks").setup({
 	notifier = { enabled = true },
 	quickfile = { enabled = true },
 	scope = { enabled = true },
-	scroll = { enabled = true },
+	scroll = { enabled = false },
 	statuscolumn = { enabled = true },
 	words = { enabled = true },
 })
