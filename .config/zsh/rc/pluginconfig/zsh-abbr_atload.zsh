ad3becf13577fc5cac6f187677d4392aa3b068cb
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2025-06-14 22:48:25 +0900

    feat(zsh): Change the abbrev plugin

---
+++ .config/zsh/rc/pluginconfig/zsh-abbr_atload.zsh
@@ -0,0 +1,10 @@
+abbr -g -f -q -S G="| grep"
+
+# git
+abbr -f -q gc='git commit'
+abbr -f -q ga='git add .'
+abbr -f -q gb='git branch'
+abbr -f -q gd='git diff HEAD'
+abbr -f -q gs='git status --short'
+abbr -f -q gr='gh renew'
+#abbr -f -q pr='gh pr view -w || gh pr create -w'
