ad678e02584832e8d1857a9e5901e063c6d05dde
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2025-06-17 11:49:00 +0900

    fix(zsh): Suppress the message (#3649)

--- .config/zsh/rc/pluginconfig/zsh-abbr_atload.zsh
+++ .config/zsh/rc/pluginconfig/zsh-abbr_atload.zsh
@@ -1,10 +1,10 @@
 abbr -g -f -q -S G="| grep"
 
 # git
-abbr -f -q gc='git commit'
-abbr -f -q ga='git add .'
-abbr -f -q gb='git branch'
-abbr -f -q gd='git diff HEAD'
-abbr -f -q gs='git status --short'
-abbr -f -q gr='gh renew'
+abbr -f --quieter gc='git commit'
+abbr -f --quieter ga='git add .'
+abbr -f --quieter gb='git branch'
+abbr -f --quieter gd='git diff HEAD'
+abbr -f --quieter gs='git status --short'
+abbr -f --quieter gr='gh renew'
 #abbr -f -q pr='gh pr view -w || gh pr create -w'
