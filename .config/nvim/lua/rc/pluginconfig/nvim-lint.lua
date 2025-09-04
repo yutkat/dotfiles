e8260a636733217d297c6b67def1c482f9e720aa
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2025-09-04 13:47:33 +0900

    feat(neovim): Update markdownlint's rule (#3695)

--- .config/nvim/lua/rc/pluginconfig/nvim-lint.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-lint.lua
@@ -1,20 +1,22 @@
 require('lint').linters_by_ft = {
-	lua = { 'selene', },
-	sh = { 'shellcheck', },
-	bash = { 'shellcheck', },
-	zsh = { 'shellcheck', },
-	javascript = { 'eslint_d' },
-	typescript = { 'eslint_d' },
-	rust = { 'clippy', },
-	markdown = { 'markdownlint-cli2', },
+  lua = { 'selene', },
+  sh = { 'shellcheck', },
+  bash = { 'shellcheck', },
+  zsh = { 'shellcheck', },
+  javascript = { 'eslint_d' },
+  typescript = { 'eslint_d' },
+  rust = { 'clippy', },
+  markdown = { 'markdownlint-cli2', },
 }
+require('lint').linters["markdownlint-cli2"].args = { "--config", "~/.config/markdownlint-cli2/.markdownlint-cli2.yaml" }
+
 require("lint").try_lint()
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-	callback = function()
-		require("lint").try_lint()
-		if vim.fn.filereadable(".vale.ini") > 0 then
-			require("lint").try_lint({ "vale" })
-		end
-	end,
+  callback = function()
+    require("lint").try_lint()
+    if vim.fn.filereadable(".vale.ini") > 0 then
+      require("lint").try_lint({ "vale" })
+    end
+  end,
 })
