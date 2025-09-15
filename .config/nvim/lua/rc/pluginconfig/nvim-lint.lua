55b70d3cb38a002e68fb1d8f1d9311775f5a3a59
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2025-09-15 17:33:48 +0900

    feat(neovim): Update nvim-lint to support multi linters (#3706)

--- .config/nvim/lua/rc/pluginconfig/nvim-lint.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-lint.lua
@@ -1,16 +1,31 @@
-require('lint').linters_by_ft = {
-  lua = { 'selene', },
-  sh = { 'shellcheck', },
-  bash = { 'shellcheck', },
-  zsh = { 'shellcheck', },
-  javascript = { 'eslint_d' },
-  typescript = { 'eslint_d' },
-  rust = { 'clippy', },
-  markdown = { 'markdownlint-cli2', },
-}
-require('lint').linters["markdownlint-cli2"].args = { "--config", "~/.config/markdownlint-cli2/.markdownlint-cli2.yaml" }
+local lint = require('lint')
+local function pick_linters(linters)
+  vim.print(linters)
+  for _, l in ipairs(linters) do
+    print(l)
+    if lint.linters[l] and vim.fn.executable(lint.linters[l].cmd()) == 1 then
+      return { l }
+    end
+  end
+  return {}
+end
+local function make_linters_by_ft()
+  return {
+    lua        = { 'selene', },
+    sh         = { 'shellcheck', },
+    bash       = { 'shellcheck', },
+    zsh        = { 'shellcheck', },
+    rust       = { 'clippy', },
+    markdown   = { 'markdownlint-cli2', },
+    javascript = pick_linters({ "biomejs", "eslint_d" }),
+    typescript = pick_linters({ "biomejs", "eslint_d" }),
+  }
+end
 
-require("lint").try_lint()
+lint.linters_by_ft = make_linters_by_ft()
+lint.linters["markdownlint-cli2"].args = { "--config", "~/.config/markdownlint-cli2/.markdownlint-cli2.yaml" }
+
+lint.try_lint()
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   callback = function()
