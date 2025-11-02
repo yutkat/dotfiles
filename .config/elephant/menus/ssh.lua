68f552cd6ccee85b3d0da81df63093b85226d2cb
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2025-11-02 17:30:57 +0900

    fix(elephant): Fix the custom menu

--- .config/elephant/menus/ssh.lua
+++ .config/elephant/menus/ssh.lua
@@ -1,23 +1,28 @@
+Name = "ssh"
+NamePretty = "ssh"
+Icon = "applications-other"
+Action = "~/.local/bin/x-terminal-emulator ssh %VALUE%"
+
 -- selene: allow(unused_variable)
 function GetEntries()
-  local entries = {}
-  local ssh_config = os.getenv("HOME") .. "/.ssh/config"
+	local entries = {}
+	local ssh_config = os.getenv("HOME") .. "/.ssh/config"
 
-  local handle = io.open(ssh_config, "r")
-  if handle then
-    for line in handle:lines() do
-      local hostname = line:match("^%s*Host%s+([^%s*?]+)%s*$")
-      if hostname and hostname ~= "" then
-        table.insert(entries, {
-          Text = hostname,
-          Subtext = "ssh host",
-          Value = hostname,
-          Icon = ssh_config
-        })
-      end
-    end
-    handle:close()
-  end
+	local handle = io.open(ssh_config, "r")
+	if handle then
+		for line in handle:lines() do
+			local hostname = line:match("^%s*Host%s+([^%s*?]+)%s*$")
+			if hostname and hostname ~= "" then
+				table.insert(entries, {
+					Text = hostname,
+					Subtext = "ssh host",
+					Value = hostname,
+					Icon = ssh_config,
+				})
+			end
+		end
+		handle:close()
+	end
 
-  return entries
+	return entries
 end
