65635a9a01c3cf5291a47c55182f38f32024d1a6
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2025-12-06 18:23:17 +0900

    feat(wezterm): Add pause mode (#3739)

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -15,46 +15,46 @@ require("on")
 -- selene: allow(unused_variable)
 ---@diagnostic disable-next-line: unused-function, unused-local
 local function enable_wayland()
-  local wayland = os.getenv("XDG_SESSION_TYPE")
-  if wayland == "wayland" then
-    return true
-  end
-  return false
+	local wayland = os.getenv("XDG_SESSION_TYPE")
+	if wayland == "wayland" then
+		return true
+	end
+	return false
 end
 
 ---------------------------------------------------------------
 --- Merge the Config
 ---------------------------------------------------------------
 local function create_ssh_domain_from_ssh_config(ssh_domains)
-  if ssh_domains == nil then
-    ssh_domains = {}
-  end
-  for host, config in pairs(wezterm.enumerate_ssh_hosts()) do
-    table.insert(ssh_domains, {
-      name = host,
-      remote_address = config.hostname .. ":" .. config.port,
-      username = config.user,
-      multiplexing = "None",
-      assume_shell = "Posix",
-    })
-  end
-  return { ssh_domains = ssh_domains }
+	if ssh_domains == nil then
+		ssh_domains = {}
+	end
+	for host, config in pairs(wezterm.enumerate_ssh_hosts()) do
+		table.insert(ssh_domains, {
+			name = host,
+			remote_address = config.hostname .. ":" .. config.port,
+			username = config.user,
+			multiplexing = "None",
+			assume_shell = "Posix",
+		})
+	end
+	return { ssh_domains = ssh_domains }
 end
 
 --- load local_config
 -- Write settings you don't want to make public, such as ssh_domains
 package.path = os.getenv("HOME") .. "/.local/share/wezterm/?.lua;" .. package.path
 local function load_local_config(module)
-  local m = package.searchpath(module, package.path)
-  if m == nil then
-    return {}
-  end
-  return dofile(m)
-  -- local ok, _ = pcall(require, "local")
-  -- if not ok then
-  -- 	return {}
-  -- end
-  -- return require("local")
+	local m = package.searchpath(module, package.path)
+	if m == nil then
+		return {}
+	end
+	return dofile(m)
+	-- local ok, _ = pcall(require, "local")
+	-- if not ok then
+	-- 	return {}
+	-- end
+	-- return require("local")
 end
 
 local local_config = load_local_config("local")
@@ -77,88 +77,90 @@ local local_config = load_local_config("local")
 --- Config
 ---------------------------------------------------------------
 local config = {
-  -- font = wezterm.font("Cica"),
-  -- font_size = 10.0,
-  font = wezterm.font("UDEV Gothic 35NFLG"),
-  font_size = 8.5,
-  -- cell_width = 1.1,
-  -- line_height = 1.1,
-  -- font_rules = {
-  -- 	{
-  -- 		italic = true,
-  -- 		font = wezterm.font("Cica", { italic = true }),
-  -- 	},
-  -- 	{
-  -- 		italic = true,
-  -- 		intensity = "Bold",
-  -- 		font = wezterm.font("Cica", { weight = "Bold", italic = true }),
-  -- 	},
-  -- },
-  check_for_updates = false,
-  use_ime = true,
-  ime_preedit_rendering = "Builtin",
-  use_dead_keys = false,
-  warn_about_missing_glyphs = false,
-  -- enable_kitty_graphics = false,
-  animation_fps = 1,
-  cursor_blink_ease_in = "Constant",
-  cursor_blink_ease_out = "Constant",
-  cursor_blink_rate = 0,
-  -- https://github.com/wez/wezterm/issues/4972
-  -- enable_wayland = enable_wayland(),
-  enable_wayland = true,
-  -- https://github.com/wez/wezterm/issues/1772
-  -- https://github.com/wez/wezterm/issues/5103
-  -- enable_wayland = false,
-  color_scheme = "nordfox",
-  color_scheme_dirs = { os.getenv("HOME") .. "/.config/wezterm/colors/" },
-  hide_tab_bar_if_only_one_tab = false,
-  adjust_window_size_when_changing_font_size = false,
-  selection_word_boundary = " \t\n{}[]()\"'`,;:│=&!%",
-  window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-  },
-  use_fancy_tab_bar = false,
-  notification_handling = "SuppressFromFocusedTab",
-  colors = {
-    tab_bar = {
-      background = scheme.background,
-      new_tab = { bg_color = "#2e3440", fg_color = scheme.ansi[8], intensity = "Bold" },
-      new_tab_hover = { bg_color = scheme.ansi[1], fg_color = scheme.brights[8], intensity = "Bold" },
-      -- format-tab-title
-      -- active_tab = { bg_color = "#121212", fg_color = "#FCE8C3" },
-      -- inactive_tab = { bg_color = scheme.background, fg_color = "#FCE8C3" },
-      -- inactive_tab_hover = { bg_color = scheme.ansi[1], fg_color = "#FCE8C3" },
-    },
-  },
-  inactive_pane_hsb = {
-    saturation = 0.7,
-    brightness = 0.7,
-  },
-  exit_behavior = "CloseOnCleanExit",
-  tab_bar_at_bottom = false,
-  window_close_confirmation = "AlwaysPrompt",
-  -- window_background_opacity = 0.8,
-  disable_default_key_bindings = true,
-  -- visual_bell = {
-  -- 	fade_in_function = "EaseIn",
-  -- 	fade_in_duration_ms = 150,
-  -- 	fade_out_function = "EaseOut",
-  -- 	fade_out_duration_ms = 150,
-  -- },
-  -- separate <Tab> <C-i>
-  enable_csi_u_key_encoding = true,
-  leader = { key = "Space", mods = "CTRL|SHIFT" },
-  keys = keybinds.create_keybinds(),
-  key_tables = keybinds.key_tables,
-  mouse_bindings = keybinds.mouse_bindings,
-  -- https://github.com/wez/wezterm/issues/2756
-  webgpu_preferred_adapter = gpus[1],
-  prefer_egl = true,
-  front_end = "WebGpu",
+	-- font = wezterm.font("Cica"),
+	-- font_size = 10.0,
+	font = wezterm.font("UDEV Gothic 35NFLG"),
+	font_size = 8.5,
+	-- cell_width = 1.1,
+	-- line_height = 1.1,
+	-- font_rules = {
+	-- 	{
+	-- 		italic = true,
+	-- 		font = wezterm.font("Cica", { italic = true }),
+	-- 	},
+	-- 	{
+	-- 		italic = true,
+	-- 		intensity = "Bold",
+	-- 		font = wezterm.font("Cica", { weight = "Bold", italic = true }),
+	-- 	},
+	-- },
+	check_for_updates = false,
+	use_ime = true,
+	send_composed_key_when_left_alt_is_pressed = false,
+	send_composed_key_when_right_alt_is_pressed = false,
+	ime_preedit_rendering = "Builtin",
+	use_dead_keys = false,
+	warn_about_missing_glyphs = false,
+	-- enable_kitty_graphics = false,
+	animation_fps = 1,
+	cursor_blink_ease_in = "Constant",
+	cursor_blink_ease_out = "Constant",
+	cursor_blink_rate = 0,
+	-- https://github.com/wez/wezterm/issues/4972
+	-- enable_wayland = enable_wayland(),
+	enable_wayland = true,
+	-- https://github.com/wez/wezterm/issues/1772
+	-- https://github.com/wez/wezterm/issues/5103
+	-- enable_wayland = false,
+	color_scheme = "nordfox",
+	color_scheme_dirs = { os.getenv("HOME") .. "/.config/wezterm/colors/" },
+	hide_tab_bar_if_only_one_tab = false,
+	adjust_window_size_when_changing_font_size = false,
+	selection_word_boundary = " \t\n{}[]()\"'`,;:│=&!%",
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+	use_fancy_tab_bar = false,
+	notification_handling = "SuppressFromFocusedTab",
+	colors = {
+		tab_bar = {
+			background = scheme.background,
+			new_tab = { bg_color = "#2e3440", fg_color = scheme.ansi[8], intensity = "Bold" },
+			new_tab_hover = { bg_color = scheme.ansi[1], fg_color = scheme.brights[8], intensity = "Bold" },
+			-- format-tab-title
+			-- active_tab = { bg_color = "#121212", fg_color = "#FCE8C3" },
+			-- inactive_tab = { bg_color = scheme.background, fg_color = "#FCE8C3" },
+			-- inactive_tab_hover = { bg_color = scheme.ansi[1], fg_color = "#FCE8C3" },
+		},
+	},
+	inactive_pane_hsb = {
+		saturation = 0.7,
+		brightness = 0.7,
+	},
+	exit_behavior = "CloseOnCleanExit",
+	tab_bar_at_bottom = false,
+	window_close_confirmation = "AlwaysPrompt",
+	-- window_background_opacity = 0.8,
+	disable_default_key_bindings = true,
+	-- visual_bell = {
+	-- 	fade_in_function = "EaseIn",
+	-- 	fade_in_duration_ms = 150,
+	-- 	fade_out_function = "EaseOut",
+	-- 	fade_out_duration_ms = 150,
+	-- },
+	-- separate <Tab> <C-i>
+	enable_csi_u_key_encoding = true,
+	leader = { key = "Space", mods = "CTRL|SHIFT" },
+	keys = keybinds.create_keybinds(),
+	key_tables = keybinds.key_tables,
+	mouse_bindings = keybinds.mouse_bindings,
+	-- https://github.com/wez/wezterm/issues/2756
+	webgpu_preferred_adapter = gpus[1],
+	prefer_egl = true,
+	front_end = "WebGpu",
 }
 
 -- https://github.com/wez/wezterm/commit/1e552d764349522dabffeb240feb5b2728eff3d8
@@ -171,49 +173,49 @@ local config = {
 -- end
 
 config.hyperlink_rules = {
-  -- Matches: a URL in parens: (URL)
-  {
-    regex = '\\((\\w+://\\S+)\\)',
-    format = '$1',
-    highlight = 1,
-  },
-  -- Matches: a URL in brackets: [URL]
-  {
-    regex = '\\[(\\w+://\\S+)\\]',
-    format = '$1',
-    highlight = 1,
-  },
-  -- Matches: a URL in curly braces: {URL}
-  {
-    regex = '\\{(\\w+://\\S+)\\}',
-    format = '$1',
-    highlight = 1,
-  },
-  -- Matches: a URL in angle brackets: <URL>
-  {
-    regex = '<(\\w+://\\S+)>',
-    format = '$1',
-    highlight = 1,
-  },
-  -- Then handle URLs not wrapped in brackets
-  {
-    -- Before
-    --regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
-    --format = '$0',
-    -- After
-    regex = '[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)',
-    format = '$1',
-    highlight = 1,
-  },
-  -- implicit mailto link
-  {
-    regex = '\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b',
-    format = 'mailto:$0',
-  },
+	-- Matches: a URL in parens: (URL)
+	{
+		regex = "\\((\\w+://\\S+)\\)",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in brackets: [URL]
+	{
+		regex = "\\[(\\w+://\\S+)\\]",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in curly braces: {URL}
+	{
+		regex = "\\{(\\w+://\\S+)\\}",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in angle brackets: <URL>
+	{
+		regex = "<(\\w+://\\S+)>",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Then handle URLs not wrapped in brackets
+	{
+		-- Before
+		--regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
+		--format = '$0',
+		-- After
+		regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
+		format = "$1",
+		highlight = 1,
+	},
+	-- implicit mailto link
+	{
+		regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+		format = "mailto:$0",
+	},
 }
 table.insert(config.hyperlink_rules, {
-  regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
-  format = "https://github.com/$1/$3",
+	regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+	format = "https://github.com/$1/$3",
 })
 
 local merged_config = utils.merge_tables(config, local_config)
