6bb65346bb9657a82145f02fb61098b1ee75c54f
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2025-12-20 00:17:58 +0900

    fix(neovim): Update linter & formatter (#3748)

--- .config/nvim/lua/rc/pluginconfig/nvim-lint.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-lint.lua
@@ -1,24 +1,24 @@
-local lint = require('lint')
+local lint = require("lint")
 local function pick_linters(linters)
-  for _, l in ipairs(linters) do
-    if lint.linters[l] and vim.fn.executable(lint.linters[l].cmd()) == 1 then
-      return { l }
-    end
-  end
-  return {}
+	for _, l in ipairs(linters) do
+		if lint.linters[l] and vim.fn.executable(lint.linters[l].cmd()) == 1 then
+			return { l }
+		end
+	end
+	return {}
 end
 local function make_linters_by_ft()
-  return {
-    lua        = { 'selene', },
-    sh         = { 'shellcheck', },
-    bash       = { 'shellcheck', },
-    zsh        = { 'shellcheck', },
-    rust       = { 'clippy', },
-    python     = { 'ruff', },
-    markdown   = { 'markdownlint-cli2', },
-    javascript = pick_linters({ "biomejs", "eslint_d" }),
-    typescript = pick_linters({ "biomejs", "eslint_d" }),
-  }
+	return {
+		lua = { "selene" },
+		sh = { "shellcheck" },
+		bash = { "shellcheck" },
+		zsh = { "shellcheck" },
+		rust = { "clippy" },
+		python = { "ruff" },
+		markdown = { "markdownlint-cli2" },
+		javascript = pick_linters({ "biomejs", "eslint_d" }),
+		typescript = pick_linters({ "biomejs", "eslint_d" }),
+	}
 end
 
 lint.linters_by_ft = make_linters_by_ft()
@@ -27,10 +27,10 @@ lint.linters["markdownlint-cli2"].args = { "--config", "~/.config/markdownlint-c
 lint.try_lint()
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-    require("lint").try_lint()
-    if vim.fn.filereadable(".vale.ini") > 0 then
-      require("lint").try_lint({ "vale" })
-    end
-  end,
+	callback = function()
+		require("lint").try_lint()
+		if vim.fn.filereadable(".vale.ini") > 0 then
+			require("lint").try_lint({ "vale" })
+		end
+	end,
 })
