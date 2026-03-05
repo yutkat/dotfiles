7e17f5e4e3dba5d6c6445e9edca616f601d50b77
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2026-03-05 16:49:52 +0900

    fix(neovim): Fix the multi-line completion

--- .config/nvim/lua/rc/pluginconfig/blink-cmp.lua
+++ .config/nvim/lua/rc/pluginconfig/blink-cmp.lua
@@ -33,7 +33,7 @@ require("blink.cmp").setup({
 	},
 	snippets = { preset = "luasnip" },
 	completion = {
-		list = { selection = { preselect = false } },
+		list = { selection = { preselect = false, auto_insert = false } },
 		-- trigger = { show_in_snippet = false }
 		documentation = { auto_show = true, auto_show_delay_ms = 500, treesitter_highlighting = false },
 	},
