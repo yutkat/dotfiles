4a8b87d8d27d13d081dd6c8dc40b3e10b64ab10c
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2026-03-05 16:50:30 +0900

    fix(neovim): Fix the multi-line completion (#3816)

--- .config/nvim/lua/rc/pluginconfig/copilot.lua
+++ .config/nvim/lua/rc/pluginconfig/copilot.lua
@@ -1,30 +1,31 @@
 require("copilot").setup({
 	suggestion = {
-		auto_trigger = true,
-		keymap = {
-			accept = "<C-S-y>",
-			accept_word = false,
-			accept_line = false,
-			next = "<C-S-n>",
-			prev = "<C-S-p>",
-			dismiss = "<C-S-]>",
-		},
+		enabled = false,
+		-- auto_trigger = true,
+		-- keymap = {
+		-- 	accept = "<C-S-y>",
+		-- 	accept_word = false,
+		-- 	accept_line = false,
+		-- 	next = "<C-S-n>",
+		-- 	prev = "<C-S-p>",
+		-- 	dismiss = "<C-S-]>",
+		-- },
 	},
 })
 
 vim.api.nvim_command("highlight link CopilotAnnotation LineNr")
 vim.api.nvim_command("highlight link CopilotSuggestion LineNr")
 
-vim.keymap.set("i", "<C-S-e>", function()
-  -- Gracefully hide any completion menu if present (blink or cmp), then accept
-  pcall(function()
-    if package.loaded["blink.cmp"] then require("blink.cmp").hide() end
-  end)
-  pcall(function()
-    if package.loaded["cmp"] then require("cmp").mapping.abort() end
-  end)
-  require("copilot.suggestion").accept()
-end, {
-	desc = "[copilot] accept suggestion",
-	silent = true,
-})
+-- vim.keymap.set("i", "<C-S-e>", function()
+--   -- Gracefully hide any completion menu if present (blink or cmp), then accept
+--   pcall(function()
+--     if package.loaded["blink.cmp"] then require("blink.cmp").hide() end
+--   end)
+--   pcall(function()
+--     if package.loaded["cmp"] then require("cmp").mapping.abort() end
+--   end)
+--   require("copilot.suggestion").accept()
+-- end, {
+-- 	desc = "[copilot] accept suggestion",
+-- 	silent = true,
+-- })
