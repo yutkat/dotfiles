22db4035b59c269a290eb6a74edc804114494065
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2026-03-15 16:58:08 +0900

    fix(neovim): Fix the lint (#3822)

--- .config/nvim/lua/rc/pluginconfig/scrap.lua
+++ .config/nvim/lua/rc/pluginconfig/scrap.lua
@@ -1,1 +1,13 @@
 local scrap = require("scrap")
+
+local patterns = {
+	{ "mx{,s}", "matri{x,ces}" },
+}
+
+local expanded = scrap.expand_many(patterns)
+-- - mx => matrix
+-- - Mx => Matrix
+-- - mxs => matrices
+-- - Mxs => Matrices
+
+scrap.many_local_abbreviations(expanded)
