d418cb33ee5634f11fe091a8f84ec12fba13d1c6
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2026-06-22 11:52:55 +0900

    fix(neovim): Fix blink.pairs build (#3885)

--- .config/nvim/lua/rc/pluginlist.lua
+++ .config/nvim/lua/rc/pluginlist.lua
@@ -860,6 +860,9 @@ local plugins = {
 		"saghen/blink.pairs",
 		version = "*",
 		dependencies = "saghen/blink.lib",
+		build = function()
+			require("blink.pairs").download():pwait(60000)
+		end,
 		event = "VimEnter",
 		config = function()
 			require("rc/pluginconfig/blink-pairs")
