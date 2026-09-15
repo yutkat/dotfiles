f2b1aca7d527e8edb1100ff0159a2fae10e42b08
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2026-09-15 16:06:14 +0900

    feat(mise): Add ouch and difftastic (#3952)

--- .config/zsh/rc/alias.zsh
+++ .config/zsh/rc/alias.zsh
@@ -108,20 +108,15 @@ alias -s {jpg,jpeg,png,bmp}='feh'
 alias -s mp3='mplayer'
 function extract() {
 	case $1 in
-	*.tar.gz | *.tgz) tar xzvf "$1" ;;
-	*.tar.xz) tar Jxvf "$1" ;;
-	*.zip) unzip "$1" ;;
+	# Legacy formats are not supported by ouch.
 	*.lzh) lha e "$1" ;;
-	*.tar.bz2 | *.tbz) tar xjvf "$1" ;;
 	*.tar.Z) tar zxvf "$1" ;;
-	*.gz) gzip -d "$1" ;;
-	*.bz2) bzip2 -dc "$1" ;;
 	*.Z) uncompress "$1" ;;
-	*.tar) tar xvf "$1" ;;
 	*.arj) unarj "$1" ;;
+	*) ouch decompress --here "$@" ;;
 	esac
 }
-alias -s {gz,tgz,zip,lzh,bz2,tbz,Z,tar,arj,xz}=extract
+alias -s {gz,tgz,zip,lzh,bz,bz2,tbz,tbz2,Z,tar,arj,xz,txz,7z,rar,lzma,lz,lz4,sz,zst,tzst,bz3,br}=extract
 
 #==============================================================#
 ##          App                                               ##
